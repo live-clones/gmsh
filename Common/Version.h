ef347379d6b494de5a59d648537af3e680ca3bce
Christophe Geuzaine <cgeuzaine@ulg.ac.be>
2000-11-24 00:53:24 +0000

    *** empty log message ***

--- Common/Version.h
+++
@@ -1,7 +0,0 @@
-#define GMSH_VERSION  0.995
-#define GMSH_DATE     "Fri Nov 24 00:22:29 CET 2000"
-#define GMSH_HOST     "geuz.montefiore.ulg.ac.be"
-#define GMSH_PACKAGER "geuzaine"
-#define GMSH_OS       "Linux 2.2.16-22"
-#define GMSH_URL      "http://www.geuz.org/gmsh/"
-#define GMSH_EMAIL    "Christophe.Geuzaine@ulg.ac.be, Remacle@scorec.rpi.edu"
