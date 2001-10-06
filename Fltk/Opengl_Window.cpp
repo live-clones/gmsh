e12ecaa7116907fc24346ed6cd7eb9ba5fdfb832
Christophe Geuzaine <cgeuzaine@ulg.ac.be>
2001-10-06 09:21:47 +0000

    set line width = 1 for the lasso zoom

--- Fltk/Opengl_Window.cpp
+++ Fltk/Opengl_Window.cpp
@@ -1,4 +1,4 @@
-// $Id: Opengl_Window.cpp,v 1.17 2001-08-11 23:28:31 geuzaine Exp $
+// $Id: Opengl_Window.cpp,v 1.18 2001-10-06 09:21:47 geuzaine Exp $
 
 #include "Gmsh.h"
 #include "Numeric.h"
@@ -62,6 +62,7 @@ void Opengl_Window::draw() {
     glColor3f(1.,1.,1.);
     glBlendFunc(GL_ONE_MINUS_DST_COLOR, GL_ZERO);
     glEnable(GL_BLEND);
+    glLineWidth(1);
     glBegin(GL_LINE_STRIP);
     glVertex2d(ZOOM_X0, ZOOM_Y0);
     glVertex2d(ZOOM_X1, ZOOM_Y0);
