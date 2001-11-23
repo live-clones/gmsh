c7cc07bbcd924803ba030246c6fdb99ef9ce0a2f
Christophe Geuzaine <cgeuzaine@ulg.ac.be>
2001-11-23 12:14:05 +0000

    corrected focus stuff for ColorBarWindow (thanks to Michael Sweet)

--- Fltk/Colorbar_Window.cpp
+++ Fltk/Colorbar_Window.cpp
@@ -1,4 +1,4 @@
-// $Id: Colorbar_Window.cpp,v 1.15 2001-11-14 14:39:39 geuzaine Exp $
+// $Id: Colorbar_Window.cpp,v 1.16 2001-11-23 12:14:05 geuzaine Exp $
 
 #include "Gmsh.h"
 #include "GmshUI.h"
@@ -349,7 +349,14 @@ int Colorbar_Window::handle(int event){
 
   switch(event){
 
+  case FL_FOCUS : 
+    return 1;
+
+  case FL_UNFOCUS : 
+    return 1;
+
   case FL_SHORTCUT :
+  case FL_KEYBOARD : 
 
     if(Fl::test_shortcut('0')){
       ColorTable_InitParam(0, ct, 1, 1); compute=1;
@@ -476,6 +483,7 @@ int Colorbar_Window::handle(int event){
     return 1;
 
   case FL_PUSH :
+    take_focus(); //force keyboard focus on the ColorbarWindow
     ibut = Fl::event_button();
     xpos = Fl::event_x();
     ypos = Fl::event_y();
