d6ab5e2849cba1331d27143d97afcc6f37e70146
Christophe Geuzaine <cgeuzaine@ulg.ac.be>
2002-05-18 08:06:10 +0000

    Add missing licences

--- utils/GmshClient.h
+++ utils/GmshClient.h
@@ -1,6 +1,24 @@
 #ifndef _GMSH_CLIENT_H_
 #define _GMSH_CLIENT_H_
 
+/*
+  Copyright (C) 1997 - 2002 C. Geuzaine, J.-F. Remacle
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+*/
+
 #define GMSH_CLIENT_START       1
 #define GMSH_CLIENT_STOP        2
 #define GMSH_CLIENT_INFO        10
