360d50f7a20f7387517e35117aa11b85860955f3
Christophe Geuzaine <cgeuzaine@ulg.ac.be>
2003-10-29 20:56:07 +0000

    forgot .in files

--- utils/commercial/sanitize.sh
+++ utils/commercial/sanitize.sh
@@ -11,7 +11,7 @@ path=$1
 
 # Remove the big license blocks
 
-files=`find ${path} -name "*.cpp" -o -name "*.h" -o -name "*.y"\
+files=`find ${path} -name "*.cpp" -o -name "*.h" -o -name "*.y" -o -name "*.in"\
                  -o -name "*.l" -o -name "*.texi" -o -name "Makefile*"`
 
 for A in $files; do
@@ -34,6 +34,12 @@ for A in $files; do
 @c\
 @c This software is provided "as is" without express or implied warranty.\
 @c See the file "COPYING" for the licensing terms.\
+'\
+      -e "/dnl This program is free software; you can redistribute/,/USA./D"\
+      -e '/dnl Copyright (C).*Remacle/a\
+dnl\
+dnl This software is provided "as is" without express or implied warranty.\
+dnl See the file "COPYING" for the licensing terms.\
 '\
   $A.old > $A
   rm -f $A.old
