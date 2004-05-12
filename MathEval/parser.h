6974762c2708dc95a7a81bc604d527e2d6f757be
Christophe Geuzaine <cgeuzaine@ulg.ac.be>
2004-05-12 18:38:44 +0000

    fix make parser on linux

--- MathEval/parser.h
+++ MathEval/parser.h
@@ -1,11 +1,20 @@
+#ifndef BISON_PARSER_HPP
+# define BISON_PARSER_HPP
+
+#ifndef YYSTYPE
 typedef union {
   Node *node;
   Record *record;
-} YYSTYPE;
-#define	NUMBER	257
-#define	VARIABLE	258
-#define	FUNCTION	259
-#define	NEG	260
+} yystype;
+# define YYSTYPE yystype
+# define YYSTYPE_IS_TRIVIAL 1
+#endif
+# define	NUMBER	257
+# define	VARIABLE	258
+# define	FUNCTION	259
+# define	NEG	260
 
 
 extern YYSTYPE melval;
+
+#endif /* not BISON_PARSER_HPP */
