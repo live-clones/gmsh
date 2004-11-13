25514ecbe472cab87cacf2a92d7432c6dccac7a4
Christophe Geuzaine <cgeuzaine@ulg.ac.be>
2004-11-13 23:57:28 +0000

    - First draft (pretty much untested!) of new "Integrate" plugin to   * integrate scalar fields over all the elements in a view   * integrate the circulation of vector fields along line elements   * integrate the flux of vector fields across surface elements
      Used with Plugin(DisplacementRaise) and Plugin(Evaluate) this
      permits for example to compute the area/volume of deformed
      configurations; and, with Plugin(CutPlane)+Plugin(Skin), the
      perimeter of deformed sections. Another interesting application is
      to use it on a vector field with Plugin(CutPlane), in order to
      compute fluxes across arbitrary cross-sections.
    
    - Added "connectPoints" option to Plugin(CutParametric) so
      that we can feed its output to Plugin(Integrate)
    
    - Added Normals and Tangents options to visualize the orientation of
      elements in post-processing views
    
    - Added "swapOrientation" in Plugin(Transform) to change the
      orientation of the elements (in place) (+ moved the transformation
      routines from the view class into the plugin)
    
    - fixed #defines in some of the plugin header files

---
+++ Plugin/ShapeFunctions.h
@@ -0,0 +1,524 @@
+#ifndef _SHAPE_FUNCTIONS_H_
+#define _SHAPE_FUNCTIONS_H_
+
+// Copyright (C) 1997-2004 C. Geuzaine, J.-F. Remacle
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+// USA.
+// 
+// Please report all bugs and problems to <gmsh@geuz.org>.
+
+#include "Numeric.h"
+
+class element{
+protected:
+  double *_x, *_y, *_z;
+public:
+  element(double *x, double *y, double *z) : _x(x), _y(y), _z(z) {}
+  virtual ~element(){}
+  virtual int getDimension() = 0;
+  virtual int getNumNodes() = 0;
+  virtual int getNumGaussPoints() = 0;
+  virtual void getGaussPoint(int num, double &u, double &v, double &w, double &weight) = 0; 
+  virtual void getShapeFunction(int num, double u, double v, double w, double &s) = 0;
+  virtual void getGradShapeFunction(int num, double u, double v, double w, double s[3]) = 0;
+  double getJacobian(double u, double v, double w, double jac[3][3])
+  {
+    jac[0][0] = jac[0][1] = jac[0][2] = 0.;
+    jac[1][0] = jac[1][1] = jac[1][2] = 0.;
+    jac[2][0] = jac[2][1] = jac[2][2] = 0.;
+    double s[3];
+    switch(getDimension()){
+    case 3 :
+      for(int i = 0; i < getNumNodes(); i++) {
+	getGradShapeFunction(i, u, v, w, s);
+	jac[0][0] += _x[i] * s[0]; jac[0][1] += _y[i] * s[0]; jac[0][2] += _z[i] * s[0];
+	jac[1][0] += _x[i] * s[1]; jac[1][1] += _y[i] * s[1]; jac[1][2] += _z[i] * s[1];
+	jac[2][0] += _x[i] * s[2]; jac[2][1] += _y[i] * s[2]; jac[2][2] += _z[i] * s[2];
+      }
+      return
+	jac[0][0] * jac[1][1] * jac[2][2] + jac[0][2] * jac[1][0] * jac[2][1] +
+	jac[0][1] * jac[1][2] * jac[2][0] - jac[0][2] * jac[1][1] * jac[2][0] -
+	jac[0][0] * jac[1][2] * jac[2][1] - jac[0][1] * jac[1][0] * jac[2][2];
+    case 2 :
+      for(int i = 0; i < getNumNodes(); i++) {
+	getGradShapeFunction(i, u, v, w, s);
+	jac[0][0] += _x[i] * s[0]; jac[0][1] += _y[i] * s[0]; jac[0][2] += _z[i] * s[0];
+	jac[1][0] += _x[i] * s[1]; jac[1][1] += _y[i] * s[1]; jac[1][2] += _z[i] * s[1];
+      }
+      return sqrt(DSQR(jac[0][0] * jac[1][1] - jac[0][1] * jac[1][0]) +
+		  DSQR(jac[0][2] * jac[1][0] - jac[0][0] * jac[1][2]) +
+		  DSQR(jac[0][1] * jac[1][2] - jac[0][2] * jac[1][1]));
+    case 1:
+      for(int i = 0; i < getNumNodes(); i++) {
+	getGradShapeFunction(i, u, v, w, s);
+	jac[0][0] += _x[i] * s[0]; jac[0][1] += _y[i] * s[0]; jac[0][2] += _z[i] * s[0];
+      }
+      return sqrt(DSQR(jac[0][0])+DSQR(jac[0][1])+DSQR(jac[0][2]));
+    default:
+      return 1.;
+    }
+  }
+  double interpolate(double val[], double u, double v, double w)
+  {
+    double sum = 0;
+    for(int i = 0; i < getNumNodes(); i++){
+      double s;
+      getShapeFunction(i, u, v, w, s);
+      sum += val[i] * s;
+    }
+    return sum;
+  }
+  double integrate(double val[])
+  {
+    double sum = 0;
+    for(int i = 0; i < getNumGaussPoints(); i++){
+      double u, v, w, weight, jac[3][3];
+      getGaussPoint(i, u, v, w, weight);
+      double det = getJacobian(u, v, w, jac);
+      double d = interpolate(val, u, v, w);
+      sum += d * weight * det;
+    }
+    return sum;
+  }
+};
+
+class point : public element{
+public:
+  point(double *x, double *y, double *z) : element(x, y, z) {}
+  inline int getDimension(){ return 0; }
+  inline int getNumNodes(){ return 1; }
+  inline int getNumGaussPoints(){ return 1; }
+  void getGaussPoint(int num, double &u, double &v, double &w, double &weight)
+  {
+    u = v = w = 0.;
+    weight = 1.;
+  }
+  void getShapeFunction(int num, double u, double v, double w, double &s) 
+  {
+    switch(num) {
+    case 0  : s = 1.; break;
+    default : s = 0.; break;
+    }
+  }
+  void getGradShapeFunction(int num, double u, double v, double w, double s[3]) 
+  {
+    s[0] = s[1] = s[2] = 0.;
+  }
+};
+
+class line : public element{
+public:
+  line(double *x, double *y, double *z) : element(x, y, z) {}
+  inline int getDimension(){ return 1; }
+  inline int getNumNodes(){ return 2; }
+  inline int getNumGaussPoints(){ return 1; }
+  void getGaussPoint(int num, double &u, double &v, double &w, double &weight)
+  {
+    if(num < 0 || num > 0) return;
+    u = v = w = 0.;
+    weight = 2.;
+  }
+  void getShapeFunction(int num, double u, double v, double w, double &s) 
+  {
+    switch(num) {
+    case 0  : s = 0.5 * (1.-u); break;
+    case 1  : s = 0.5 * (1.+u); break;
+    default : s = 0.; break;
+    }
+  }
+  void getGradShapeFunction(int num, double u, double v, double w, double s[3]) 
+  {
+    switch(num) {
+    case 0  : s[0] = -0.5; s[1] = 0.; s[2] = 0.; break;
+    case 1  : s[0] =  0.5; s[1] = 0.; s[2] = 0.; break;
+    default : s[0] = s[1] = s[2] = 0.; break;
+    }
+  }
+  double integrateCirculation(double val[])
+  {
+    double t[3] = {_x[1]-_x[0], _y[1]-_y[0], _z[1]-_z[0]};
+    norme(t);
+    double v[3];
+    for(int i = 0; i < 3; i++){
+      double tmp[2]; 
+      tmp[0] = val[i];
+      tmp[1] = val[3+i];
+      v[i] = integrate(tmp);
+    }
+    double d;
+    prosca(t, v, &d);
+    return d;
+  }
+};
+
+class triangle : public element{
+public:
+  triangle(double *x, double *y, double *z) : element(x, y, z) {}
+  inline int getDimension(){ return 2; }
+  inline int getNumNodes(){ return 3; }
+  inline int getNumGaussPoints(){ return 3; }
+  void getGaussPoint(int num, double &u, double &v, double &w, double &weight)
+  {
+    static double u3[3] = {0.16666666666666,0.66666666666666,0.16666666666666};
+    static double v3[3] = {0.16666666666666,0.16666666666666,0.66666666666666};
+    static double p3[3] = {0.16666666666666,0.16666666666666,0.16666666666666};
+    if(num < 0 || num > 2) return;
+    u = u3[num];
+    v = v3[num];
+    w = 0.;
+    weight = p3[num];
+  }
+  void getShapeFunction(int num, double u, double v, double w, double &s) 
+  {
+    switch(num){
+    case 0  : s = 1.-u-v; break;
+    case 1  : s =    u  ; break;
+    case 2  : s =      v; break;
+    default : s = 0.; break;
+    }
+  }
+  void getGradShapeFunction(int num, double u, double v, double w, double s[3]) 
+  {
+    switch(num) {
+    case 0  : s[0] = -1.; s[1] = -1.; s[2] =  0.; break;
+    case 1  : s[0] =  1.; s[1] =  0.; s[2] =  0.; break;
+    case 2  : s[0] =  0.; s[1] =  1.; s[2] =  0.; break;
+    default : s[0] = s[1] = s[2] = 0.; break;
+    }
+  }
+  double integrateFlux(double val[])
+  {
+    double t1[3] = {_x[1]-_x[0], _y[1]-_y[0], _z[1]-_z[0]};
+    double t2[3] = {_x[2]-_x[0], _y[2]-_y[0], _z[2]-_z[0]};
+    double n[3];
+    prodve(t1, t2, n);
+    norme(n);
+    double v[3];
+    for(int i = 0; i < 3; i++){
+      double tmp[3]; 
+      tmp[0] = val[i];
+      tmp[1] = val[3+i];
+      tmp[2] = val[6+i];
+      v[i] = integrate(tmp);
+    }
+    double d;
+    prosca(n, v, &d);
+    return d;
+  }
+};
+
+class quadrangle : public element{
+public:
+  quadrangle(double *x, double *y, double *z) : element(x, y, z) {}
+  inline int getDimension(){ return 2; }
+  inline int getNumNodes(){ return 4; }
+  inline int getNumGaussPoints(){ return 4; }
+  void getGaussPoint(int num, double &u, double &v, double &w, double &weight)
+  {
+    static double u4[4] = {0.577350269189,-0.577350269189,0.577350269189,-0.577350269189};
+    static double v4[4] = {0.577350269189,0.577350269189,-0.577350269189,-0.577350269189};
+    static double p4[4] = {1.,1.,1.,1.};
+    if(num < 0 || num > 3) return;
+    u = u4[num];
+    v = v4[num];
+    w = 0.;
+    weight = p4[num];
+  }
+  void getShapeFunction(int num, double u, double v, double w, double &s) 
+  {
+    switch(num) {
+    case 0  : s = 0.25 * (1.-u) * (1.-v); break;
+    case 1  : s = 0.25 * (1.+u) * (1.-v); break;
+    case 2  : s = 0.25 * (1.+u) * (1.+v); break;
+    case 3  : s = 0.25 * (1.-u) * (1.+v); break;
+    default : s = 0.; break;
+    }
+  }
+  void getGradShapeFunction(int num, double u, double v, double w, double s[3]) 
+  {
+    switch(num) {
+    case 0  : s[0] = -0.25 * (1.-v); s[1] = -0.25 * (1.-u); s[2] = 0.; break;
+    case 1  : s[0] =  0.25 * (1.-v); s[1] = -0.25 * (1.+u); s[2] = 0.; break;
+    case 2  : s[0] =  0.25 * (1.+v); s[1] =  0.25 * (1.+u); s[2] = 0.; break;
+    case 3  : s[0] = -0.25 * (1.+v); s[1] =  0.25 * (1.-u); s[2] = 0.; break;
+    default : s[0] = s[1] = s[2] = 0.; break;
+    }
+  }
+  double integrateFlux(double val[])
+  {
+    double t1[3] = {_x[1]-_x[0], _y[1]-_y[0], _z[1]-_z[0]};
+    double t2[3] = {_x[2]-_x[0], _y[2]-_y[0], _z[2]-_z[0]};
+    double n[3];
+    prodve(t1, t2, n);
+    norme(n);
+    double v[3];
+    for(int i = 0; i < 3; i++){
+      double tmp[4]; 
+      tmp[0] = val[i];
+      tmp[1] = val[3+i];
+      tmp[2] = val[6+i];
+      tmp[3] = val[9+i];
+      v[i] = integrate(tmp);
+    }
+    double d;
+    prosca(n, v, &d);
+    return d;
+  }
+};
+
+class tetrahedron : public element{
+public:
+  tetrahedron(double *x, double *y, double *z) : element(x, y, z) {}
+  inline int getDimension(){ return 3; }
+  inline int getNumNodes(){ return 4; }
+  inline int getNumGaussPoints(){ return 4; }
+  void getGaussPoint(int num, double &u, double &v, double &w, double &weight)
+  {
+    static double u4[4] = {0.138196601125,0.138196601125,0.138196601125,0.585410196625};
+    static double v4[4] = {0.138196601125,0.138196601125,0.585410196625,0.138196601125};
+    static double w4[4] = {0.138196601125,0.585410196625,0.138196601125,0.138196601125};
+    static double p4[4] = {0.0416666666667,0.0416666666667,0.0416666666667,0.0416666666667};
+    if(num < 0 || num > 3) return;
+    u = u4[num];
+    v = v4[num];
+    w = w4[num];
+    weight = p4[num];
+  }
+  void getShapeFunction(int num, double u, double v, double w, double &s) 
+  {
+    switch(num) {
+    case 0  : s = 1.-u-v-w; break;
+    case 1  : s =    u    ; break;
+    case 2  : s =      v  ; break;
+    case 3  : s =        w; break;
+    default : s = 0.; break;
+    }
+  }
+  void getGradShapeFunction(int num, double u, double v, double w, double s[3]) 
+  {
+    switch(num) {
+    case 0  : s[0] = -1.; s[1] = -1.; s[2] = -1.; break;
+    case 1  : s[0] =  1.; s[1] =  0.; s[2] =  0.; break;
+    case 2  : s[0] =  0.; s[1] =  1.; s[2] =  0.; break;
+    case 3  : s[0] =  0.; s[1] =  0.; s[2] =  1.; break;
+    default : s[0] = s[1] = s[2] = 0.; break;
+    }
+  }
+};
+
+class hexahedron : public element{
+public:
+  hexahedron(double *x, double *y, double *z) : element(x, y, z) {}
+  inline int getDimension(){ return 3; }
+  inline int getNumNodes(){ return 8; }
+  inline int getNumGaussPoints(){ return 6; }
+  void getGaussPoint(int num, double &u, double &v, double &w, double &weight)
+  {
+    static double u6[6] = { 0.40824826,  0.40824826, -0.40824826,
+			    -0.40824826, -0.81649658,  0.81649658};
+    static double v6[6] = { 0.70710678, -0.70710678,  0.70710678, 
+			    -0.70710678,  0.,  0.};
+    static double w6[6] = {-0.57735027, -0.57735027,  0.57735027,  
+			   0.57735027, -0.57735027,  0.57735027};
+    static double p6[6] = { 1.3333333333,  1.3333333333,  1.3333333333,
+			    1.3333333333,  1.3333333333,  1.3333333333};
+    if(num < 0 || num > 5) return;
+    u = u6[num];
+    v = v6[num];
+    w = w6[num];
+    weight = p6[num];
+  }
+  void getShapeFunction(int num, double u, double v, double w, double &s) 
+  {
+    switch(num) {
+    case 0  : s = (1.-u) * (1.-v) * (1.-w) * 0.125; break;
+    case 1  : s = (1.+u) * (1.-v) * (1.-w) * 0.125; break;
+    case 2  : s = (1.+u) * (1.+v) * (1.-w) * 0.125; break;
+    case 3  : s = (1.-u) * (1.+v) * (1.-w) * 0.125; break;
+    case 4  : s = (1.-u) * (1.-v) * (1.+w) * 0.125; break;
+    case 5  : s = (1.+u) * (1.-v) * (1.+w) * 0.125; break;
+    case 6  : s = (1.+u) * (1.+v) * (1.+w) * 0.125; break;
+    case 7  : s = (1.-u) * (1.+v) * (1.+w) * 0.125; break;
+    default : s = 0.; break;
+    }
+  }
+  void getGradShapeFunction(int num, double u, double v, double w, double s[3]) 
+  {
+    switch(num) {
+    case 0  : s[0] = -0.125 * (1.-v) * (1.-w);
+              s[1] = -0.125 * (1.-u) * (1.-w);
+              s[2] = -0.125 * (1.-u) * (1.-v); break;
+    case 1  : s[0] =  0.125 * (1.-v) * (1.-w);
+              s[1] = -0.125 * (1.+u) * (1.-w);
+              s[2] = -0.125 * (1.+u) * (1.-v); break;
+    case 2  : s[0] =  0.125 * (1.+v) * (1.-w);
+              s[1] =  0.125 * (1.+u) * (1.-w);
+              s[2] = -0.125 * (1.+u) * (1.+v); break;
+    case 3  : s[0] = -0.125 * (1.+v) * (1.-w);
+              s[1] =  0.125 * (1.-u) * (1.-w);
+              s[2] = -0.125 * (1.-u) * (1.+v); break;
+    case 4  : s[0] = -0.125 * (1.-v) * (1.+w);
+              s[1] = -0.125 * (1.-u) * (1.+w);
+              s[2] =  0.125 * (1.-u) * (1.-v); break;
+    case 5  : s[0] =  0.125 * (1.-v) * (1.+w);
+              s[1] = -0.125 * (1.+u) * (1.+w);
+              s[2] =  0.125 * (1.+u) * (1.-v); break;
+    case 6  : s[0] =  0.125 * (1.+v) * (1.+w);
+              s[1] =  0.125 * (1.+u) * (1.+w);
+              s[2] =  0.125 * (1.+u) * (1.+v); break;
+    case 7  : s[0] = -0.125 * (1.+v) * (1.+w);
+              s[1] =  0.125 * (1.-u) * (1.+w);
+              s[2] =  0.125 * (1.-u) * (1.+v); break;
+    default : s[0] = s[1] = s[2] = 0.; break;
+    }
+  }
+};
+
+class prism : public element{
+public:
+  prism(double *x, double *y, double *z) : element(x, y, z) {};
+  inline int getDimension(){ return 3; }
+  inline int getNumNodes(){ return 6; }
+  inline int getNumGaussPoints(){ return 6; }
+  void getGaussPoint(int num, double &u, double &v, double &w, double &weight)
+  {
+    static double u6[6] = {0.166666666666666, 0.333333333333333, 0.166666666666666, 
+			   0.166666666666666, 0.333333333333333, 0.166666666666666};
+    static double v6[6] = {0.166666666666666, 0.166666666666666, 0.333333333333333,
+			   0.166666666666666, 0.166666666666666, 0.333333333333333};
+    static double w6[6] = {-0.577350269189, -0.577350269189, -0.577350269189,
+			   0.577350269189,  0.577350269189,  0.577350269189};
+    static double p6[6] = {0.166666666666666,0.166666666666666,0.166666666666666,
+			   0.166666666666666,0.166666666666666,0.166666666666666,};
+    if(num < 0 || num > 5) return;
+    u = u6[num];
+    v = v6[num];
+    w = w6[num];
+    weight = p6[num];
+  }
+  void getShapeFunction(int num, double u, double v, double w, double &s) 
+  {
+    switch(num) {
+    case 0  : s = (1.-u-v) * (1.-w) * 0.5; break;
+    case 1  : s =     u    * (1.-w) * 0.5; break;
+    case 2  : s =       v  * (1.-w) * 0.5; break;
+    case 3  : s = (1.-u-v) * (1.+w) * 0.5; break;
+    case 4  : s =     u    * (1.+w) * 0.5; break;
+    case 5  : s =       v  * (1.+w) * 0.5; break;
+    default : s = 0.; break;
+    }
+  }
+  void getGradShapeFunction(int num, double u, double v, double w, double s[3]) 
+  {
+    switch(num) {
+    case 0  : s[0] = -0.5 * (1.-w)   ; 
+              s[1] = -0.5 * (1.-w)   ;
+              s[2] = -0.5 * (1.-u-v) ; break ;
+    case 1  : s[0] =  0.5 * (1.-w)   ; 
+              s[1] =  0.             ;
+              s[2] = -0.5 * u        ; break ;
+    case 2  : s[0] =  0.             ; 
+              s[1] =  0.5 * (1.-w)   ;
+              s[2] = -0.5 * v        ; break ;
+    case 3  : s[0] = -0.5 * (1.+w)   ; 
+              s[1] = -0.5 * (1.+w)   ;
+              s[2] =  0.5 * (1.-u-v) ; break ;
+    case 4  : s[0] =  0.5 * (1.+w)   ; 
+              s[1] =  0.             ;
+              s[2] =  0.5 * u        ; break ;
+    case 5  : s[0] =  0.             ; 
+              s[1] =  0.5 * (1.+w)   ;
+              s[2] =  0.5 * v        ; break ;
+    default : s[0] = s[1] = s[2] = 0.; break;
+    }
+  }
+};
+
+class pyramid : public element{
+public:
+  pyramid(double *x, double *y, double *z) : element(x, y, z) {}
+  inline int getDimension(){ return 3; }
+  inline int getNumNodes(){ return 5; }
+  inline int getNumGaussPoints(){ return 8; }
+  void getGaussPoint(int num, double &u, double &v, double &w, double &weight)
+  {
+    static double u8[8] = {0.3595161057791018,0.09633205020967324,
+			   0.3595161057791018,0.09633205020967324,
+			   0.6920507403468987,0.1854344369976602,
+			   0.6920507403468987,0.1854344369976602};
+    static double v8[8] = {0.3595161057791018,0.3595161057791018,
+			   0.09633205020967324,0.09633205020967324,
+			   0.6920507403468987,0.6920507403468987,
+			   0.1854344369976602,0.1854344369976602};
+    static double w8[8] = {0.544151844011225,0.544151844011225,
+			   0.544151844011225,0.544151844011225,
+			   0.122514822655441,0.122514822655441,
+			   0.122514822655441,0.122514822655441};
+    static double p8[8] = {0.02519647051995625,0.02519647051995625,
+			   0.02519647051995625,0.02519647051995625,
+			   0.058136862813377,0.058136862813377,
+			   0.058136862813377,0.058136862813377};
+    if(num < 0 || num > 7) return;
+    u = u8[num];
+    v = v8[num];
+    w = w8[num];
+    weight = p8[num];
+  }
+  void getShapeFunction(int num, double u, double v, double w, double &s) 
+  {
+    double r;
+    if(w != 1. && num != 4) r = u*v*w / (1.-w);
+    else                    r = 0.;
+    switch(num) {
+    case 0  : s = 0.25 * ((1.-u) * (1.-v) - w + r); break;
+    case 1  : s = 0.25 * ((1.+u) * (1.-v) - w - r); break;
+    case 2  : s = 0.25 * ((1.+u) * (1.+v) - w + r); break;
+    case 3  : s = 0.25 * ((1.-u) * (1.+v) - w - r); break;
+    case 4  : s = w; break;
+    default : s = 0.; break;
+    }
+  }
+  void getGradShapeFunction(int num, double u, double v, double w, double s[3]) 
+  {
+    if(w == 1. && num != 4) {
+      s[0] =  0.25; 
+      s[1] =  0.25;
+      s[2] = -0.25; 
+    }
+    else{
+      switch(num) {
+      case 0  : s[0] = 0.25 * ( -(1.-v) + v*w/(1.-w) ) ;
+	        s[1] = 0.25 * ( -(1.-u) + u*w/(1.-w) ) ;
+      	        s[2] = 0.25 * ( -1.     + u*v/DSQR(1.-w) ) ; break ;
+      case 1  : s[0] = 0.25 * (  (1.-v) + v*w/(1.-w) ) ;
+	        s[1] = 0.25 * ( -(1.+u) + u*w/(1.-w) ) ;
+	        s[2] = 0.25 * ( -1.     + u*v/DSQR(1.-w) ) ; break ;
+      case 2  : s[0] = 0.25 * (  (1.+v) + v*w/(1.-w) ) ;
+                s[1] = 0.25 * (  (1.+u) + u*w/(1.-w) ) ;
+                s[2] = 0.25 * ( -1.     + u*v/DSQR(1.-w) ) ; break ;
+      case 3  : s[0] = 0.25 * ( -(1.+v) + v*w/(1.-w) ) ;
+                s[1] = 0.25 * (  (1.-u) + u*w/(1.-w) ) ;
+                s[2] = 0.25 * ( -1.     + u*v/DSQR(1.-w) ) ; break ;
+      case 4  : s[0] = 0. ; 
+                s[1] = 0. ;
+                s[2] = 1. ; break ;
+      default : s[0] = s[1] = s[2] = 0.; break;
+      }
+    }
+  }
+};
+
+#endif
