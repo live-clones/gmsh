0809ecfa03771316adbc16a07ff0104230f3bcd8
Christophe Geuzaine <cgeuzaine@ulg.ac.be>
2004-11-22 08:10:29 +0000

    *** empty log message ***

--- Graphics/gl2ps.cpp
+++ Graphics/gl2ps.cpp
@@ -1,4 +1,4 @@
-/* $Id: gl2ps.cpp,v 1.89 2004-11-22 08:00:01 geuzaine Exp $ */
+/* $Id: gl2ps.cpp,v 1.90 2004-11-22 08:10:29 geuzaine Exp $ */
 /*
  * GL2PS, an OpenGL to PostScript Printing Library
  * Copyright (C) 1999-2004 Christophe Geuzaine <geuz@geuz.org>
@@ -1305,9 +1305,9 @@ static void gl2psSetPrecisionAndApplyOffsets()
 
   /* set precision */
   gl2ps->epsilon = (maxZ - minZ) / 5000.0F;
-  simple = (maxZ - minZ) / 500.0F;
+  simple = (maxZ - minZ) / 1000.0F;
   if(GL2PS_ZERO(gl2ps->epsilon)) gl2ps->epsilon = 0.0002F;
-  if(GL2PS_ZERO(simple)) simple = 0.002F;
+  if(GL2PS_ZERO(simple)) simple = 0.001F;
 
   /* apply offsets */
   if(gl2ps->options & GL2PS_SIMPLE_LINE_OFFSET){
@@ -1315,8 +1315,8 @@ static void gl2psSetPrecisionAndApplyOffsets()
       prim = *(GL2PSprimitive**)gl2psListPointer(gl2ps->primitives, i);
       if(prim->type == GL2PS_LINE){
 	if(gl2ps->sort == GL2PS_SIMPLE_SORT){
-	  prim->verts[0].xyz[2] -= simple * 10.0F;
-	  prim->verts[1].xyz[2] -= simple * 10.0F;
+	  prim->verts[0].xyz[2] -= simple * 20.0F;
+	  prim->verts[1].xyz[2] -= simple * 20.0F;
 	}
 	else{
 	  prim->verts[0].xyz[2] -= simple;
