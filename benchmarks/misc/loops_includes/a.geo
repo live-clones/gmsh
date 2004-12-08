4abcc19d7fc0c00c2ae1d25d02e9a86ea8e59d59
Christophe Geuzaine <cgeuzaine@ulg.ac.be>
2004-12-08 18:41:55 +0000

    This commit was manufactured by cvs2svn to create tag 'gmsh_1_57_0'.

--- benchmarks/misc/loops_includes/a.geo
+++
@@ -1,5 +0,0 @@
-For i In {1:5}
-  For j In {1:3}
-    Include "b.geo";
-  EndFor
-EndFor
--- utils/converters/xyz2pos.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-awk 'BEGIN{ printf "View \"xyz2pos\"{\n";  } { printf "SP(%g,%g,%g){%g};\n", $1, $2, $3, $4; } END {printf "};\n"}'
