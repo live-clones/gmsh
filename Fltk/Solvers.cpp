b13756a9b18886448ff1a39526a5807839f0f726
Christophe Geuzaine <cgeuzaine@ulg.ac.be>
2005-01-13 23:39:10 +0000

    don't include GmshClient

--- Fltk/Solvers.cpp
+++ Fltk/Solvers.cpp
@@ -1,4 +1,4 @@
-// $Id: Solvers.cpp,v 1.31 2005-01-01 19:35:28 geuzaine Exp $
+// $Id: Solvers.cpp,v 1.32 2005-01-13 23:39:10 geuzaine Exp $
 //
 // Copyright (C) 1997-2005 C. Geuzaine, J.-F. Remacle
 //
@@ -20,8 +20,22 @@
 // Please report all bugs and problems to <gmsh@geuz.org>.
 
 #include "Gmsh.h"
-#include "GmshClient.h"
 #include "GmshServer.h"
+
+// This should match what's in GmshClient.h
+#define GMSH_CLIENT_START       1
+#define GMSH_CLIENT_STOP        2
+#define GMSH_CLIENT_INFO        10
+#define GMSH_CLIENT_WARNING     11
+#define GMSH_CLIENT_ERROR       12
+#define GMSH_CLIENT_PROGRESS    13
+#define GMSH_CLIENT_VIEW        20
+#define GMSH_CLIENT_OPTION      100
+#define GMSH_CLIENT_OPTION_1    (GMSH_CLIENT_OPTION+0)
+#define GMSH_CLIENT_OPTION_2    (GMSH_CLIENT_OPTION+1)
+#define GMSH_CLIENT_OPTION_3    (GMSH_CLIENT_OPTION+2)
+#define GMSH_CLIENT_OPTION_4    (GMSH_CLIENT_OPTION+3)
+#define GMSH_CLIENT_OPTION_5    (GMSH_CLIENT_OPTION+4)
 
 #include <sys/types.h>
 #include <sys/socket.h>
