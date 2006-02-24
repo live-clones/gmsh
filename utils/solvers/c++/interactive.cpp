a1802176b5d2dcde6cf5165f05398e24f739fdb5
Christophe Geuzaine <cgeuzaine@ulg.ac.be>
2006-02-24 01:57:09 +0000

    added example of interactive command line driver for Gmsh

---
+++ utils/solvers/c++/interactive.cpp
@@ -0,0 +1,62 @@
+#include "GmshClient.h"
+#include <readline/readline.h>
+#include <readline/history.h>
+
+// compile with: g++ interact.cpp -lreadline
+
+class GmshInteractiveClient{
+ private:
+  GmshClient _client;
+ public:
+  GmshInteractiveClient()
+  {
+    using_history();
+    char *socket = "/Users/geuzaine/.gmshsock";
+    if(_client.Connect(socket) < 0) {
+      printf("Unable to connect to Gmsh\n");
+      exit(1);
+    }
+    _client.Start();
+  }
+  ~GmshInteractiveClient()
+  {
+    _client.Stop();
+    _client.Disconnect();
+  }
+  void read(char *prompt)
+  {
+    while (1) {
+      // read input char until CR, LF, EOF, ^D
+      char *ptr = readline(prompt);
+      // exit interactive if EOF or ^D
+      if(!ptr) break;
+      // if there is something in the line
+      if(strlen(ptr)){
+	// add the command in the stack
+	add_history(ptr);
+	if(!strcmp(ptr,"q") || !strcmp(ptr, "quit")){
+	  // exit interactive if q, quit, exit
+	  free(ptr);
+	  break;
+	}
+	else if(!strcmp(ptr, "dir") || !strcmp(ptr, "ls")){
+	  // direct system calls
+	  system("ls -al");
+	}
+	else{
+	  // pass any other command to gmsh
+	  _client.ParseString(ptr);
+	}
+      }
+      free(ptr);
+    }
+  }
+};
+
+int main() 
+{
+  GmshInteractiveClient c;
+  c.read("gmsh> ");
+
+  return 0;
+}
