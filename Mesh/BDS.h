d0bad1d9ae9d035c0ed388560bce4da5e0cbf75d
Christophe Geuzaine <cgeuzaine@ulg.ac.be>
2006-03-15 19:00:38 +0000

    ExtractEdge

--- Mesh/BDS.h
+++ Mesh/BDS.h
@@ -32,6 +32,7 @@
 #include <algorithm>
 #include <list>
 #include <math.h>
+#include "Views.h"
 
 class BDS_Tet;
 class BDS_Edge;
@@ -644,6 +645,7 @@ public:
   bool read_mesh(const char *filename);
   bool read_vrml(const char *filename);
   void save_gmsh_format(const char *filename);
+  bool import_view(Post_View *view, const double tolerance);
   void applyOptimizationPatterns();
 };
 
