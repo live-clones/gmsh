e7a4693ceca4afe69595f527f2718d010d7fe9f8
Christophe Geuzaine <cgeuzaine@ulg.ac.be>
2007-02-14 12:31:15 +0000

    use the 3D Delaunay by default (if tetgen is available)

--- Common/DefaultOptions.h
+++ Common/DefaultOptions.h
@@ -853,7 +853,12 @@ StringXNumber GeometryOptions_Number[] = {
 StringXNumber MeshOptions_Number[] = {
   { F|O, "Algorithm" , opt_mesh_algo2d , ALGO_2D_MESHADAPT ,
     "2D mesh algorithm (1=meshadapt, 2=delaunay)" }, 
-  { F|O, "Algorithm3D" , opt_mesh_algo3d , ALGO_3D_NETGEN ,
+  { F|O, "Algorithm3D" , opt_mesh_algo3d , 
+#if defined(HAVE_TETGEN)
+    ALGO_3D_DELAUNAY ,
+#else
+    ALGO_3D_NETGEN ,
+#endif
     "3D mesh algorithm (1=delaunay, 4=netgen, 5=tetgen)" }, 
   { F|O, "AngleSmoothNormals" , opt_mesh_angle_smooth_normals , 30.0 ,
     "Threshold angle below which normals are not smoothed" }, 
