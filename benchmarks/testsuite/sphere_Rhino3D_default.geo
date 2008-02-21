d8d650ed5b5ca3d9ef38d84679b1f998d4d9055b
Christophe Geuzaine <cgeuzaine@ulg.ac.be>
2008-02-21 14:33:10 +0000

    *** empty log message ***

---
+++ benchmarks/testsuite/sphere_Rhino3D_default.geo
@@ -0,0 +1,3 @@
+Geometry.OCCFixSmallEdges=0;
+Geometry.OCCFixSmallFaces=0;
+Merge "sphere_Rhino3D_default.igs";
--- benchmarks/testsuite/testsuite.sh
+++ benchmarks/testsuite/testsuite.sh
@@ -5,10 +5,9 @@ date >> statreport.dat
 ../../bin/gmsh Zylkopf.geo -clscale 1 -v 0 -2 -append_statreport statreport.dat
 ../../bin/gmsh Top.geo -clscale 1 -v 0 -2 -append_statreport statreport.dat
 ../../bin/gmsh ocean.geo -clscale 1 -v 0 -2 -append_statreport statreport.dat -epslc1d 1.e-2 -algo del2d
-../../bin/gmsh 15_cylinder_head.brep -2 -clscale .1 -append_statreport statreport.dat
-../../bin/gmsh sphere_Rhino3D_default.igs -clscale 1 -v 0 -2 -append_statreport statreport.dat
+../../bin/gmsh 15_cylinder_head.brep -v 0 -2 -clscale .1 -append_statreport statreport.dat
+../../bin/gmsh sphere_Rhino3D_default.geo -clscale 1 -v 0 -2 -append_statreport statreport.dat
 ../../bin/gmsh Cone_1.brep -clscale 1 -v 0 -2 -append_statreport statreport.dat
 ../../bin/gmsh Cylinder_1.brep -clscale 1 -v 0 -2 -append_statreport statreport.dat
 ../../bin/gmsh Torus_1.brep -clscale 300 -v 0 -2 -append_statreport statreport.dat
 ../../bin/gmsh A319.geo -clscale 3 -v 0 -2 -append_statreport statreport.dat
-
