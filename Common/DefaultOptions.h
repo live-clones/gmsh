5778668f6ba78f57790381960d89da4693242a08
Christophe Geuzaine <cgeuzaine@ulg.ac.be>
2009-04-24 09:27:08 +0000

    quote emacs

--- Common/DefaultOptions.h
+++ Common/DefaultOptions.h
@@ -69,7 +69,7 @@ StringXString GeneralOptions_String[] = {
 #elif defined(__APPLE__)
     "open -t %s" ,
 #else
-    "emacs %s &" ,
+    "emacs '%s' &" ,
 #endif
     "System command to launch a text editor" },
   { F|S, "TmpFileName" , opt_general_tmp_filename , ".gmsh-tmp" ,
