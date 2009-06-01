b229a6d886e21a61a2246ac8739fac53959c3c61
Christophe Geuzaine <cgeuzaine@ulg.ac.be>
2009-06-01 20:32:09 +0000

    *** empty log message ***

--- Fltk/fileDialogs.cpp
+++ Fltk/fileDialogs.cpp
@@ -34,7 +34,39 @@
 #include <FL/Fl_Native_File_Chooser.H>
 static Fl_Native_File_Chooser *fc = 0;
 #else
-#include "fileChooser.h"
+#include <FL/Fl_File_Chooser.H>
+#include <FL/Fl_Window.H>
+#include <FL/Fl_File_Input.H>
+class fileChooser : public Fl_File_Chooser {
+  // we derive our own so we can set its position (The original file
+  // chooser doesn't expose its window to the world, so we need to use
+  // a cheap hack to get to it. Even worse is the hack used to get the
+  // focus on the file input widget.)
+ private:
+  Fl_Window *_win;
+  Fl_File_Input *_in;
+ public:
+  fileChooser(const char *d, const char *p, int t, const char *title)
+    : Fl_File_Chooser(d, p, t, title) 
+  { 
+    _win = dynamic_cast<Fl_Window*>(newButton->parent()->parent()); 
+    _in = dynamic_cast<Fl_File_Input *>(previewButton->parent()->parent()->resizable());
+  }
+  void show()
+  {
+    if(_win){
+      _win->show();
+      rescan(); // necessary since fltk 1.1.7
+      if(_in) _in->take_focus();
+      else _win->take_focus();
+    }
+    else
+      Fl_File_Chooser::show();
+  }
+  void position(int x, int y){ if(_win) _win->position(x,y); }
+  int x(){ if(_win) return _win->x(); else return 100; }
+  int y(){ if(_win) return _win->y(); else return 100; }
+};
 static fileChooser *fc = 0;
 #endif
 
