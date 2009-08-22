bd8cc8203518e4c65093e851ae43a0da9c811661
Christophe Geuzaine <cgeuzaine@ulg.ac.be>
2009-08-22 13:29:09 +0000

    This commit was manufactured by cvs2svn to create tag 'gmsh_2_4_0'.

---
+++ contrib/MathEval/common.h
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 1999, 2002, 2003  Free Software Foundation, Inc.
+ * 
+ * This file is part of GNU libmatheval
+ * 
+ * GNU libmatheval is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2, or (at your option) any later
+ * version.
+ * 
+ * GNU libmatheval is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * program; see the file COPYING. If not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+#ifndef COMMON_H
+#define COMMON_H
+
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "MallocUtils.h"
+
+/* Macro definitions to simplify corresponding function calls.  */
+#define XMALLOC(type, num) ((type *) Malloc ((num) * sizeof(type)))
+#define XREALLOC(type, p, num) ((type *) Realloc ((p), (num) * sizeof(type)))
+#define XCALLOC(type, num) ((type *) Calloc ((num), sizeof(type)))
+#define XFREE(stale) Free (stale);
+
+#endif
