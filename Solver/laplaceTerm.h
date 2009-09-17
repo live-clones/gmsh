2ec0553a729292aa0aafc2245cf1c63ea26464a1
Jean-Francois Remacle <jean-francois.remacle@uclouvain.be>
2009-09-17 12:05:21 +0000

    more solver stuff

---
+++ Solver/laplaceTerm.h
@@ -0,0 +1,35 @@
+// Gmsh - Copyright (C) 1997-2009 C. Geuzaine, J.-F. Remacle
+//
+// See the LICENSE.txt file for license information. Please report all
+// bugs and problems to <gmsh@geuz.org>.
+
+#ifndef _LAPLACE_H_
+#define _LAPLACE_H_
+
+#include "helmholtzTerm.h"
+
+namespace gsolver {
+  // \nabla \cdot k \nabla U 
+  template<class scalar> 
+  class laplaceTerm : public helmholtzTerm<scalar> {
+    gmshFunction<scalar> *ONE;
+  public:
+    laplaceTerm(GModel *gm, int iFieldR) : 
+      helmholtzTerm<scalar>(gm,iFieldR,iFieldR,0,0)
+    {
+      ONE = new  gmshFunction<scalar>(1.0);
+      _k = ONE;
+    }
+    laplaceTerm(GModel *gm, int iFieldR, gmshFunction<scalar> *k) : 
+      helmholtzTerm<scalar>(gm,iFieldR,iFieldR,k,0),ONE(0)
+    {}
+    laplaceTerm(GModel *gm, int iFieldR, int iFieldC, gmshFunction<scalar> *k) : 
+      helmholtzTerm<scalar>(gm,iFieldR,iFieldC,k,0),ONE(0)
+    {}
+    virtual ~laplaceTerm(){
+      if(ONE) delete ONE;
+    } 
+  };// end of the class definition 
+}// end of namespace
+
+#endif
