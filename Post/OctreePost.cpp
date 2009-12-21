8df370e0f97bd2bda0049b7b29d17ac8fbd38db3
Christophe Geuzaine <cgeuzaine@ulg.ac.be>
2009-12-21 08:41:58 +0000

    fix compile

--- Post/OctreePost.cpp
+++ Post/OctreePost.cpp
@@ -353,6 +353,8 @@ bool OctreePost::_getValue(void *in, int nbComp, double P[3], int timestep,
   double U[3];
   e->xyz2uvw(P, U);
 
+  //printf("hey!\n");
+
   double nodeval[8 * 9];
   for(int step = 0; step < _theViewDataGModel->getNumTimeSteps(); step++){
     if(timestep < 0 || step == timestep){
