3aef592ea42444043335fd1deb2d04e20519af5a
Tuomas Karna <tuomas.karna@uclouvain.be>
2010-03-01 15:24:17 +0000

    fixed Stommel lua file

--- Solver/TESTCASES/Stommel.lua
+++ Solver/TESTCASES/Stommel.lua
@@ -1,6 +1,6 @@
 model = GModel()
 model:load ('stommel_square.msh')
-order=3
+order=1
 dimension = 2
 claw = dgConservationLawShallowWater2d()
 claw:addBoundaryCondition('Wall',claw:newBoundaryWall())
@@ -8,17 +8,11 @@ groups = dgGroupCollection(model, dimension, order)
 -- groups:split...
 groups:buildGroupsOfInterfaces()
 solution = dgDofContainer(groups, claw:getNbFields())
-a = fullMatrix(3,1);
-a:set(0,0, 1.);
-a:set(1,0, 2.);
-a:set(2,0, 3.);
-f = functionConstant(a):getName()
-solution:L2Projection(f)
 solution:exportMsh('output/init')
-
+rk=dgRungeKutta()
 
 for i=1,100000 do
-  norm = dg:RK44(150*(3/(2.*order+1)))
+  norm = rk:iterate33(claw,150*(3/(2.*order+1)/2),solution)
   if ( i%100 ==0 ) then
     print ('iter ', i, norm)
   end
