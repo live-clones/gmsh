9078416aa7ee759ab703771bc16dc14b002a2101
Bastien Gorissen <bastien.gorissen@cenaero.be>
2010-05-25 07:59:29 +0000

    Fix compile without lua

--- Common/DummyBindings.h
+++ Common/DummyBindings.h
@@ -14,6 +14,9 @@ class classBinding {
   template <typename cb>
   methodBinding *addMethod(std::string n, cb f){ return new methodBinding(); }
   template <typename tObj, typename t0, typename t1, typename t2, typename t3, 
+            typename t4, typename t5>
+  methodBinding *setConstructor(){ return 0; }
+  template <typename tObj, typename t0, typename t1, typename t2, typename t3, 
             typename t4>
   methodBinding *setConstructor(){ return 0; }
   template <typename tObj, typename t0, typename t1, typename t2, typename t3>
