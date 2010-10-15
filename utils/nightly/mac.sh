396284ce71dadb213dc145b1f5110db8381afc26
Christophe Geuzaine <cgeuzaine@ulg.ac.be>
2010-10-15 09:55:00 +0000

    build with petsc and slepc

--- utils/nightly/mac.sh
+++ utils/nightly/mac.sh
@@ -4,16 +4,18 @@ GMSH=${HOME}/src/gmsh
 LOG=${GMSH}/nightly.log
 WEB_BIN=geuzaine@geuz.org:/home/www/geuz.org/gmsh/bin/MacOSX
 CMAKE=/usr/local/bin/cmake
+PETSC_DIR=${HOME}/src/petsc-3.0.0-p7
+PETSC_ARCH=umfpack-cxx-opt
+SLEPC_DIR=${HOME}/src/slepc-3.0.0-p7
 
 rm -f ${LOG}
 rm -rf ${GMSH}/bin
 echo "BUILD BEGIN: `date`" > ${LOG}
 cd ${GMSH} && svn update >> ${LOG} 2>&1
 mkdir ${GMSH}/bin
-cd ${GMSH}/bin && \
+cd ${GMSH}/bin &&\
   ${CMAKE} -DGMSH_EXTRA_VERSION:string="-svn"\
            -DCMAKE_PREFIX_PATH:path="/usr/local;/usr/local/opencascade"\
-           -DENABLE_PETSC:bool=FALSE\
   ${GMSH} >> ${LOG} 2>&1
 cd ${GMSH}/bin && make package -j 4 >> ${LOG} 2>&1
 echo "BUILD END: `date`" >> ${LOG}
--- utils/nightly/windows.sh
+++ utils/nightly/windows.sh
@@ -4,6 +4,9 @@ GMSH=${HOME}/src/gmsh
 LOG=${GMSH}/nightly.log
 WEB_BIN=geuzaine@geuz.org:/home/www/geuz.org/gmsh/bin/Windows
 CMAKE=cmake
+PETSC_DIR=${HOME}/src/petsc-3.0.0-p7
+PETSC_ARCH=umfpack-cxx-opt
+SLEPC_DIR=${HOME}/src/slepc-3.0.0-p7
 
 rm -f ${LOG}
 rm -rf ${GMSH}/bin
@@ -15,7 +18,6 @@ cd ${GMSH}/bin && \
            -DCMAKE_PREFIX_PATH:path="/usr/local;/usr/local/opencascade"\
            -DCMAKE_CXX_FLAGS:string="-mno-cygwin"\
            -DCMAKE_C_FLAGS:string="-mno-cygwin"\
-           -DENABLE_PETSC:bool=FALSE\
   ${GMSH} >> ${LOG} 2>&1
 cd ${GMSH}/bin && make package >> ${LOG} 2>&1
 echo "BUILD END: `date`" >> ${LOG}
