3f6084c271f842a36b7026c115fbfab22b249f4a
Christophe Geuzaine <cgeuzaine@ulg.ac.be>
2012-01-21 14:07:15 +0000

    copyright update

--- Fltk/aboutWindow.cpp
+++ Fltk/aboutWindow.cpp
@@ -35,7 +35,7 @@ aboutWindow::aboutWindow()
   o->box(FL_FLAT_BOX);
   std::ostringstream sstream;
   sstream << "<center><h3>Gmsh</h3><br>version " << GetGmshVersion()
-          << "<p>Copyright (C) 1997-2011"
+          << "<p>Copyright (C) 1997-2012"
           << "<br>Christophe Geuzaine and Jean-Francois Remacle"
           << "<p><a href=\"http://geuz.org/gmsh/doc/CREDITS.txt\">Credits</a> "
           << "and <a href=\"http://geuz.org/gmsh/doc/LICENSE.txt\">licensing "
