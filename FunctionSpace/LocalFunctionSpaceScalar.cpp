7bd6a51b2a09e44f122b949de3a2f970ac0beb06
Nicolas Marsic <nicolas.marsic@gmail.com>
2012-06-28 15:40:21 +0000

    LocalFunctionSpace no longer needed

--- FunctionSpace/LocalFunctionSpaceScalar.cpp
+++
@@ -1,9 +0,0 @@
-#include "LocalFunctionSpaceScalar.h"
-
-LocalFunctionSpaceScalar::LocalFunctionSpaceScalar(void){
-  scalar = true;
-}
-
-LocalFunctionSpaceScalar::~LocalFunctionSpaceScalar(void){
-
-}
--- FunctionSpace/LocalFunctionSpaceVector.cpp
+++
@@ -1,9 +0,0 @@
-#include "LocalFunctionSpaceVector.h"
-
-LocalFunctionSpaceVector::LocalFunctionSpaceVector(void){
-  scalar = false;
-}
-
-LocalFunctionSpaceVector::~LocalFunctionSpaceVector(void){
-
-}
