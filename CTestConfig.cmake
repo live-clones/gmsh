96aaf00471cf154f6f26d3c3fdf4e8559f7d558f
Christophe Geuzaine <cgeuzaine@ulg.ac.be>
2012-08-17 08:28:50 +0000

    ctest work

---
+++ CTestConfig.cmake
@@ -0,0 +1,12 @@
+# Gmsh - Copyright (C) 1997-2012 C. Geuzaine, J.-F. Remacle
+#
+# See the LICENSE.txt file for license information. Please report all
+# bugs and problems to <gmsh@geuz.org>.
+
+set(CTEST_PROJECT_NAME "Gmsh")
+set(CTEST_DROP_METHOD "http")
+set(CTEST_DROP_SITE_CDASH TRUE)
+set(CTEST_DROP_SITE "onelab.info")
+set(CTEST_DROP_LOCATION "/CDash/submit.php?project=Gmsh")
+set(CTEST_SITE ${GMSH_HOST})
+set(CTEST_TRIGGER_SITE "")
