aafaaed5d8337c6b2daf62288705f975ff06e2c5
Christophe Geuzaine <cgeuzaine@ulg.ac.be>
2013-01-23 10:30:00 +0000

    fix

--- Geo/GModelIO_MESH.cpp
+++ Geo/GModelIO_MESH.cpp
@@ -109,7 +109,8 @@ int GModel::readMESH(const std::string &name)
         for(int i = 0; i < nbe; i++) {
           if(!fgets(buffer, sizeof(buffer), fp)) break;
           int n[6], cl;
-          sscanf(buffer, "%d %d %d %d", &n[0], &n[1], &n[2], &n[3], &n[4], &n[5],&cl);
+          sscanf(buffer, "%d %d %d %d %d %d %d", &n[0], &n[1], &n[2],
+                 &n[3], &n[4], &n[5], &cl);
           for(int j = 0; j < 6; j++) n[j]--;
           std::vector<MVertex*> vertices;
           if(!getVertices(6, n, vertexVector, vertices)) return 0;
@@ -154,7 +155,8 @@ int GModel::readMESH(const std::string &name)
         for(int i = 0; i < nbe; i++) {
           if(!fgets(buffer, sizeof(buffer), fp)) break;
           int n[10], cl;
-          sscanf(buffer, "%d %d %d %d %d %d %d %d %d %d", &n[0], &n[1], &n[2], &n[3],&n[4], &n[5], &n[6], &n[7], &n[9], &n[8], &cl);
+          sscanf(buffer, "%d %d %d %d %d %d %d %d %d %d %d", &n[0], &n[1], &n[2],
+                 &n[3], &n[4], &n[5], &n[6], &n[7], &n[9], &n[8], &cl);
           for(int j = 0; j < 10; j++) n[j]--;
           std::vector<MVertex*> vertices;
           if(!getVertices(10, n, vertexVector, vertices)) return 0;
@@ -251,7 +253,6 @@ int GModel::writeMESH(const std::string &name, int elementTagType,
       fprintf(fp, " TrianglesP2\n");
     else
       fprintf(fp, " Triangles\n");
-
     fprintf(fp, " %d\n", numTriangles);
     for(fiter it = firstFace(); it != lastFace(); ++it){
       int numPhys = (*it)->physicals.size();
