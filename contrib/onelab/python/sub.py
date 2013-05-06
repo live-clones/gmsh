de8a9d08f85015367abad3f2c4de76eff9544920
Christophe Geuzaine <cgeuzaine@ulg.ac.be>
2013-05-06 09:12:23 +0000

    OnelabClient.py -> onelab.py

--- contrib/onelab/python/sub.py
+++ contrib/onelab/python/sub.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 #coding=utf-8
 
-import OnelabClient as onelab
+import onelab
 
 OL = onelab.client()
 
--- contrib/onelab/python/test.py
+++ contrib/onelab/python/test.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #coding=utf-8
-import OnelabClient as onelab
+import onelab
 
 modelName = 'coin'
 
