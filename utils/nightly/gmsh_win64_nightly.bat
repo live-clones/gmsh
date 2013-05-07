654cfec69c632fbc987cf790eefb4eae8dfd5f1f
Christophe Geuzaine <cgeuzaine@ulg.ac.be>
2013-05-07 05:58:56 +0000

    disable dynamic builds on windows for now

--- utils/nightly/gmsh_win64_nightly.bat
+++ utils/nightly/gmsh_win64_nightly.bat
@@ -4,4 +4,5 @@ C:
 chdir C:\cygwin\bin
 
 bash --login -c "cd ${HOME}/src/gmsh/utils/nightly && ctest -S gmsh_win64_nightly.ctest -VV > ${HOME}/src/gmsh/nightly.log 2>&1"
-bash --login -c "cd ${HOME}/src/gmsh/utils/nightly && ctest -S gmsh_win64_dynamic_nightly.ctest -VV > ${HOME}/src/gmsh/nightly_dynamic.log 2>&1"
+
+REM bash --login -c "cd ${HOME}/src/gmsh/utils/nightly && ctest -S gmsh_win64_dynamic_nightly.ctest -VV > ${HOME}/src/gmsh/nightly_dynamic.log 2>&1"
