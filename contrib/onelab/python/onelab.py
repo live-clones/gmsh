26bffd7697dda3e4dfc05c073550559994e857e9
Christophe Geuzaine <cgeuzaine@ulg.ac.be>
2013-05-12 12:24:46 +0000

    missing copyright

--- contrib/onelab/python/onelab.py
+++ contrib/onelab/python/onelab.py
@@ -1,3 +1,32 @@
+"""
+OneLab - Copyright (C) 2011-2013 ULg-UCL
+
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without
+restriction, including without limitation the rights to use, copy,
+modify, merge, publish, distribute, and/or sell copies of the
+Software, and to permit persons to whom the Software is furnished
+to do so, provided that the above copyright notice(s) and this
+permission notice appear in all copies of the Software and that
+both the above copyright notice(s) and this permission notice
+appear in supporting documentation.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT OF THIRD PARTY RIGHTS. IN NO EVENT SHALL THE
+COPYRIGHT HOLDER OR HOLDERS INCLUDED IN THIS NOTICE BE LIABLE FOR
+ANY CLAIM, OR ANY SPECIAL INDIRECT OR CONSEQUENTIAL DAMAGES, OR ANY
+DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS,
+WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS
+ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE
+OF THIS SOFTWARE.
+
+Please report all bugs and problems to the public mailing list
+<gmsh@geuz.org>.
+"""
+
 import socket, struct, os, sys
 _VERSION = '1.05'
 
@@ -280,7 +309,3 @@ class client :
     if self.socket :
       self._send(self._GMSH_STOP, 'Goodbye!')
       self.socket.close()
-
-
-
-      
