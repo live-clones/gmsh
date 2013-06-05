e553ef12310a112265a9704daddde415cf307cca
Christophe Geuzaine <cgeuzaine@ulg.ac.be>
2013-06-05 12:58:31 +0000

    ctrl+h pour afficher les shortcuts sous linux

--- Fltk/graphicWindow.cpp
+++ Fltk/graphicWindow.cpp
@@ -1966,7 +1966,7 @@ static Fl_Menu_Item bar_table[] = {
     {0},
   {"&Help", 0, 0, 0, FL_SUBMENU},
     {"On&line Documentation", 0, (Fl_Callback *)help_online_cb, 0, FL_MENU_DIVIDER},
-    {"&Keyboard and Mouse Usage",    0, (Fl_Callback *)help_basic_cb, 0, FL_MENU_DIVIDER},
+    {"&Keyboard and Mouse Usage",  FL_CTRL+'h', (Fl_Callback *)help_basic_cb, 0, FL_MENU_DIVIDER},
     {"&Current Options",      0, (Fl_Callback *)status_options_cb, (void*)"?", 0},
     {"&Restore all Options to Default Settings", 0, (Fl_Callback *)options_restore_defaults_cb, 0, FL_MENU_DIVIDER},
     {"&About Gmsh",           0, (Fl_Callback *)help_about_cb, 0},
