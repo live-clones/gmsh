c3b12d2bf5b66130659463ba56b73b9022733c21
Maxime Graulich <maxime.graulich@gmail.com>
2013-10-01 06:48:53 +0000

    oops, forgot to commit this one

--- contrib/mobile/iOS/Onelab/PostProcessingViewController.h
+++ contrib/mobile/iOS/Onelab/PostProcessingViewController.h
@@ -21,7 +21,10 @@
 @property (weak, nonatomic) IBOutlet UILabel *Name;
 @property (weak, nonatomic) IBOutlet UIPickerView *IntervalsType;
 @property (weak, nonatomic) IBOutlet UITextField *Intervals;
+@property (weak, nonatomic) IBOutlet UIStepper *IntervalsStepper;
 @property (weak, nonatomic) IBOutlet UISlider *RaiseZ;
+
+- (IBAction)stepperValueChanged:(UIStepper *)sender;
 
 - (void)setPView:(PView*)p;
 
