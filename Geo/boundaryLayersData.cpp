5623db364b99798345609ca2bb6f3b21cde6af89
Gauthier Becker <gauthierbecker@gmail.com>
2013-10-13 16:07:53 +0000

    Fix compile without MESH and ANN

--- Geo/boundaryLayersData.cpp
+++ Geo/boundaryLayersData.cpp
@@ -22,7 +22,15 @@ BoundaryLayerField* getBLField(GModel *gm){ return 0; }
 bool buildAdditionalPoints2D (GFace *gf ) { return false; }
 BoundaryLayerColumns * buildAdditionalPoints3D (GRegion *gr) { return 0; }
 void buildMeshMetric(GFace *gf, double *uv, SMetric3 &m, double metric[3]) {}
-
+faceColumn BoundaryLayerColumns::getColumns(GFace *gf, MVertex *v1, MVertex *v2,
+                                            MVertex *v3, int side)
+{
+  return faceColumn(BoundaryLayerData(),BoundaryLayerData(),BoundaryLayerData());
+}
+edgeColumn BoundaryLayerColumns::getColumns(MVertex *v1, MVertex *v2 , int side)
+{
+  return edgeColumn(BoundaryLayerData(),BoundaryLayerData());
+}
 #else
 
 #include "Field.h"
