555d7b8af0ed3626c4272c119311f1e1dec3b5b4
Christophe Geuzaine <cgeuzaine@ulg.ac.be>
2013-10-23 14:01:27 +0000

    fix compile on osx 10.9

--- Fltk/Main.cpp
+++ Fltk/Main.cpp
@@ -3,6 +3,7 @@
 // See the LICENSE.txt file for license information. Please report all
 // bugs and problems to the public mailing list <gmsh@geuz.org>.
 
+#include <stdlib.h>
 #include <string>
 #include "Gmsh.h"
 #include "GmshMessage.h"
