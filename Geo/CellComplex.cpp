6b82316853f5767811a4246aa1dfa3b1907c1b2b
Christophe Geuzaine <cgeuzaine@ulg.ac.be>
2013-12-01 08:30:43 +0000

    fix crash in reduction

--- Geo/CellComplex.cpp
+++ Geo/CellComplex.cpp
@@ -370,7 +370,7 @@ int CellComplex::reduction(int dim, int omit,
       }
 
       if(getSize(dim) == 0 || getSize(dim-1) == 0) break;
-      cit++;
+      if(cit != lastCell(dim)) cit++;
     }
   }
   _reduced = true;
