0cd369f9a7f054382d9f434a26eaf692f3e8ede9
Christophe Geuzaine <cgeuzaine@ulg.ac.be>
2014-03-03 07:40:42 +0000

    fix win build

--- Parser/Gmsh.tab.cpp
+++ Parser/Gmsh.tab.cpp
@@ -7444,7 +7444,7 @@ yyreduce:
 	GModel::current()->setName((yyvsp[(2) - (3)].c));
       }
       else if(!strcmp((yyvsp[(1) - (3)].c), "CreateDir")){
-	CreateDirectory((yyvsp[(2) - (3)].c));
+	CreateDirectory(std::string((yyvsp[(2) - (3)].c)));
       }
       else{
 	yymsg(0, "Unknown command '%s'", (yyvsp[(1) - (3)].c));
