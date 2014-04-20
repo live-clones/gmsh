live-clones/gmsh

commit d794aaa05df665539da1960d98b2ade8417e8163

meshGFace : remove 2 memory leaks (other still remain)
git-svn-id: https://geuz.org/svn/gmsh/trunk@18338 59c8cabf-10d6-4a28-b43f-3936b85956c4

Mesh/meshGFace.cpp
@@ -1389,8 +1389,9 @@ bool meshGenerator(GFace *gf, int RECUR_ITER,
     Msg::Debug("Delaunizing the initial mesh");
     delaunayizeBDS(gf, *m, nb_swap);
   }
-  gf->triangles.clear();
-  gf->quadrangles.clear();
+  //gf->triangles.clear();
+  //gf->quadrangles.clear();
+  gf->deleteMesh();
 
   Msg::Debug("Starting to add internal points");
   // start mesh generation

Mesh/meshGFaceDelaunayInsertion.cpp
@@ -792,7 +792,7 @@ bool insertVertexB (std::list<edgeXface> &shell,
     //    _printTris("new_cavity.pos", new_cavity.begin(), new_cavity.end(), Us, Vs, false);
     //    _printTris("newTris.pos", &newTris[0], newTris+shell.size(), Us, Vs, false);
     //    _printTris("allTris.pos", allTets.begin(),allTets.end(), Us, Vs, false);
-    for (unsigned int i = 0; i < shell.size(); i++) delete newTris[i];
+    for (unsigned int i = 0; i < shell.size(); i++) {delete newTris[i]->tri(), delete newTris[i];}
     delete [] newTris;
     //    throw;
     //    double t2 = Cpu();

commit 8047ecbaa8633c5f2e0723e0f70958d303a68e62

trying to fix compound surface meshing regression introduced by r18338

Mesh/meshGFace.cpp
@@ -1391,24 +1391,28 @@ bool meshGenerator(GFace *gf, int RECUR_ITER,
   }
   //gf->triangles.clear();
   //gf->quadrangles.clear();
-  gf->deleteMesh();
+
+  // only delete the mesh data stored in the base GFace class (calling
+  // gf->deleteMesh() would also destroy e.g. the data in a compound face, which
+  // we should not do)
+  gf->GFace::deleteMesh();
 
   Msg::Debug("Starting to add internal points");
   // start mesh generation
   if(!algoDelaunay2D(gf) && !onlyInitialMesh){
-       // if(CTX::instance()->mesh.recombineAll || gf->meshAttributes.recombine || 1) {
-       //   backgroundMesh::unset();
-       //   buildBackGroundMesh (gf);
-       // }
+    // if(CTX::instance()->mesh.recombineAll || gf->meshAttributes.recombine || 1) {
+    //   backgroundMesh::unset();
+    //   buildBackGroundMesh (gf);
+    // }
     refineMeshBDS(gf, *m, CTX::instance()->mesh.refineSteps, true,
                   &recoverMapInv);
     optimizeMeshBDS(gf, *m, 2);
     refineMeshBDS(gf, *m, CTX::instance()->mesh.refineSteps, false,
                 &recoverMapInv);
     optimizeMeshBDS(gf, *m, 2);
-       // if(CTX::instance()->mesh.recombineAll || gf->meshAttributes.recombine || 1) {
-       //   backgroundMesh::unset();
-       // }
+    // if(CTX::instance()->mesh.recombineAll || gf->meshAttributes.recombine || 1) {
+    //   backgroundMesh::unset();
+    // }
   }
 
   /*
@@ -1426,7 +1430,8 @@ bool meshGenerator(GFace *gf, int RECUR_ITER,
   BDS2GMSH(m, gf, recoverMap);
 
   bool infty = false;
-  if (gf->getMeshingAlgo() == ALGO_2D_FRONTAL_QUAD || gf->getMeshingAlgo() == ALGO_2D_PACK_PRLGRMS)
+  if (gf->getMeshingAlgo() == ALGO_2D_FRONTAL_QUAD ||
+      gf->getMeshingAlgo() == ALGO_2D_PACK_PRLGRMS)
     infty = true;
   if (!onlyInitialMesh) {
     if (infty)