7ced095b5d7f4616668a719836e358eb0b89f153
Christophe Geuzaine <cgeuzaine@ulg.ac.be>
2014-06-09 11:57:56 +0000

    simpler

--- utils/nightly/gmsh_mac_nightly.sh
+++ utils/nightly/gmsh_mac_nightly.sh
@@ -5,4 +5,3 @@
 #  * launchctl load ~/Library/LaunchAgents/org.geuz.gmsh.nightly.plist
 
 cd ${HOME}/src/gmsh/utils/nightly && /usr/local/bin/ctest -S gmsh_mac_nightly.ctest -VV > ${HOME}/src/gmsh/nightly.log 2>&1
-cd ${HOME}/src/gmsh/utils/nightly && /usr/local/bin/ctest -S gmsh_mac_dynamic_nightly.ctest -VV > ${HOME}/src/gmsh/nightly_dynamic.log 2>&1
--- utils/nightly/gmsh_win64_nightly.bat
+++ utils/nightly/gmsh_win64_nightly.bat
@@ -5,6 +5,4 @@ chdir C:\cygwin\bin
 
 bash --login -c "cd ${HOME}/src/gmsh/utils/nightly && ctest -S gmsh_win64_nightly.ctest -VV > ${HOME}/src/gmsh/nightly.log 2>&1"
 
-bash --login -c "cd ${HOME}/src/gmsh/utils/nightly && ctest -S gmsh_win64_dynamic_nightly.ctest -VV > ${HOME}/src/gmsh/nightly_dynamic.log 2>&1"
-
 bash --login -c "cd ${HOME}/src/gmsh/utils/nightly && /cygdrive/c/Program\ Files\ \(x86\)/CMake\ 2.8/bin/ctest.exe -S gmsh_msvc.ctest -VV > ${HOME}/src/gmsh/nightly_msvc.log 2>&1"
