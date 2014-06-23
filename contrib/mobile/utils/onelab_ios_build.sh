e311c2b2219bb69fca787fd5206b206076f4ddca
Maxime Graulich <maxime.graulich@gmail.com>
2014-06-23 16:57:05 +0000

    fix for older xcode

--- contrib/mobile/utils/onelab_ios_build.sh
+++ contrib/mobile/utils/onelab_ios_build.sh
@@ -10,6 +10,7 @@ slepc_framework="$frameworks_dir/slepc.framework"
 cmake_default="-DDEFAULT=0 -DCMAKE_TOOLCHAIN_FILE=$gmsh_svn/contrib/mobile/utils/iOS.cmake -DENABLE_BUILD_IOS=1 -DCMAKE_BUILD_TYPE=Release -GXcode"
 
 build_cmd="xcodebuild -verbose -target lib -configuration Release"
+headers_cmd="xcodebuild -verbose -target get_headers -configuration Release"
 
 function check {
   return_code=$?
@@ -32,6 +33,7 @@ function build_gmsh {
   check
   $build_cmd
   check
+  $headers_cmd
   cd -
 }
 function build_getdp {
@@ -45,10 +47,11 @@ function build_getdp {
   cd $getdp_svn/build_ios_$1
   export PETSC_DIR=
   export PETSC_ARCH=
-  cmake $cmake_default -DENABLE_BLAS_LAPACK=1 -DENABLE_BUILD_LIB=1 -DENABLE_GMSH=1 -DENABLE_LEGACY=1 -DENABLE_PETSC=1 -DPETSC_INC="$petsc_framework/Headers/" -DPETSC_LIBS="$petsc_framework/petsc" -DENABLE_SLEPC=1 -DSLEPC_INC="$slepc_framework/Headers/" -DSLEPC_LIB="$slepc_framework/slepc" -DGMSH_INC="$gmsh_svn/build_ios_$1/Gmsh.framework/Headers/" -DGMSH_LIB="$gmsh_svn/build_ios_$1/Gmsh.framework/Gmsh" -DCMAKE_OSX_ARCHITECTURES="$1" ..
+  cmake $cmake_default -DENABLE_BLAS_LAPACK=1 -DENABLE_BUILD_LIB=1 -DENABLE_GMSH=1 -DENABLE_LEGACY=1 -DENABLE_PETSC=1 -DPETSC_INC="$petsc_framework/Headers/" -DPETSC_LIBS="$petsc_framework/petsc" -DENABLE_SLEPC=1 -DSLEPC_INC="$slepc_framework/Headers/" -DSLEPC_LIB="$slepc_framework/slepc" -DGMSH_INC="$gmsh_svn/build_ios_$1/Headers/" -DGMSH_LIB="$gmsh_svn/build_ios_$1/Release-iphoneos/libGmsh.a" -DCMAKE_OSX_ARCHITECTURES="$1" ..
   check
   $build_cmd
   check
+  $headers_cmd
   cd -
 }
 
@@ -58,8 +61,10 @@ build_gmsh armv7
 build_gmsh armv7s
 build_gmsh arm64
 mkdir -p $frameworks_dir/Gmsh.framework/Headers
-lipo -create $gmsh_svn/build_ios_armv7/Gmsh.framework/Gmsh $gmsh_svn/build_ios_arm64/Gmsh.framework/Gmsh $gmsh_svn/build_ios_armv7s/Gmsh.framework/Gmsh -output $frameworks_dir/Gmsh.framework/Gmsh
-cp $gmsh_svn/build_ios_armv7/Gmsh.framework/Headers/* $frameworks_dir/Gmsh.framework/Headers
+lipo -create $gmsh_svn/build_ios_armv7/Release-iphoneos/libGmsh.a $gmsh_svn/build_ios_arm64/Release-iphoneos/libGmsh.a $gmsh_svn/build_ios_armv7s/Release-iphoneos/libGmsh.a -output $frameworks_dir/Gmsh.framework/Gmsh
+cd $frameworks_dir/Gmsh.framework/Headers
+cp $gmsh_svn/build_ios_armv7/Headers/gmsh/* .
+ln -s . gmsh
 
 # build getdp framework
 cd $getdp_svn && svn up
@@ -67,8 +72,9 @@ build_getdp armv7
 build_getdp armv7s
 build_getdp arm64
 mkdir -p $frameworks_dir/GetDP.framework/Headers
-lipo -create $getdp_svn/build_ios_armv7/GetDP.framework/GetDP $getdp_svn/build_ios_arm64/GetDP.framework/GetDP $getdp_svn/build_ios_armv7s/GetDP.framework/GetDP -output $frameworks_dir/GetDP.framework/GetDP
-cp $getdp_svn/build_ios_armv7/GetDP.framework/Headers/* $frameworks_dir/GetDP.framework/Headers
+lipo -create $getdp_svn/build_ios_armv7/Release-iphoneos/libGetDP.a $getdp_svn/build_ios_arm64/Release-iphoneos/libGetDP.a $getdp_svn/build_ios_armv7s/Release-iphoneos/libGetDP.a -output $frameworks_dir/GetDP.framework/GetDP
+cd $frameworks_dir/GetDP.framework/Headers
+cp $getdp_svn/build_ios_armv7/Headers/getdp/* .
 
 # create xcode project
 mkdir $gmsh_svn/contrib/mobile/build_ios
