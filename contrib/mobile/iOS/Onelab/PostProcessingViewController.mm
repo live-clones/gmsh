6b929c5dd8b3d13f050444e3c687604de6ed4138
Christophe Geuzaine <cgeuzaine@ulg.ac.be>
2014-06-25 14:16:18 +0000

    set min/max for raise

--- contrib/mobile/iOS/Onelab/PostProcessingViewController.mm
+++ contrib/mobile/iOS/Onelab/PostProcessingViewController.mm
@@ -1,5 +1,7 @@
 #import "PostProcessingViewController.h"
 
+#include <gmsh/Context.h>
+
 @interface PostProcessingViewController ()
 
 @end
@@ -30,7 +32,6 @@
     [_IntervalsType setDelegate:self];
     [_Intervals setText:[NSString stringWithFormat:@"%d",_pview->getOptions()->nbIso]];
     UIToolbar* numberToolbar = [[UIToolbar alloc]initWithFrame:CGRectMake(0, 0, 320, 50)];
-    numberToolbar.barStyle = UIBarStyleBlackTranslucent;
     numberToolbar.items = [NSArray arrayWithObjects:
                                      [[UIBarButtonItem alloc]initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:nil action:nil],
                                    [[UIBarButtonItem alloc]initWithTitle:@"Done" style:UIBarButtonItemStyleDone target:self action:@selector(doneWithNumberPad)],
@@ -39,6 +40,12 @@
     _Intervals.delegate = self;
     _Intervals.inputAccessoryView = numberToolbar;
     [_RaiseZ setValue:_pview->getOptions()->raise[2]];
+
+    double maxval = std::max(fabs(_pview->getData()->getMin()), fabs(_pview->getData()->getMax()));
+    if(!maxval) maxval = 1.;
+    double val2 = 2. * CTX::instance()->lc / maxval;
+    [_RaiseZ setMinimumValue:-val2];
+    [_RaiseZ setMaximumValue:val2];
     [_RaiseZ addTarget:self action:@selector(slideRaiseZ:) forControlEvents:UIControlEventValueChanged];
     [_IntervalsStepper setStepValue:1];
     [_IntervalsStepper setValue:_pview->getOptions()->nbIso];
