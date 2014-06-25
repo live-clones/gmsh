7e8e859e5897f4312df2271475c0e759d77502dd
Christophe Geuzaine <cgeuzaine@ulg.ac.be>
2014-06-25 11:56:34 +0000

    adjust spacing

--- contrib/mobile/Android/src/org/geuz/onelab/MainActivity.java
+++ contrib/mobile/Android/src/org/geuz/onelab/MainActivity.java
@@ -193,157 +193,157 @@ public class MainActivity extends Activity{
     	@Override
             protected void onPreExecute() {
             _compute = true;
-    		_runStopMenuItem.setTitle(R.string.menu_stop);
-    		super.onPreExecute();
+            _runStopMenuItem.setTitle(R.string.menu_stop);
+            super.onPreExecute();
     	}
 
-		@Override
-		protected Integer[] doInBackground(Void... params) {
-			_gmsh.onelabCB("compute");
-			return new Integer[] {1};
-		}
+        @Override
+            protected Integer[] doInBackground(Void... params) {
+            _gmsh.onelabCB("compute");
+            return new Integer[] {1};
+        }
 
-		@Override
-		protected void onPostExecute(Integer[] result) {
-			//(Vibrator) getSystemService(Context.VIBRATOR_SERVICE).vibrate(350);
-			_runStopMenuItem.setTitle(R.string.menu_run);
-			_runStopMenuItem.setEnabled(true);
-			if(_modelFragment != null) _modelFragment.hideProgress();
-			_compute = false;
-			if(_notify) notifyEndComputing();
-			super.onPostExecute(result);
-		}
+        @Override
+            protected void onPostExecute(Integer[] result) {
+            //(Vibrator) getSystemService(Context.VIBRATOR_SERVICE).vibrate(350);
+            _runStopMenuItem.setTitle(R.string.menu_run);
+            _runStopMenuItem.setEnabled(true);
+            if(_modelFragment != null) _modelFragment.hideProgress();
+            _compute = false;
+            if(_notify) notifyEndComputing();
+            super.onPostExecute(result);
+        }
 
     }
-	private void showError(){
+    private void showError(){
     	if(_errors.size()>0){
-    		if(_errorDialog != null && _errorDialog.isShowing()) _errorDialog.dismiss();
-    		AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
-    		dialogBuilder.setTitle("Gmsh/GetDP Error(s)")
-		    .setMessage(_errors.get(_errors.size()-1))
-		    .setNegativeButton("Hide", new DialogInterface.OnClickListener() {
+            if(_errorDialog != null && _errorDialog.isShowing()) _errorDialog.dismiss();
+            AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+            dialogBuilder.setTitle("Gmsh/GetDP Error(s)")
+                .setMessage(_errors.get(_errors.size()-1))
+                .setNegativeButton("Hide", new DialogInterface.OnClickListener() {
 		        public void onClick(DialogInterface dialog, int which) {
-		        	_errors.clear();
-		        	_errorDialog.dismiss();
+                            _errors.clear();
+                            _errorDialog.dismiss();
 		        }
-		     });
-    		if(_errors.size()>1)dialogBuilder.setPositiveButton("Show more", new DialogInterface.OnClickListener() {
-		        public void onClick(DialogInterface dialog, int which) {
-		        	_errors.remove(_errors.size()-1);
-		        	_errorDialog.dismiss();
-		            showError();
-		        }
-		     });
-		     _errorDialog = dialogBuilder.show();
+                    });
+            if(_errors.size()>1)dialogBuilder.setPositiveButton("Show more", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        _errors.remove(_errors.size()-1);
+                        _errorDialog.dismiss();
+                        showError();
+                    }
+                });
+            _errorDialog = dialogBuilder.show();
     	}
     }
-	@Override
+    @Override
 	protected void onPause() {
-		if(_compute) notifyComputing();
-		super.onPause();
-		_notify = true;
-	}
+        if(_compute) notifyComputing();
+        super.onPause();
+        _notify = true;
+    }
 
-	@Override
+    @Override
 	protected void onResume() {
-		super.onResume();
-		NotificationManager mNotificationManager =
-			    (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-		mNotificationManager.cancel(1337);
-		_notify = false;
-	}
+        super.onResume();
+        NotificationManager mNotificationManager =
+            (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        mNotificationManager.cancel(1337);
+        _notify = false;
+    }
 
-	@Override
+    @Override
 	protected void onStop() {
-		super.onStop();
-		if(_compute) notifyComputing();
-		_notify = true;
-	}
+        super.onStop();
+        if(_compute) notifyComputing();
+        _notify = true;
+    }
 
-	@Override
+    @Override
 	public void onLowMemory() {
-		if(!_compute) return;
-		_gmsh.onelabCB("stop");
-		Toast.makeText(this, "Low memory! Computation is going to stop", Toast.LENGTH_LONG).show();
-		super.onLowMemory();
-	}
+        if(!_compute) return;
+        _gmsh.onelabCB("stop");
+        Toast.makeText(this, "Low memory! Computation is going to stop", Toast.LENGTH_LONG).show();
+        super.onLowMemory();
+    }
 
-	@Override
+    @Override
 	public void onTrimMemory(int level) {
-		if(!_compute) return;
-		if(level == Activity.TRIM_MEMORY_COMPLETE){
-			_gmsh.onelabCB("stop");
-			notifyEndComputing("The computation had to stop because your device ran out of memory");
-			_notify = false;
-		}
-		else if(level == Activity.TRIM_MEMORY_MODERATE) {
-			notifyComputing("Computation in progress - low memory", true);
-		}
-		super.onTrimMemory(level);
-	}
+        if(!_compute) return;
+        if(level == Activity.TRIM_MEMORY_COMPLETE){
+            _gmsh.onelabCB("stop");
+            notifyEndComputing("The computation had to stop because your device ran out of memory");
+            _notify = false;
+        }
+        else if(level == Activity.TRIM_MEMORY_MODERATE) {
+            notifyComputing("Computation in progress - low memory", true);
+        }
+        super.onTrimMemory(level);
+    }
 
-	private void notifyComputing(String msg, boolean alert) {
-		Intent intent = new Intent(this, MainActivity.class);
-	    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_SINGLE_TOP);
-	    PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_CANCEL_CURRENT);
-	    Notification.Builder notifyBuilder = new Notification.Builder(this);
-		notifyBuilder.setContentTitle("ONELAB")
-			.setContentIntent(pendingIntent)
-		    .setContentText(msg)
-		    .setSmallIcon(R.drawable.ic_launcher)
-		    .setProgress(0, 0, true);
-		if(alert) notifyBuilder.setDefaults(Notification.DEFAULT_SOUND | Notification.DEFAULT_VIBRATE);
-		NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-		mNotificationManager.notify(1337, notifyBuilder.getNotification());
-	}
+    private void notifyComputing(String msg, boolean alert) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+        Notification.Builder notifyBuilder = new Notification.Builder(this);
+        notifyBuilder.setContentTitle("ONELAB")
+            .setContentIntent(pendingIntent)
+            .setContentText(msg)
+            .setSmallIcon(R.drawable.ic_launcher)
+            .setProgress(0, 0, true);
+        if(alert) notifyBuilder.setDefaults(Notification.DEFAULT_SOUND | Notification.DEFAULT_VIBRATE);
+        NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        mNotificationManager.notify(1337, notifyBuilder.getNotification());
+    }
 
-	private void notifyComputing() {
-		notifyComputing("Computation in progress", false);
-	}
+    private void notifyComputing() {
+        notifyComputing("Computation in progress", false);
+    }
 
-	private void notifyEndComputing() {
-		notifyEndComputing("Computation done!");
-	}
+    private void notifyEndComputing() {
+        notifyEndComputing("Computation done!");
+    }
 
-	private void notifyEndComputing(String msg) {
-		Intent intent = new Intent(this, MainActivity.class);
-	    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_SINGLE_TOP);
-	    PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_CANCEL_CURRENT);
-		Notification.Builder notifyBuilder = new Notification.Builder(this)
-		        .setSmallIcon(R.drawable.ic_launcher)
-		        .setContentIntent(pendingIntent)
-		        .setContentTitle("ONELAB")
-		        .setDefaults(Notification.DEFAULT_ALL)
-		        .setAutoCancel(true)
-		        .setProgress(0, 0, false)
-		        .setContentText(msg);
-		NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-		mNotificationManager.notify(1337, notifyBuilder.getNotification());
-	}
+    private void notifyEndComputing(String msg) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+        Notification.Builder notifyBuilder = new Notification.Builder(this)
+            .setSmallIcon(R.drawable.ic_launcher)
+            .setContentIntent(pendingIntent)
+            .setContentTitle("ONELAB")
+            .setDefaults(Notification.DEFAULT_ALL)
+            .setAutoCancel(true)
+            .setProgress(0, 0, false)
+            .setContentText(msg);
+        NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        mNotificationManager.notify(1337, notifyBuilder.getNotification());
+    }
 
-	private final Handler mainHandler = new Handler(){
-    	public void handleMessage(android.os.Message msg) {
+    private final Handler mainHandler = new Handler(){
+            public void handleMessage(android.os.Message msg) {
     		switch (msg.what) {
-			case 0: // we get a message from gmsh library
-				String message =(String) msg.obj;
-				_errors.add(message);
-				showError();
-				break;
-			case 1: // request render from gmsh library
-				if(_modelFragment != null) _modelFragment.requestRender();
-				if(_optionsFragment != null) _optionsFragment.refresh();
-				break;
-			case 2: // we get a message for loading
-				if(_modelFragment != null) _modelFragment.showProgress((String) msg.obj);
-				break;
-			case 3: // we get a progress for loading
-				//loading.setProgress(msg.arg1);
-				break;
-			default:
-				break;
-			}
-    	};
-    };
+                case 0: // we get a message from gmsh library
+                    String message =(String) msg.obj;
+                    _errors.add(message);
+                    showError();
+                    break;
+                case 1: // request render from gmsh library
+                    if(_modelFragment != null) _modelFragment.requestRender();
+                    if(_optionsFragment != null) _optionsFragment.refresh();
+                    break;
+                case 2: // we get a message for loading
+                    if(_modelFragment != null) _modelFragment.showProgress((String) msg.obj);
+                    break;
+                case 3: // we get a progress for loading
+                    //loading.setProgress(msg.arg1);
+                    break;
+                default:
+                    break;
+                }
+            };
+        };
 
     public boolean isComputing() {return _compute;}
 }
