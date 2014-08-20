d11073acedcf36a64d51846445f94b5adc537157
Gauthier Becker <gauthierbecker@gmail.com>
2014-08-20 09:42:40 +0000

    Try to avoid conflict between 2 different cdash drop site

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -3,11 +3,11 @@
 # See the LICENSE.txt file for license information. Please report all
 # bugs and problems to the public mailing list <gmsh@geuz.org>.
 
-set(CTEST_PROJECT_NAME "Gmsh")
+set(CTEST_PROJECT_NAME ${CUSTOM_DROP_PROJECT})
 set(CTEST_DROP_METHOD "http")
 set(CTEST_DROP_SITE_CDASH TRUE)
-set(CTEST_DROP_SITE "onelab.info")
-set(CTEST_DROP_LOCATION "/CDash/submit.php?project=Gmsh")
+set(CTEST_DROP_SITE ${CUSTOM_DROP_SITE})
+set(CTEST_DROP_LOCATION ${CUSTOM_DROP_LOCATION})
 set(CTEST_TRIGGER_SITE "")
 set(SITE ${GMSH_HOST})
 set(BUILDNAME "${GMSH_OS}-${GMSH_PACKAGER}")
