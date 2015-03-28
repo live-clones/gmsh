37e5e68f568ae3c721d398355a7d540d0eea8d7f
Christophe Geuzaine <cgeuzaine@ulg.ac.be>
2015-03-28 15:16:19 +0000

    package tuto + onelab models

--- utils/misc/package_gmsh_getdp.sh
+++ utils/misc/package_gmsh_getdp.sh
@@ -11,8 +11,8 @@ License. See the LICENSE and CREDITS files for more information.
 The Gmsh tutorial and sample GetDP models are provided in the tutorial and
 models directories.
 
-See the official websites http://gmsh.info and http://getdp.info for
-up-to-date versions, documentation and examples." > /tmp/README.txt
+See the official websites for up-to-date versions, documentation and additional
+examples." > /tmp/README.txt
 
 GMSH=svn
 GETDP=svn
@@ -26,12 +26,11 @@ MODELS='machines relay inductor indheat magnetometer antennas acoustic_scatterin
 mkdir /tmp/models
 for m in ${MODELS}; do
   curl -O http://onelab.info/files/${m}.zip
-  mkdir /tmp/models/$m
-  unzip -q -o ${m}.zip -d /tmp/models/$m
+  unzip -q -o ${m}.zip -d /tmp/models
   rm -f ${m}.zip
 done
 
-rm -rf gmsh-getdp-Windows64
+rm -rf gmsh-getdp-Windows64*
 mkdir gmsh-getdp-Windows64
 curl -O http://geuz.org/gmsh/bin/Windows/gmsh-${GMSH}-Windows64.zip
 curl -O http://geuz.org/getdp/bin/Windows/getdp-${GETDP}-Windows64c.zip
@@ -44,8 +43,10 @@ mv /tmp/gmsh-*${GMSH}-Windows/gmsh.exe gmsh-getdp-Windows64
 mv /tmp/gmsh-*${GMSH}-Windows/onelab.py gmsh-getdp-Windows64
 mv /tmp/getdp-*${GETDP}-Windows/getdp.exe gmsh-getdp-Windows64
 mv /tmp/gmsh-*${GMSH}-Windows/LICENSE.txt gmsh-getdp-Windows64/LICENSE.txt
+echo "\n\n" >> gmsh-getdp-Windows64/LICENSE.txt
 cat /tmp/getdp-*${GETDP}-Windows/LICENSE.txt >> gmsh-getdp-Windows64/LICENSE.txt
 mv /tmp/gmsh-*${GMSH}-Windows/CREDITS.txt gmsh-getdp-Windows64/CREDITS.txt
+echo "\n\n" >> gmsh-getdp-Windows64/CREDITS.txt
 cat /tmp/getdp-*${GETDP}-Windows/CREDITS.txt >> gmsh-getdp-Windows64/CREDITS.txt
 mv /tmp/gmsh-*${GMSH}-Windows/tutorial gmsh-getdp-Windows64
 cp -R /tmp/models gmsh-getdp-Windows64
@@ -54,7 +55,7 @@ rm -rf /tmp/getdp-*
 zip -r gmsh-getdp-Windows64.zip gmsh-getdp-Windows64
 rm -rf gmsh-getdp-Windows64
 
-rm -rf gmsh-getdp-Windows32
+rm -rf gmsh-getdp-Windows32*
 mkdir gmsh-getdp-Windows32
 curl -O http://geuz.org/gmsh/bin/Windows/gmsh-${GMSH}-Windows32.zip
 curl -O http://geuz.org/getdp/bin/Windows/getdp-${GETDP}-Windows32c.zip
@@ -67,8 +68,10 @@ mv /tmp/gmsh-*${GMSH}-Windows/gmsh.exe gmsh-getdp-Windows32
 mv /tmp/gmsh-*${GMSH}-Windows/onelab.py gmsh-getdp-Windows32
 mv /tmp/getdp-*${GETDP}-Windows/getdp.exe gmsh-getdp-Windows32
 mv /tmp/gmsh-*${GMSH}-Windows/LICENSE.txt gmsh-getdp-Windows32/LICENSE.txt
+echo "\n\n" >> gmsh-getdp-Windows32/LICENSE.txt
 cat /tmp/getdp-*${GETDP}-Windows/LICENSE.txt >> gmsh-getdp-Windows32/LICENSE.txt
 mv /tmp/gmsh-*${GMSH}-Windows/CREDITS.txt gmsh-getdp-Windows32/CREDITS.txt
+echo "\n\n" >> gmsh-getdp-Windows32/CREDITS.txt
 cat /tmp/getdp-*${GETDP}-Windows/CREDITS.txt >> gmsh-getdp-Windows32/CREDITS.txt
 mv /tmp/gmsh-*${GMSH}-Windows/tutorial gmsh-getdp-Windows32
 cp -R /tmp/models gmsh-getdp-Windows32
@@ -77,7 +80,7 @@ rm -rf /tmp/getdp-*
 zip -r gmsh-getdp-Windows32.zip gmsh-getdp-Windows32
 rm -rf gmsh-getdp-Windows32
 
-rm -rf gmsh-getdp-Linux32
+rm -rf gmsh-getdp-Linux32*
 mkdir gmsh-getdp-Linux32
 curl -O http://geuz.org/gmsh/bin/Linux/gmsh-${GMSH}-Linux32.tgz
 curl -O http://geuz.org/getdp/bin/Linux/getdp-${GETDP}-Linux32c.tgz
@@ -90,8 +93,10 @@ mv /tmp/gmsh-*${GMSH}-Linux/bin/gmsh gmsh-getdp-Linux32
 mv /tmp/gmsh-*${GMSH}-Linux/bin/onelab.py gmsh-getdp-Linux32
 mv /tmp/getdp-*${GETDP}-Linux/bin/getdp gmsh-getdp-Linux32
 mv /tmp/gmsh-*${GMSH}-Linux/share/doc/gmsh/LICENSE.txt gmsh-getdp-Linux32/LICENSE.txt
+echo "\n\n" >> gmsh-getdp-Linux32/LICENSE.txt
 cat /tmp/getdp-*${GETDP}-Linux/share/doc/getdp/LICENSE.txt >> gmsh-getdp-Linux32/LICENSE.txt
 mv /tmp/gmsh-*${GMSH}-Linux/share/doc/gmsh/CREDITS.txt gmsh-getdp-Linux32/CREDITS.txt
+echo "\n\n" >> gmsh-getdp-Linux32/CREDITS.txt
 cat /tmp/getdp-*${GETDP}-Linux/share/doc/getdp/CREDITS.txt >> gmsh-getdp-Linux32/CREDITS.txt
 mv /tmp/gmsh-*${GMSH}-Linux/share/doc/gmsh/tutorial gmsh-getdp-Linux32
 cp -R /tmp/models gmsh-getdp-Linux32
@@ -100,7 +105,7 @@ rm -rf /tmp/getdp-*
 zip -r gmsh-getdp-Linux32.zip gmsh-getdp-Linux32
 rm -rf gmsh-getdp-Linux32
 
-rm -rf gmsh-getdp-Linux64
+rm -rf gmsh-getdp-Linux64*
 mkdir gmsh-getdp-Linux64
 curl -O http://geuz.org/gmsh/bin/Linux/gmsh-${GMSH}-Linux64.tgz
 curl -O http://geuz.org/getdp/bin/Linux/getdp-${GETDP}-Linux64c.tgz
@@ -113,8 +118,10 @@ mv /tmp/gmsh-*${GMSH}-Linux/bin/gmsh gmsh-getdp-Linux64
 mv /tmp/gmsh-*${GMSH}-Linux/bin/onelab.py gmsh-getdp-Linux64
 mv /tmp/getdp-*${GETDP}-Linux/bin/getdp gmsh-getdp-Linux64
 mv /tmp/gmsh-*${GMSH}-Linux/share/doc/gmsh/LICENSE.txt gmsh-getdp-Linux64/LICENSE.txt
+echo "\n\n" >> gmsh-getdp-Linux64/LICENSE.txt
 cat /tmp/getdp-*${GETDP}-Linux/share/doc/getdp/LICENSE.txt >> gmsh-getdp-Linux64/LICENSE.txt
 mv /tmp/gmsh-*${GMSH}-Linux/share/doc/gmsh/CREDITS.txt gmsh-getdp-Linux64/CREDITS.txt
+echo "\n\n" >> gmsh-getdp-Linux64/CREDITS.txt
 cat /tmp/getdp-*${GETDP}-Linux/share/doc/getdp/CREDITS.txt >> gmsh-getdp-Linux64/CREDITS.txt
 mv /tmp/gmsh-*${GMSH}-Linux/share/doc/gmsh/tutorial gmsh-getdp-Linux64
 cp -R /tmp/models gmsh-getdp-Linux64
@@ -123,7 +130,7 @@ rm -rf /tmp/getdp-*
 zip -r gmsh-getdp-Linux64.zip gmsh-getdp-Linux64
 rm -rf gmsh-getdp-Linux64
 
-rm -rf gmsh-getdp-MacOSX
+rm -rf gmsh-getdp-MacOSX*
 mkdir gmsh-getdp-MacOSX
 curl -O http://geuz.org/gmsh/bin/MacOSX/gmsh-${GMSH}-MacOSX.dmg
 curl -O http://geuz.org/getdp/bin/MacOSX/getdp-${GETDP}-MacOSX64c.tgz
@@ -137,8 +144,10 @@ cp /tmp/README.txt gmsh-getdp-MacOSX
 cp -R gmsh_mount/Gmsh.app gmsh-getdp-MacOSX
 mv /tmp/getdp-*${GETDP}-MacOSX/bin/getdp gmsh-getdp-MacOSX/Gmsh.app/Contents/MacOS/
 cp gmsh_mount/LICENSE.txt gmsh-getdp-MacOSX/LICENSE.txt
+echo "\n\n" >> gmsh-getdp-MacOSX/LICENSE.txt
 cat /tmp/getdp-*${GETDP}-MacOSX/share/doc/getdp/LICENSE.txt >> gmsh-getdp-MacOSX/LICENSE.txt
 cp gmsh_mount/CREDITS.txt gmsh-getdp-MacOSX/CREDITS.txt
+echo "\n\n" >> gmsh-getdp-MacOSX/CREDITS.txt
 cat /tmp/getdp-*${GETDP}-MacOSX/share/doc/getdp/CREDITS.txt >> gmsh-getdp-MacOSX/CREDITS.txt
 cp -R gmsh_mount/tutorial gmsh-getdp-MacOSX
 cp -R /tmp/models gmsh-getdp-MacOSX
@@ -147,3 +156,5 @@ rm -rf /tmp/gmsh-*
 rm -rf /tmp/getdp-*
 zip -r gmsh-getdp-MacOSX.zip gmsh-getdp-MacOSX
 rm -rf gmsh-getdp-MacOSX
+
+rm -rf /tmp/models
