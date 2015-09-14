6da4fd6ac4d7ced71d24d5a519b822e5991f0c5a
Christophe Geuzaine <cgeuzaine@ulg.ac.be>
2015-09-14 20:25:00 +0000

    branding

--- contrib/mobile/utils/make_icon_ios.sh
+++ contrib/mobile/utils/make_icon_ios.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
+source=../../../utils/icons/gmsh_mobile_master.png
+#source=../../../utils/icons/bbemg-twitter.png
+
 mkdir -p ios.iconset
 # App icons (iPad)
-convert -scale 152 ../../../utils/icons/gmsh_mobile_master.png ios.iconset/icon_app_ipad_retina.png
-convert -scale 76 ../../../utils/icons/gmsh_mobile_master.png ios.iconset/icon_app_ipad.png
+convert -scale 152 ${source} ios.iconset/icon_app_ipad_retina.png
+convert -scale 76 ${source} ios.iconset/icon_app_ipad.png
 
 # App icon (iPhone)
-convert -scale 120 ../../../utils/icons/gmsh_mobile_master.png ios.iconset/icon_app_iphone_retina.png
+convert -scale 120 ${source} ios.iconset/icon_app_iphone_retina.png
