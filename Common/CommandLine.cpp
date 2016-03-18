deafe2f052aaabd5529d7e5063413626eb91b450
Christophe Geuzaine <cgeuzaine@ulg.ac.be>
2016-03-18 08:23:41 +0000

    oops

--- Common/CommandLine.cpp
+++ Common/CommandLine.cpp
@@ -839,7 +839,7 @@ void GetOptions(int argc, char *argv[])
             CTX::instance()->mesh.algo2d = ALGO_2D_BAMG;
           else if(!strncmp(argv[i], "gmsh3d", 6))
             CTX::instance()->mesh.algo3d = ALGO_3D_DELAUNAY_NEW;
-          else if(!strncmp(strncmp(argv[i], "del3d", 5) || argv[i], "tetgen", 6))
+          else if(!strncmp(argv[i], "del3d", 5) || !strncmp(argv[i], "tetgen", 6))
             CTX::instance()->mesh.algo3d = ALGO_3D_DELAUNAY;
           else if(!strncmp(argv[i], "front3d", 7) || !strncmp(argv[i], "netgen", 6))
             CTX::instance()->mesh.algo3d = ALGO_3D_FRONTAL;
