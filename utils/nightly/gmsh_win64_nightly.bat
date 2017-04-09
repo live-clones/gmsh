c1decb6334fa6051d5faedd72c83be00ce09f419
Christophe Geuzaine <cgeuzaine@ulg.ac.be>
2017-04-09 23:16:09 +0200

    update win64 build machine

--- utils/nightly/gmsh_win64_nightly.bat
+++ utils/nightly/gmsh_win64_nightly.bat
@@ -1,7 +1,7 @@
 @echo off
 
 C:
-chdir C:\cygwin\bin
+chdir C:\cygwin64\bin
 
 bash --login -c "cd ${HOME}/src/gmsh/utils/nightly && ctest -S gmsh_win64_nightly.ctest -VV > ${HOME}/src/gmsh/nightly.log 2>&1"
 
