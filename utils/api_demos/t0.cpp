aa65af638fec762f8454d5bdf7e23cb4880214a7
Christophe Geuzaine <cgeuzaine@ulg.ac.be>
2017-11-12 12:21:03 +0100

    up

--- utils/api_demos/t0.cpp
+++ utils/api_demos/t0.cpp
@@ -5,7 +5,7 @@ int main(int argc, char **argv)
   gmshInitialize(argc, argv);
   gmshOptionSetNumber("General.Terminal", 1);
   gmshOptionSetNumber("Mesh.Algorithm", 5);
-  gmshOpen("../../tutorial/t5.geo");
+  gmshOpen("test.geo");
   gmshModelMesh(3);
   gmshExport("test.msh");
   gmshExport("test.unv");
