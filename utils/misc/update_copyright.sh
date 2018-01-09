30b1abbe2d4b755765e0dd514fdcaea8dc8b542c
Christophe Geuzaine <cgeuzaine@ulg.ac.be>
2018-01-09 15:45:04 +0100

    pp

--- utils/misc/update_copyright.sh
+++ utils/misc/update_copyright.sh
@@ -4,7 +4,7 @@ files=`git grep -l '(C) 1997-2017 C. Geuzaine'`
 
 for file in $files; do
   echo "modifying $file"
-  sed -i '' -e "s/(C) 1997-2017 C. Geuzaine/(C) 1997-2018 C. Geuzaine/g" $file
+  sed -i '' -e "s/(C) 1997-2017 C\. Geuzaine/(C) 1997-2018 C\. Geuzaine/g" $file
 done
 
 # Need to manually change:
