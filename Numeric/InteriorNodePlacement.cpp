f7a3ced61fc22f81554b7b3a3a0f7ab158f03d4c
Amaury Johnen <amaury.johnen@uclouvain.be>
2018-02-15 17:39:21 +0100

    cleanup

--- Numeric/InteriorNodePlacement.cpp
+++ Numeric/InteriorNodePlacement.cpp
@@ -5,10 +5,15 @@
 #include "InteriorNodePlacement.h"
 #include "pointsGenerators.h"
 
-typedef std::pair<std::pair<int, int>, int> mytuple;
-#define MAKE_TUPLE(a,b,c) std::make_pair(std::make_pair(a,b),c)
 
 namespace {
+  typedef std::pair<std::pair<int, int>, int> mytuple;
+
+  mytuple make_mytuple(int a, int b, int c)
+  {
+    return std::make_pair(std::make_pair(a,b),c);
+  }
+  
   void double2int(const fullMatrix<double> &doubleMat,
                   fullMatrix<int> &intMat) {
     intMat.resize(doubleMat.size1(), doubleMat.size2(), false);
@@ -19,6 +24,7 @@ namespace {
     }
   }
 }
+
 
 fullMatrix<double> gmshGenerateInteriorNodePlacementTriangle(int order)
 {
@@ -154,7 +160,7 @@ fullMatrix<double> gmshGenerateInteriorNodePlacementTetrahedron(int order)
     int u = coordinates(i, 0);
     int v = coordinates(i, 1);
     int w = coordinates(i, 2);
-    coord2idx[MAKE_TUPLE(u, v, w)] = i;
+    coord2idx[make_mytuple(u, v, w)] = i;
   }
 
   int &n = order;
@@ -168,36 +174,36 @@ fullMatrix<double> gmshGenerateInteriorNodePlacementTetrahedron(int order)
     double zeta = (double)w / n;
     double rho = (double)q / n;
 
-    M(i, coord2idx[MAKE_TUPLE(u+v, 0, w)]) += xi;
-    M(i, coord2idx[MAKE_TUPLE(u+w, v, 0)]) += xi;
-    M(i, coord2idx[MAKE_TUPLE(u+q, v, w)]) += xi;
-    M(i, coord2idx[MAKE_TUPLE(0, v+u, w)]) += eta;
-    M(i, coord2idx[MAKE_TUPLE(u, v+w, 0)]) += eta;
-    M(i, coord2idx[MAKE_TUPLE(u, v+q, w)]) += eta;
-    M(i, coord2idx[MAKE_TUPLE(0, v, w+u)]) += zeta;
-    M(i, coord2idx[MAKE_TUPLE(u, 0, w+v)]) += zeta;
-    M(i, coord2idx[MAKE_TUPLE(u, v, w+q)]) += zeta;
-    M(i, coord2idx[MAKE_TUPLE(0, v, w)]) += rho;
-    M(i, coord2idx[MAKE_TUPLE(u, 0, w)]) += rho;
-    M(i, coord2idx[MAKE_TUPLE(u, v, 0)]) += rho;
+    M(i, coord2idx[make_mytuple(u+v, 0, w)]) += xi;
+    M(i, coord2idx[make_mytuple(u+w, v, 0)]) += xi;
+    M(i, coord2idx[make_mytuple(u+q, v, w)]) += xi;
+    M(i, coord2idx[make_mytuple(0, v+u, w)]) += eta;
+    M(i, coord2idx[make_mytuple(u, v+w, 0)]) += eta;
+    M(i, coord2idx[make_mytuple(u, v+q, w)]) += eta;
+    M(i, coord2idx[make_mytuple(0, v, w+u)]) += zeta;
+    M(i, coord2idx[make_mytuple(u, 0, w+v)]) += zeta;
+    M(i, coord2idx[make_mytuple(u, v, w+q)]) += zeta;
+    M(i, coord2idx[make_mytuple(0, v, w)]) += rho;
+    M(i, coord2idx[make_mytuple(u, 0, w)]) += rho;
+    M(i, coord2idx[make_mytuple(u, v, 0)]) += rho;
 
-    M(i, coord2idx[MAKE_TUPLE(n-q, 0, 0)]) -= xi;
-    M(i, coord2idx[MAKE_TUPLE(n-w, 0, w)]) -= xi;
-    M(i, coord2idx[MAKE_TUPLE(n-v, v, 0)]) -= xi;
-    M(i, coord2idx[MAKE_TUPLE(0, n-q, 0)]) -= eta;
-    M(i, coord2idx[MAKE_TUPLE(0, n-w, w)]) -= eta;
-    M(i, coord2idx[MAKE_TUPLE(u, n-u, 0)]) -= eta;
-    M(i, coord2idx[MAKE_TUPLE(0, 0, n-q)]) -= zeta;
-    M(i, coord2idx[MAKE_TUPLE(0, v, n-v)]) -= zeta;
-    M(i, coord2idx[MAKE_TUPLE(u, 0, n-u)]) -= zeta;
-    M(i, coord2idx[MAKE_TUPLE(0, 0, w)]) -= rho;
-    M(i, coord2idx[MAKE_TUPLE(0, v, 0)]) -= rho;
-    M(i, coord2idx[MAKE_TUPLE(u, 0, 0)]) -= rho;
+    M(i, coord2idx[make_mytuple(n-q, 0, 0)]) -= xi;
+    M(i, coord2idx[make_mytuple(n-w, 0, w)]) -= xi;
+    M(i, coord2idx[make_mytuple(n-v, v, 0)]) -= xi;
+    M(i, coord2idx[make_mytuple(0, n-q, 0)]) -= eta;
+    M(i, coord2idx[make_mytuple(0, n-w, w)]) -= eta;
+    M(i, coord2idx[make_mytuple(u, n-u, 0)]) -= eta;
+    M(i, coord2idx[make_mytuple(0, 0, n-q)]) -= zeta;
+    M(i, coord2idx[make_mytuple(0, v, n-v)]) -= zeta;
+    M(i, coord2idx[make_mytuple(u, 0, n-u)]) -= zeta;
+    M(i, coord2idx[make_mytuple(0, 0, w)]) -= rho;
+    M(i, coord2idx[make_mytuple(0, v, 0)]) -= rho;
+    M(i, coord2idx[make_mytuple(u, 0, 0)]) -= rho;
 
-    M(i, coord2idx[MAKE_TUPLE(n, 0, 0)]) += xi;
-    M(i, coord2idx[MAKE_TUPLE(0, n, 0)]) += eta;
-    M(i, coord2idx[MAKE_TUPLE(0, 0, n)]) += zeta;
-    M(i, coord2idx[MAKE_TUPLE(0, 0, 0)]) += rho;
+    M(i, coord2idx[make_mytuple(n, 0, 0)]) += xi;
+    M(i, coord2idx[make_mytuple(0, n, 0)]) += eta;
+    M(i, coord2idx[make_mytuple(0, 0, n)]) += zeta;
+    M(i, coord2idx[make_mytuple(0, 0, 0)]) += rho;
   }
 
   M.print("Mtet");
@@ -235,7 +241,7 @@ fullMatrix<double> gmshGenerateInteriorNodePlacementHexahedron(int order)
     int u = coordinates(i, 0);
     int v = coordinates(i, 1);
     int w = coordinates(i, 2);
-    coord2idx[MAKE_TUPLE(u, v, w)] = i;
+    coord2idx[make_mytuple(u, v, w)] = i;
   }
 
   int &n = order;
@@ -247,34 +253,34 @@ fullMatrix<double> gmshGenerateInteriorNodePlacementHexahedron(int order)
     double eta = (double)v / n;
     double zeta = (double)w / n;
 
-    M(i, coord2idx[MAKE_TUPLE(0, v, w)]) += 1-xi;
-    M(i, coord2idx[MAKE_TUPLE(n, v, w)]) += xi;
-    M(i, coord2idx[MAKE_TUPLE(u, 0, w)]) += 1-eta;
-    M(i, coord2idx[MAKE_TUPLE(u, n, w)]) += eta;
-    M(i, coord2idx[MAKE_TUPLE(u, v, 0)]) += 1-zeta;
-    M(i, coord2idx[MAKE_TUPLE(u, v, n)]) += zeta;
+    M(i, coord2idx[make_mytuple(0, v, w)]) += 1-xi;
+    M(i, coord2idx[make_mytuple(n, v, w)]) += xi;
+    M(i, coord2idx[make_mytuple(u, 0, w)]) += 1-eta;
+    M(i, coord2idx[make_mytuple(u, n, w)]) += eta;
+    M(i, coord2idx[make_mytuple(u, v, 0)]) += 1-zeta;
+    M(i, coord2idx[make_mytuple(u, v, n)]) += zeta;
 
-    M(i, coord2idx[MAKE_TUPLE(0, 0, w)]) -= (1-xi) * (1-eta);
-    M(i, coord2idx[MAKE_TUPLE(0, n, w)]) -= (1-xi) * eta;
-    M(i, coord2idx[MAKE_TUPLE(n, 0, w)]) -= xi * (1-eta);
-    M(i, coord2idx[MAKE_TUPLE(n, n, w)]) -= xi * eta;
-    M(i, coord2idx[MAKE_TUPLE(u, 0, 0)]) -= (1-eta) * (1-zeta);
-    M(i, coord2idx[MAKE_TUPLE(u, 0, n)]) -= (1-eta) * zeta;
-    M(i, coord2idx[MAKE_TUPLE(u, n, 0)]) -= eta * (1-zeta);
-    M(i, coord2idx[MAKE_TUPLE(u, n, n)]) -= eta * zeta;
-    M(i, coord2idx[MAKE_TUPLE(0, v, 0)]) -= (1-zeta) * (1-xi);
-    M(i, coord2idx[MAKE_TUPLE(n, v, 0)]) -= (1-zeta) * xi;
-    M(i, coord2idx[MAKE_TUPLE(0, v, n)]) -= zeta * (1-xi);
-    M(i, coord2idx[MAKE_TUPLE(n, v, n)]) -= zeta * xi;
+    M(i, coord2idx[make_mytuple(0, 0, w)]) -= (1-xi) * (1-eta);
+    M(i, coord2idx[make_mytuple(0, n, w)]) -= (1-xi) * eta;
+    M(i, coord2idx[make_mytuple(n, 0, w)]) -= xi * (1-eta);
+    M(i, coord2idx[make_mytuple(n, n, w)]) -= xi * eta;
+    M(i, coord2idx[make_mytuple(u, 0, 0)]) -= (1-eta) * (1-zeta);
+    M(i, coord2idx[make_mytuple(u, 0, n)]) -= (1-eta) * zeta;
+    M(i, coord2idx[make_mytuple(u, n, 0)]) -= eta * (1-zeta);
+    M(i, coord2idx[make_mytuple(u, n, n)]) -= eta * zeta;
+    M(i, coord2idx[make_mytuple(0, v, 0)]) -= (1-zeta) * (1-xi);
+    M(i, coord2idx[make_mytuple(n, v, 0)]) -= (1-zeta) * xi;
+    M(i, coord2idx[make_mytuple(0, v, n)]) -= zeta * (1-xi);
+    M(i, coord2idx[make_mytuple(n, v, n)]) -= zeta * xi;
 
-    M(i, coord2idx[MAKE_TUPLE(0, 0, 0)]) += (1-xi) * (1-eta) * (1-zeta);
-    M(i, coord2idx[MAKE_TUPLE(0, 0, n)]) += (1-xi) * (1-eta) * zeta;
-    M(i, coord2idx[MAKE_TUPLE(0, n, 0)]) += (1-xi) * eta * (1-zeta);
-    M(i, coord2idx[MAKE_TUPLE(0, n, n)]) += (1-xi) * eta * zeta;
-    M(i, coord2idx[MAKE_TUPLE(n, 0, 0)]) += xi * (1-eta) * (1-zeta);
-    M(i, coord2idx[MAKE_TUPLE(n, 0, n)]) += xi * (1-eta) * zeta;
-    M(i, coord2idx[MAKE_TUPLE(n, n, 0)]) += xi * eta * (1-zeta);
-    M(i, coord2idx[MAKE_TUPLE(n, n, n)]) += xi * eta * zeta;
+    M(i, coord2idx[make_mytuple(0, 0, 0)]) += (1-xi) * (1-eta) * (1-zeta);
+    M(i, coord2idx[make_mytuple(0, 0, n)]) += (1-xi) * (1-eta) * zeta;
+    M(i, coord2idx[make_mytuple(0, n, 0)]) += (1-xi) * eta * (1-zeta);
+    M(i, coord2idx[make_mytuple(0, n, n)]) += (1-xi) * eta * zeta;
+    M(i, coord2idx[make_mytuple(n, 0, 0)]) += xi * (1-eta) * (1-zeta);
+    M(i, coord2idx[make_mytuple(n, 0, n)]) += xi * (1-eta) * zeta;
+    M(i, coord2idx[make_mytuple(n, n, 0)]) += xi * eta * (1-zeta);
+    M(i, coord2idx[make_mytuple(n, n, n)]) += xi * eta * zeta;
   }
 
   M.print("Mhex");
@@ -313,7 +319,7 @@ fullMatrix<double> gmshGenerateInteriorNodePlacementPrism(int order)
     int u = coordinates(i, 0);
     int v = coordinates(i, 1);
     int w = coordinates(i, 2);
-    coord2idx[MAKE_TUPLE(u, v, w)] = i;
+    coord2idx[make_mytuple(u, v, w)] = i;
   }
 
   int &n = order;
@@ -327,37 +333,37 @@ fullMatrix<double> gmshGenerateInteriorNodePlacementPrism(int order)
     double zeta = (double)w / n;
     double rho = (double)q / n;
 
-    M(i, coord2idx[MAKE_TUPLE(u+v, 0, w)]) += xi;
-    M(i, coord2idx[MAKE_TUPLE(u+q, v, w)]) += xi;
-    M(i, coord2idx[MAKE_TUPLE(0, v+u, w)]) += eta;
-    M(i, coord2idx[MAKE_TUPLE(u, v+q, w)]) += eta;
-    M(i, coord2idx[MAKE_TUPLE(0, v, w)]) += rho;
-    M(i, coord2idx[MAKE_TUPLE(u, 0, w)]) += rho;
-    M(i, coord2idx[MAKE_TUPLE(u, v, 0)]) += 1-zeta;
-    M(i, coord2idx[MAKE_TUPLE(u, v, n)]) += zeta;
+    M(i, coord2idx[make_mytuple(u+v, 0, w)]) += xi;
+    M(i, coord2idx[make_mytuple(u+q, v, w)]) += xi;
+    M(i, coord2idx[make_mytuple(0, v+u, w)]) += eta;
+    M(i, coord2idx[make_mytuple(u, v+q, w)]) += eta;
+    M(i, coord2idx[make_mytuple(0, v, w)]) += rho;
+    M(i, coord2idx[make_mytuple(u, 0, w)]) += rho;
+    M(i, coord2idx[make_mytuple(u, v, 0)]) += 1-zeta;
+    M(i, coord2idx[make_mytuple(u, v, n)]) += zeta;
 
-    M(i, coord2idx[MAKE_TUPLE(n, 0, w)]) -= xi;
-    M(i, coord2idx[MAKE_TUPLE(0, n, w)]) -= eta;
-    M(i, coord2idx[MAKE_TUPLE(0, 0, w)]) -= rho;
-    M(i, coord2idx[MAKE_TUPLE(u+v, 0, 0)]) -= xi * (1-zeta);
-    M(i, coord2idx[MAKE_TUPLE(u+q, v, 0)]) -= xi * (1-zeta);
-    M(i, coord2idx[MAKE_TUPLE(0, v+u, 0)]) -= eta * (1-zeta);
-    M(i, coord2idx[MAKE_TUPLE(u, v+q, 0)]) -= eta * (1-zeta);
-    M(i, coord2idx[MAKE_TUPLE(0, v, 0)]) -= rho * (1-zeta);
-    M(i, coord2idx[MAKE_TUPLE(u, 0, 0)]) -= rho * (1-zeta);
-    M(i, coord2idx[MAKE_TUPLE(u+v, 0, n)]) -= xi * zeta;
-    M(i, coord2idx[MAKE_TUPLE(u+q, v, n)]) -= xi * zeta;
-    M(i, coord2idx[MAKE_TUPLE(0, v+u, n)]) -= eta * zeta;
-    M(i, coord2idx[MAKE_TUPLE(u, v+q, n)]) -= eta * zeta;
-    M(i, coord2idx[MAKE_TUPLE(0, v, n)]) -= rho * zeta;
-    M(i, coord2idx[MAKE_TUPLE(u, 0, n)]) -= rho * zeta;
+    M(i, coord2idx[make_mytuple(n, 0, w)]) -= xi;
+    M(i, coord2idx[make_mytuple(0, n, w)]) -= eta;
+    M(i, coord2idx[make_mytuple(0, 0, w)]) -= rho;
+    M(i, coord2idx[make_mytuple(u+v, 0, 0)]) -= xi * (1-zeta);
+    M(i, coord2idx[make_mytuple(u+q, v, 0)]) -= xi * (1-zeta);
+    M(i, coord2idx[make_mytuple(0, v+u, 0)]) -= eta * (1-zeta);
+    M(i, coord2idx[make_mytuple(u, v+q, 0)]) -= eta * (1-zeta);
+    M(i, coord2idx[make_mytuple(0, v, 0)]) -= rho * (1-zeta);
+    M(i, coord2idx[make_mytuple(u, 0, 0)]) -= rho * (1-zeta);
+    M(i, coord2idx[make_mytuple(u+v, 0, n)]) -= xi * zeta;
+    M(i, coord2idx[make_mytuple(u+q, v, n)]) -= xi * zeta;
+    M(i, coord2idx[make_mytuple(0, v+u, n)]) -= eta * zeta;
+    M(i, coord2idx[make_mytuple(u, v+q, n)]) -= eta * zeta;
+    M(i, coord2idx[make_mytuple(0, v, n)]) -= rho * zeta;
+    M(i, coord2idx[make_mytuple(u, 0, n)]) -= rho * zeta;
 
-    M(i, coord2idx[MAKE_TUPLE(n, 0, 0)]) += xi * (1-zeta);
-    M(i, coord2idx[MAKE_TUPLE(0, n, 0)]) += eta * (1-zeta);
-    M(i, coord2idx[MAKE_TUPLE(0, 0, 0)]) += rho * (1-zeta);
-    M(i, coord2idx[MAKE_TUPLE(n, 0, n)]) += xi * zeta;
-    M(i, coord2idx[MAKE_TUPLE(0, n, n)]) += eta * zeta;
-    M(i, coord2idx[MAKE_TUPLE(0, 0, n)]) += rho * zeta;
+    M(i, coord2idx[make_mytuple(n, 0, 0)]) += xi * (1-zeta);
+    M(i, coord2idx[make_mytuple(0, n, 0)]) += eta * (1-zeta);
+    M(i, coord2idx[make_mytuple(0, 0, 0)]) += rho * (1-zeta);
+    M(i, coord2idx[make_mytuple(n, 0, n)]) += xi * zeta;
+    M(i, coord2idx[make_mytuple(0, n, n)]) += eta * zeta;
+    M(i, coord2idx[make_mytuple(0, 0, n)]) += rho * zeta;
   }
 
   M.print("Mprism");
@@ -396,7 +402,7 @@ fullMatrix<double> gmshGenerateInteriorNodePlacementPyramid(int order)
     int u = coordinates(i, 0);
     int v = coordinates(i, 1);
     int w = coordinates(i, 2);
-    coord2idx[MAKE_TUPLE(u, v, w)] = i;
+    coord2idx[make_mytuple(u, v, w)] = i;
   }
 
   int &n = order;
@@ -415,34 +421,34 @@ fullMatrix<double> gmshGenerateInteriorNodePlacementPyramid(int order)
     double etap = (double)v / (n-w);
 
     // lateral faces
-    M(i, coord2idx[MAKE_TUPLE(0, v, w)]) += 1-xip;
-    M(i, coord2idx[MAKE_TUPLE(u, 0, w)]) += 1-etap;
-    M(i, coord2idx[MAKE_TUPLE(n-w, v, w)]) += xip;
-    M(i, coord2idx[MAKE_TUPLE(u, n-w, w)]) += etap;
+    M(i, coord2idx[make_mytuple(0, v, w)]) += 1-xip;
+    M(i, coord2idx[make_mytuple(u, 0, w)]) += 1-etap;
+    M(i, coord2idx[make_mytuple(n-w, v, w)]) += xip;
+    M(i, coord2idx[make_mytuple(u, n-w, w)]) += etap;
     // basis face
-    M(i, coord2idx[MAKE_TUPLE(u, v, 0)]) += rho * tau;
-    M(i, coord2idx[MAKE_TUPLE(u+w, v, 0)]) += xi * tau;
-    M(i, coord2idx[MAKE_TUPLE(u+w, v+w, 0)]) += xi * eta;
-    M(i, coord2idx[MAKE_TUPLE(u, v+w, 0)]) += rho * eta;
+    M(i, coord2idx[make_mytuple(u, v, 0)]) += rho * tau;
+    M(i, coord2idx[make_mytuple(u+w, v, 0)]) += xi * tau;
+    M(i, coord2idx[make_mytuple(u+w, v+w, 0)]) += xi * eta;
+    M(i, coord2idx[make_mytuple(u, v+w, 0)]) += rho * eta;
     // vertical edges
-    M(i, coord2idx[MAKE_TUPLE(0, 0, w)]) -= (1-xip) * (1-etap);
-    M(i, coord2idx[MAKE_TUPLE(n-w, 0, w)]) -= xip * (1-etap);
-    M(i, coord2idx[MAKE_TUPLE(n-w, n-w, w)]) -= xip * etap;
-    M(i, coord2idx[MAKE_TUPLE(0, n-w, w)]) -= (1-xip) * etap;
+    M(i, coord2idx[make_mytuple(0, 0, w)]) -= (1-xip) * (1-etap);
+    M(i, coord2idx[make_mytuple(n-w, 0, w)]) -= xip * (1-etap);
+    M(i, coord2idx[make_mytuple(n-w, n-w, w)]) -= xip * etap;
+    M(i, coord2idx[make_mytuple(0, n-w, w)]) -= (1-xip) * etap;
     // basis edges
-    M(i, coord2idx[MAKE_TUPLE(0, v, 0)]) -= rho * tau;
-    M(i, coord2idx[MAKE_TUPLE(u, 0, 0)]) -= rho * tau;
-    M(i, coord2idx[MAKE_TUPLE(u+w, 0, 0)]) -= xi * tau;
-    M(i, coord2idx[MAKE_TUPLE(n, v, 0)]) -= xi * tau;
-    M(i, coord2idx[MAKE_TUPLE(n, v+w, 0)]) -= xi * eta;
-    M(i, coord2idx[MAKE_TUPLE(u+w, n, 0)]) -= xi * eta;
-    M(i, coord2idx[MAKE_TUPLE(u, n, 0)]) -= rho * eta;
-    M(i, coord2idx[MAKE_TUPLE(0, v+w, 0)]) -= rho * eta;
+    M(i, coord2idx[make_mytuple(0, v, 0)]) -= rho * tau;
+    M(i, coord2idx[make_mytuple(u, 0, 0)]) -= rho * tau;
+    M(i, coord2idx[make_mytuple(u+w, 0, 0)]) -= xi * tau;
+    M(i, coord2idx[make_mytuple(n, v, 0)]) -= xi * tau;
+    M(i, coord2idx[make_mytuple(n, v+w, 0)]) -= xi * eta;
+    M(i, coord2idx[make_mytuple(u+w, n, 0)]) -= xi * eta;
+    M(i, coord2idx[make_mytuple(u, n, 0)]) -= rho * eta;
+    M(i, coord2idx[make_mytuple(0, v+w, 0)]) -= rho * eta;
     // basis corners
-    M(i, coord2idx[MAKE_TUPLE(0, 0, 0)]) += rho * tau;
-    M(i, coord2idx[MAKE_TUPLE(n, 0, 0)]) += xi * tau;
-    M(i, coord2idx[MAKE_TUPLE(n, n, 0)]) += xi * eta;
-    M(i, coord2idx[MAKE_TUPLE(0, n, 0)]) += rho * eta;
+    M(i, coord2idx[make_mytuple(0, 0, 0)]) += rho * tau;
+    M(i, coord2idx[make_mytuple(n, 0, 0)]) += xi * tau;
+    M(i, coord2idx[make_mytuple(n, n, 0)]) += xi * eta;
+    M(i, coord2idx[make_mytuple(0, n, 0)]) += rho * eta;
   }
 
   M.print("Mpyr");
@@ -457,4 +463,4 @@ fullMatrix<double> gmshGenerateInteriorNodePlacementPyramid(int order)
   return M;
 }
 
-#undef MAKE_TUPLE
+#undef make_mytuple
