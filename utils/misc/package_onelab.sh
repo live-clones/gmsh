07d86b2b9ed05beb796fcc25223e4570c965189d
Christophe Geuzaine <cgeuzaine@ulg.ac.be>
2018-03-12 21:40:52 +0100

    up

--- utils/misc/package_onelab.sh
+++ utils/misc/package_onelab.sh
@@ -11,9 +11,8 @@ with the File/Open menu, then click on \"Run\".
 The software is distributed under the terms of the GNU General Public License. 
 See the LICENSE and CREDITS files for more information. 
 
-The 'models' directory contains a selection of ready-to-use examples from
-http://onelab.info. The 'template' directory contains GetDP templates. The
-'tutorial' directory contains the Gmsh tutorial.
+The 'tutorials' directory contains ONELAB tutorials. The 'models' directory contains
+a selection of other ready-to-use models from http://onelab.info.
 
 See http://onelab.info, http://gmsh.info and http://getdp.info for additional
 examples, up-to-date versions and documentation." > /tmp/README.txt
@@ -24,13 +23,39 @@ GETDP=git
 #GMSH=3.0.0
 #GETDP=2.11.0
 
-MODELS='machines relay inductor indheat magnetometer antennas acoustic_scattering time_reversal shielding waveguides transfo_simple ddm_waves bloch_periodic_waveguides magnets thermal_conduction magnetostriction superconductors'
+TUTO_GETDP='Electrostatics Magnetostatics Elasticity ElectrostaticsFloating Thermics PotentialFlow'
+TUTO_CPP='PendulumC++'
+TUTO_PYTHON='PendulumPython'
+MODELS='AcousticScattering Antennas BlochPeriodicWaveguides Cohomology DDMWaves DiffractionGratings ElectricMachines ElectromechanicalRelay Inductor Magnetometer Magnetostriction Magnets Shielding Superconductors ThermalConduction TimeReversal Waveguides'
 
-# get onelab models
+# get models and tutorials
+rm -rf /tmp/models
 mkdir /tmp/models
 for m in ${MODELS}; do
   curl -O http://onelab.info/files/${m}.zip
   unzip -q -o ${m}.zip -d /tmp/models
+  rm -f ${m}.zip
+done
+
+rm -rf /tmp/tutorials
+mkdir /tmp/tutorials
+mkdir /tmp/tutorials/gmsh
+mkdir /tmp/tutorials/getdp
+for m in ${TUTO_GETDP}; do
+  curl -O http://onelab.info/files/${m}.zip
+  unzip -q -o ${m}.zip -d /tmp/tutorials/getdp
+  rm -f ${m}.zip
+done
+mkdir /tmp/tutorials/python
+for m in ${TUTO_PYTHON}; do
+  curl -O http://onelab.info/files/${m}.zip
+  unzip -q -o ${m}.zip -d /tmp/tutorials/python
+  rm -f ${m}.zip
+done
+mkdir /tmp/tutorials/c++
+for m in ${TUTO_CPP}; do
+  curl -O http://onelab.info/files/${m}.zip
+  unzip -q -o ${m}.zip -d /tmp/tutorials/c++
   rm -f ${m}.zip
 done
 
@@ -52,7 +77,9 @@ if [ $# -lt 1 ] || [ $1 == "source" ]; then
   cp onelab-source/gmsh-*${GMSH}*-source/CREDITS.txt onelab-source/CREDITS.txt
   echo "\n\n" >> onelab-source/CREDITS.txt
   cat onelab-source/getdp-*${GETDP}*-source/CREDITS.txt >> onelab-source/CREDITS.txt
-  cp -R /tmp/models onelab-source
+  cp -R /tmp/models onelab-source/
+  cp -R /tmp/tutorials onelab-source/
+  cp -R onelab-source/gmsh-*${GMSH}*-source/tutorial/*.* onelab-source/tutorials/gmsh/
   rm -rf /tmp/gmsh-*
   rm -rf /tmp/getdp-*
   zip -r onelab-source.zip onelab-source
@@ -79,9 +106,9 @@ if [ $# -lt 1 ] || [ $1 == "win64" ]; then
   mv /tmp/gmsh-*${GMSH}-Windows64/CREDITS.txt onelab-Windows64/CREDITS.txt
   echo "\n\n" >> onelab-Windows64/CREDITS.txt
   cat /tmp/getdp-*${GETDP}-Windows64/CREDITS.txt >> onelab-Windows64/CREDITS.txt
-  mv /tmp/getdp-*${GETDP}-Windows64/templates onelab-Windows64
-  mv /tmp/gmsh-*${GMSH}-Windows64/tutorial onelab-Windows64
-  cp -R /tmp/models onelab-Windows64
+  cp -R /tmp/models onelab-Windows64/
+  cp -R /tmp/tutorials onelab-Windows64/
+  cp -R /tmp/gmsh-*${GMSH}-Windows64/tutorial/*.* onelab-Windows64/tutorials/gmsh/
   rm -rf /tmp/gmsh-*
   rm -rf /tmp/getdp-*
   zip -r onelab-Windows64.zip onelab-Windows64
@@ -108,9 +135,9 @@ if [ $# -lt 1 ] || [ $1 == "win32" ]; then
   mv /tmp/gmsh-*${GMSH}-Windows32/CREDITS.txt onelab-Windows32/CREDITS.txt
   echo "\n\n" >> onelab-Windows32/CREDITS.txt
   cat /tmp/getdp-*${GETDP}-Windows32/CREDITS.txt >> onelab-Windows32/CREDITS.txt
-  mv /tmp/getdp-*${GETDP}-Windows32/templates onelab-Windows32
-  mv /tmp/gmsh-*${GMSH}-Windows32/tutorial onelab-Windows32
-  cp -R /tmp/models onelab-Windows32
+  cp -R /tmp/models onelab-Windows32/
+  cp -R /tmp/tutorials onelab-Windows32/
+  cp -R /tmp/gmsh-*${GMSH}-Windows32/tutorial/*.* onelab-Windows32/tutorials/gmsh/
   rm -rf /tmp/gmsh-*
   rm -rf /tmp/getdp-*
   zip -r onelab-Windows32.zip onelab-Windows32
@@ -136,9 +163,9 @@ if [ $# -lt 1 ] || [ $1 == "linux64" ]; then
   mv /tmp/gmsh-*${GMSH}-Linux64/share/doc/gmsh/CREDITS.txt onelab-Linux64/CREDITS.txt
   echo "\n\n" >> onelab-Linux64/CREDITS.txt
   cat /tmp/getdp-*${GETDP}-Linux64/share/doc/getdp/CREDITS.txt >> onelab-Linux64/CREDITS.txt
-  mv /tmp/getdp-*${GETDP}-Linux64/share/doc/getdp/templates onelab-Linux64
-  mv /tmp/gmsh-*${GMSH}-Linux64/share/doc/gmsh/tutorial onelab-Linux64
-  cp -R /tmp/models onelab-Linux64
+  cp -R /tmp/models onelab-Linux64/
+  cp -R /tmp/tutorials onelab-Linux64/
+  cp -R /tmp/gmsh-*${GMSH}-Linux64/share/doc/gmsh/tutorial/*.* onelab-Linux64/tutorials/gmsh/
   rm -rf /tmp/gmsh-*
   rm -rf /tmp/getdp-*
   zip -r onelab-Linux64.zip onelab-Linux64
@@ -164,9 +191,9 @@ if [ $# -lt 1 ] || [ $1 == "linux32" ]; then
   mv /tmp/gmsh-*${GMSH}-Linux32/share/doc/gmsh/CREDITS.txt onelab-Linux32/CREDITS.txt
   echo "\n\n" >> onelab-Linux32/CREDITS.txt
   cat /tmp/getdp-*${GETDP}-Linux32/share/doc/getdp/CREDITS.txt >> onelab-Linux32/CREDITS.txt
-  mv /tmp/getdp-*${GETDP}-Linux32/share/doc/getdp/templates onelab-Linux32
-  mv /tmp/gmsh-*${GMSH}-Linux32/share/doc/gmsh/tutorial onelab-Linux32
-  cp -R /tmp/models onelab-Linux32
+  cp -R /tmp/models onelab-Linux32/
+  cp -R /tmp/tutorials onelab-Linux32/
+  cp -R /tmp/gmsh-*${GMSH}-Linux32/share/doc/gmsh/tutorial/*.* onelab-Linux32/tutorials/gmsh/
   rm -rf /tmp/gmsh-*
   rm -rf /tmp/getdp-*
   zip -r onelab-Linux32.zip onelab-Linux32
@@ -184,9 +211,9 @@ if [ $# -lt 1 ] || [ $1 == "mac" ]; then
   hdiutil attach -nobrowse -noverify -noautoopen -mountpoint gmsh_mount /tmp/gmsh-tmp.cdr
   tar zxvf /tmp/getdp-${GETDP}-MacOSXc.tgz -C /tmp
   cp /tmp/README.txt onelab-MacOSX
-  cp -R gmsh_mount/Gmsh.app onelab-MacOSX
+  cp -R gmsh_mount/Gmsh.app onelab-MacOSX/
   mv /tmp/getdp-*${GETDP}-MacOSX/bin/getdp onelab-MacOSX/Gmsh.app/Contents/MacOS/
-  security unlock-keychain -p "FIXME!" ${HOME}/Library/Keychains/login.keychain
+  security unlock-keychain -p "FIXME" ${HOME}/Library/Keychains/login.keychain
   codesign -v --force --deep --sign "Developer ID Application: Christophe Geuzaine" onelab-MacOSX/Gmsh.app
   cp gmsh_mount/LICENSE.txt onelab-MacOSX/LICENSE.txt
   echo "\n\n" >> onelab-MacOSX/LICENSE.txt
@@ -194,9 +221,9 @@ if [ $# -lt 1 ] || [ $1 == "mac" ]; then
   cp gmsh_mount/CREDITS.txt onelab-MacOSX/CREDITS.txt
   echo "\n\n" >> onelab-MacOSX/CREDITS.txt
   cat /tmp/getdp-*${GETDP}-MacOSX/share/doc/getdp/CREDITS.txt >> onelab-MacOSX/CREDITS.txt
-  mv /tmp/getdp-*${GETDP}-MacOSX/share/doc/getdp/templates onelab-MacOSX
-  cp -R gmsh_mount/tutorial onelab-MacOSX
-  cp -R /tmp/models onelab-MacOSX
+  cp -R /tmp/models onelab-MacOSX/
+  cp -R /tmp/tutorials onelab-MacOSX/
+  cp -R gmsh_mount/tutorial/*.* onelab-MacOSX/tutorials/gmsh/
   hdiutil eject gmsh_mount
   rm -rf /tmp/gmsh-*
   rm -rf /tmp/getdp-*
@@ -205,5 +232,3 @@ if [ $# -lt 1 ] || [ $1 == "mac" ]; then
   codesign -v --sign "Developer ID Application: Christophe Geuzaine" onelab-MacOSX.dmg
   rm -rf onelab-MacOSX
 fi
-
-rm -rf /tmp/models
