523d8d79f48ab1a54af1af079d1ecaf0518b9764
Christophe Geuzaine <cgeuzaine@ulg.ac.be>
2018-04-22 15:20:44 +0200

    test

--- Common/GmshGlobal.h
+++ Common/GmshGlobal.h
@@ -63,10 +63,8 @@ int GmshFLTK(int argc = 0, char **argv = 0);
 #define GMSH_API
 #endif
 
-extern "C" {
-  GMSH_API int GmshMainBatch(int argc, char **argv);
-  GMSH_API int GmshMainFLTK(int argc, char **argv);
-}
+GMSH_API int GmshMainBatch(int argc, char **argv);
+GMSH_API int GmshMainFLTK(int argc, char **argv);
 
 #undef GMSH_API
 
