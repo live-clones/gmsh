ba5d289764121d744aec1386994fddc1c0151a0a
Christophe Geuzaine <cgeuzaine@ulg.ac.be>
2018-06-29 09:52:52 +0200

    use BRepClass_FaceClassifier in OCCFace::containsParam instead of the STL. It's slower, but more robust. It also avoids using the STL... which OpenCASCADE sometimes fails to generate (fixes issue #415)

--- Geo/OCCFace.cpp
+++ Geo/OCCFace.cpp
@@ -20,6 +20,7 @@
 #include <BOPTools_AlgoTools.hxx>
 #include <BOPTools_AlgoTools2D.hxx>
 #include <BRepBndLib.hxx>
+#include <BRepClass_FaceClassifier.hxx>
 #include <BRepLProp_SLProps.hxx>
 #include <BRep_Builder.hxx>
 #include <Bnd_Box.hxx>
@@ -433,12 +434,9 @@ bool OCCFace::isSphere(double &radius, SPoint3 &center) const
   }
 }
 
-
-// Function containsparam should
-
-
 bool OCCFace::containsParam(const SPoint2 &pt)
 {
+#if 0
   if(!buildSTLTriangulation(false)){
     Msg::Info("Inacurate computation in OCCFace::containsParam");
     return GFace::containsParam(pt);
@@ -457,6 +455,13 @@ bool OCCFace::containsParam(const SPoint2 &pt)
       return true;
   }
   return false;
+#else
+  const Standard_Real tolerance = BRep_Tool::Tolerance(s);
+  BRepClass_FaceClassifier faceClassifier;
+  faceClassifier.Perform(s, gp_Pnt2d{pt.x(), pt.y()}, tolerance);
+  const TopAbs_State state = faceClassifier.State();
+  return (state == TopAbs_IN || state == TopAbs_ON);
+#endif
 }
 
 void OCCFace::writeBREP(const char *filename)
