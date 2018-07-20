28d1e0162d3d907a722fcbd794b373c05f9a981e
ws <ws@plaxis.nl>
2018-07-20 14:26:50 +0200

    Comment out one more line of code to avoid compilation error.

--- Mesh/meshGFaceDelaunayInsertion.cpp
+++ Mesh/meshGFaceDelaunayInsertion.cpp
@@ -1130,7 +1130,7 @@ void bowyerWatson(GFace *gf, int MAXPNT,
   //  if (equivalence)_printTris ("before.pos", AllTris.begin(), AllTris.end(), DATA);
   // int nbSwaps = edgeSwapPass(gf, AllTris, SWCR_DEL, DATA);
   // _printTris ("after2.pos", AllTris, Us,Vs);
-  Msg::Debug("Delaunization of the initial mesh done (%d swaps)", nbSwaps);
+  // Msg::Debug("Delaunization of the initial mesh done (%d swaps)", nbSwaps);
 
   if(AllTris.empty()){
     Msg::Error("No triangles in initial mesh");
