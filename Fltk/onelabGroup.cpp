1ea71b5b338051736a3498ec440fd698920d6090
Christophe Geuzaine <cgeuzaine@ulg.ac.be>
2018-07-27 13:35:34 +0200

    fix #426

--- Fltk/onelabGroup.cpp
+++ Fltk/onelabGroup.cpp
@@ -969,7 +969,7 @@ static void onelab_input_choice_file_chooser_cb(Fl_Widget *w, void *data)
   Fl_Input_Choice *but = (Fl_Input_Choice*)w->parent();
   if(fileChooser(FILE_CHOOSER_SINGLE, "Choose", "", but->value())){
     but->value(fileChooserGetName(1).c_str());
-    but->do_callback(but, data);
+    but->do_callback();
   }
 }
 
