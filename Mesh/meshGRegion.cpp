f73f249a9cb9da6e102fc1e4e287dd8764356d03
Christophe Geuzaine <cgeuzaine@ulg.ac.be>
2018-08-31 15:50:29 +0200

    only keep vertices linked to elements in mesh_vertices

--- Mesh/meshGRegion.cpp
+++ Mesh/meshGRegion.cpp
@@ -316,7 +316,7 @@ void MeshDelaunayVolume(std::vector<GRegion *> &regions)
 
   // now do insertion of points
 
-if(CTX::instance()->mesh.algo3d == ALGO_3D_MMG3D) {
+  if(CTX::instance()->mesh.algo3d == ALGO_3D_MMG3D) {
     refineMeshMMG(gr);
   }
   else if(CTX::instance()->mesh.oldRefinement) {
