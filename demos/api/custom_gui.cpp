e0162ee1a17a06029cf8373d4f0d1cf6954ce10f
Christophe Geuzaine <cgeuzaine@ulg.ac.be>
2019-01-12 08:52:40 +0100

    use std::thread

--- demos/api/custom_gui.cpp
+++ demos/api/custom_gui.cpp
@@ -1,14 +1,8 @@
 #include <cmath>
 #include <sstream>
 #include <atomic>
+#include <thread>
 #include "gmsh.h"
-
-#if defined(_WIN32)
-#include <windows.h>
-#include <process.h>
-#else
-#include <pthread.h>
-#endif
 
 // This example shows how to implement a custom user interface running
 // computationally expensive calculations in separate threads. The threads can
@@ -18,37 +12,34 @@
 std::atomic<int> stop_computation(0);
 
 // a computationally expensive routine, that will be run in its own thread
-void *compute(void *ptr)
+void compute(const std::string &arg)
 {
-  std::string *arg = static_cast<std::string*>(ptr);
   double k = 0.;
   int p = 0, n = 10000000;
   std::vector<double> progress;
   gmsh::onelab::getNumber("My App/Show progress?", progress);
   for(int j = 0; j < n; j++){
     // stop computation if requested by clicking on "Stop it!"
-    if(stop_computation)
+    if(stop_computation > 0)
       break;
     k = sin(k) + cos(j / 45.);
     // show progress in real time?
     if(progress.size() && progress[0] && !(j % (n / 100))){
       p++;
-      gmsh::onelab::setString(*arg, {std::to_string(p) + "%"});
+      gmsh::onelab::setString(arg, {std::to_string(p) + "%"});
       // any code in a thread other than the main thread that modifies the user
       // interface should be locked
       gmsh::fltk::lock();
-      gmsh::logger::write(*arg + " progress " + std::to_string(p) + "%");
+      gmsh::logger::write(arg + " progress " + std::to_string(p) + "%");
       gmsh::fltk::unlock();
       // ask the main thread to process pending events and to update the user
       // interface
       gmsh::fltk::awake("update");
     }
   }
-  gmsh::onelab::setNumber(*arg + " result", {k});
+  gmsh::onelab::setNumber(arg + " result", {k});
   gmsh::onelab::setString("Action", {"done computing"});
   gmsh::fltk::awake("update");
-  delete arg;
-  return 0;
 }
 
 int main(int argc, char **argv)
@@ -94,10 +85,8 @@ int main(int argc, char **argv)
       for(unsigned int i = 0; i < n; i++){
         std::ostringstream sstream;
         sstream << "My App/Thread " << i + 1;
-        std::string *arg = new std::string(sstream.str());
-        pthread_t t;
-        pthread_create(&t, nullptr, compute, static_cast<void*>(arg));
-        pthread_detach(t);
+        std::thread t(compute, sstream.str());
+        t.detach();
       }
     }
     if(a.size() && a[0] == "should stop"){
@@ -108,7 +97,7 @@ int main(int argc, char **argv)
       gmsh::onelab::setString("Action", {""});
       gmsh::onelab::setString("Button", {"Do it!", "should compute"});
       gmsh::fltk::update();
-      stop_computation = false;
+      stop_computation--;
     }
   }
 
