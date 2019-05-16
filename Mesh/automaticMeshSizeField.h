d29c8febe7af03d679bb1942c0676c13c6013ddc
jf remacle <jean-francois.remacle@uclouvain.be>
2019-05-16 14:47:21 -0400

    octree

--- Mesh/automaticMeshSizeField.h
+++ Mesh/automaticMeshSizeField.h
@@ -1,41 +1,55 @@
 #ifndef _AUTOMATIC_MESH_SIZE_FIELD_H_
 #define _AUTOMATIC_MESH_SIZE_FIELD_H_
 
+#include "GmshConfig.h"
+
+#ifdef HAVE_HXT
+#include "hxt_octree.h"
+#endif
+
 #include "Field.h"
 
 class automaticMeshSizeField : public Field {
-
+  
+#ifdef HAVE_HXT
+  struct HXTForest *forest;
+  struct HXTForestOptions *forestOptions;
+  HXTStatus updateHXT();
+#endif
+  
   int _nPointsPerCircle;
   int _nPointsPerGap;
   double _hmin, _hmax;
   double _hbulk;
   double _gradientMax;
+  int _nRefine;
   char fileName[256];
-
-public:
-  automaticMeshSizeField()
-  {
-    _nPointsPerCircle = 15;
+  
+ public:
+  ~automaticMeshSizeField();
+  automaticMeshSizeField() : forest(NULL), forestOptions(NULL){
+    _nPointsPerCircle = 55 ;
     _nPointsPerGap = 5;
     _hmin = 1.e-8;// update needed
     _hmax = 1.e+8;// update needed    
     _hbulk = 0.1; // update needed
-    _gradientMax =1.4; 
-
-    
+    _gradientMax =1.4;
+    _nRefine = 5;
     
     options["nPointsPerCircle"] = new FieldOptionInt(_nPointsPerCircle,
-						     "Number of points per circle (adapt to curvature of surfaces)");
+						     "Number of points per circle (adapt to curvature of surfaces)",&update_needed);
     
     options["nPointsPerGap"] = new FieldOptionInt(_nPointsPerGap,
-						  "Number of points in thin layers");
+						  "Number of points in thin layers",&update_needed);
     
     options["hBulk"] = new FieldOptionDouble(_hbulk,
 					     "Size everywhere no size is prescribed", &update_needed);
     
     options["gradientMax"] = new FieldOptionDouble(_gradientMax,
-						   "Maximun gradient of the size field");
+						   "Maximun gradient of the size field",&update_needed);
     
+    options["NRefine"] = new FieldOptionInt(_nRefine,
+					    "Initial refinement level for the octree",&update_needed);
   }
   const char *getName() { return "AutomaticMeshSizeField"; }
   
