bea1e5dde58df756579d3c013a92f14e97242322
Christophe Geuzaine <cgeuzaine@ulg.ac.be>
2019-05-20 18:40:48 +0200

    fix

--- Mesh/automaticMeshSizeField.cpp
+++ Mesh/automaticMeshSizeField.cpp
@@ -9,32 +9,32 @@ extern "C" {
 #include "hxt_curvature.h"
 #include "hxt_bbox.h"
 }
-#endif  
+#endif
 
-double automaticMeshSizeField:: operator()(double X, double Y, double Z, GEntity *ge) {  
-#ifdef HAVE_HXT
-  //  return .2;
+double automaticMeshSizeField:: operator()(double X, double Y, double Z, GEntity *ge)
+{
   double val = 1.e22;
+#if defined(HAVE_HXT) && defined(HAVE_P4EST)
   HXTStatus s = hxtOctreeSearchOne(forest, X, Y, Z, &val);
   if (s == HXT_STATUS_OK){
     return val;
   }
   else Msg::Error ("Cannot find point %g %g %g in the octree",X,Y,Z);
-  return val;
 #else
-  Msg::Error ("Gmsh has to be compiled with HXT for using automaticMeshSizeField");
-#endif  
+  Msg::Error ("Gmsh has to be compiled with HXT and P4EST for using automaticMeshSizeField");
+#endif
+  return val;
 }
 
 automaticMeshSizeField::~automaticMeshSizeField(){
-#ifdef HAVE_P4EST
-  if (forest)hxtForestDelete(&forest);  
+#if defined(HAVE_HXT) && defined(HAVE_P4EST)
+  if (forest)hxtForestDelete(&forest);
   if (forestOptions)hxtForestOptionsDelete(&forestOptions);
 #endif
-}  
+}
 
 
-#ifdef HAVE_HXT
+#if defined(HAVE_HXT) && defined(HAVE_P4EST)
 
 HXTStatus Gmsh2Hxt(std::vector<GRegion *> &regions, HXTMesh *m,
 		   std::map<MVertex *, int> &v2c,
@@ -42,14 +42,12 @@ HXTStatus Gmsh2Hxt(std::vector<GRegion *> &regions, HXTMesh *m,
 
 
 HXTStatus automaticMeshSizeField:: updateHXT(){
-#ifdef HAVE_P4EST
 
-  
   //  printf("%d points per circle\n",_nPointsPerCircle);
-  
+
   if (!update_needed)return HXT_STATUS_OK;
-  
-  if (forest)hxtForestDelete(&forest);  
+
+  if (forest)hxtForestDelete(&forest);
   if (forestOptions)hxtForestOptionsDelete(&forestOptions);
 
   update_needed = false;
@@ -57,7 +55,7 @@ HXTStatus automaticMeshSizeField:: updateHXT(){
   // Soit on charge un fichier avec l'octree (ma préférence)
   // Soit on calcule "en live" l'octree ici
 
-  
+
   // verify that only one region exists in the model
   if (GModel::current()->getNumRegions() != 1){
     Msg::Error ("automaticMeshSizeField can only work for now on models with one region");
@@ -65,14 +63,14 @@ HXTStatus automaticMeshSizeField:: updateHXT(){
   }
   std::vector<GRegion*> regions;
   regions.push_back(*(GModel::current()->firstRegion()));
-  
+
   // create HXT mesh structure
   HXTMesh *mesh;
   HXTContext *context;
   HXT_CHECK(hxtContextCreate(&context));
   HXT_CHECK(hxtMeshCreate(context, &mesh));
   std::map<MVertex *, int> v2c;
-  std::vector<MVertex *> c2v;  
+  std::vector<MVertex *> c2v;
   Gmsh2Hxt(regions, mesh, v2c, c2v);
 
   // Compute curvature
@@ -81,7 +79,7 @@ HXTStatus automaticMeshSizeField:: updateHXT(){
   HXTEdges         *edges;
   HXT_CHECK(hxtEdgesCreate(mesh,&edges));
   HXT_CHECK(hxtCurvatureRusinkiewicz(mesh,&nodalCurvature,&curvatureCrossfield,edges,1));
-  
+
   // compute rtree
   RTree<int , double,3> triRTree;
   HXTBbox          bbox_triangle;
@@ -99,7 +97,7 @@ HXTStatus automaticMeshSizeField:: updateHXT(){
     int K = (int)i;
     triRTree.Insert(bbox_triangle.min,bbox_triangle.max,K);
   }
-  
+
   // compute bbox of the mesh
   double            bbox_vertices[6];
   HXTBbox           bbox_mesh;
@@ -110,16 +108,16 @@ HXTStatus automaticMeshSizeField:: updateHXT(){
     bbox_vertices[i  ] = 1.3*bbox_mesh.min[i];
     bbox_vertices[i+3] = 1.3*bbox_mesh.max[i];
   }
-  
+
   // 1 --> OPTIONS ------------------------------------------
   HXT_CHECK(hxtForestOptionsCreate(&forestOptions));
-  forestOptions->nodePerTwoPi = _nPointsPerCircle;  
+  forestOptions->nodePerTwoPi = _nPointsPerCircle;
   forestOptions->bbox = bbox_vertices;
   forestOptions->nodalCurvature = nodalCurvature;
   forestOptions->triRTree = &triRTree;
   forestOptions->mesh = mesh;
   forestOptions->sizeFunction = NULL;
-  
+
   // --------------------------------------------------------
   HXT_CHECK(hxtForestCreate(0, NULL, &forest, NULL, forestOptions));
   //  return HXT_STATUS_OK;
@@ -130,18 +128,18 @@ HXTStatus automaticMeshSizeField:: updateHXT(){
   int ITER = 0;
   if (forestOptions->nodePerTwoPi > 0){
     HXT_CHECK(hxtOctreeCurvatureRefine(forest, 3/*_nRefine*/));
-    // ensuite lissage du gradient 
+    // ensuite lissage du gradient
     while (ITER++ < 4*_nRefine) {
-      HXT_CHECK(hxtOctreeComputeLaplacian(forest));    
+      HXT_CHECK(hxtOctreeComputeLaplacian(forest));
       HXT_CHECK(hxtOctreeSetMaxGradient(forest));
     }
   }
   forestOptions->nodePerGap = _nPointsPerGap;
   printf("coucou\n");
-  HXT_CHECK(hxtOctreeSurfacesProches(forest));   
+  HXT_CHECK(hxtOctreeSurfacesProches(forest));
   ITER = 0;
   //  while (ITER++ < 4*_nRefine) {
-  //    HXT_CHECK(hxtOctreeComputeLaplacian(forest));    
+  //    HXT_CHECK(hxtOctreeComputeLaplacian(forest));
   //    HXT_CHECK(hxtOctreeSetMaxGradient(forest));
   //  }
 
@@ -150,18 +148,15 @@ HXTStatus automaticMeshSizeField:: updateHXT(){
   HXT_CHECK(hxtFree(&nodalCurvature)              );
   HXT_CHECK(hxtMeshDelete(&mesh)                  );
   HXT_CHECK(hxtContextDelete(&context)            );
-
-
-#endif //ifdef HAVE_P4EST
 }
+
 #endif
 
 void automaticMeshSizeField:: update(){
-#ifdef HAVE_HXT
+#if defined(HAVE_HXT) && defined(HAVE_P4EST)
   HXTStatus s = updateHXT();
   if (s != HXT_STATUS_OK)Msg::Error ("Something went wrong when computing the octree");
 #else
-  Msg::Error ("Gmsh has to be compiled with HXT for using automaticMeshSizeField");
-#endif    
-    
+  Msg::Error ("Gmsh has to be compiled with HXT and P4EST for using automaticMeshSizeField");
+#endif
 };
--- Mesh/automaticMeshSizeField.h
+++ Mesh/automaticMeshSizeField.h
@@ -10,24 +10,27 @@
 #include "Field.h"
 
 class automaticMeshSizeField : public Field {
-  
-#ifdef HAVE_HXT
+
+#if defined(HAVE_HXT) && defined(HAVE_P4EST)
   struct HXTForest *forest;
   struct HXTForestOptions *forestOptions;
   HXTStatus updateHXT();
 #endif
-  
+
   int _nPointsPerCircle;
   int _nPointsPerGap;
   double _hmin, _hmax;
   double _hbulk;
   double _gradientMax;
   int _nRefine;
-  char fileName[256];
-  
+
  public:
   ~automaticMeshSizeField();
-  automaticMeshSizeField() : forest(NULL), forestOptions(NULL){
+  automaticMeshSizeField()
+#if defined(HAVE_HXT) && defined(HAVE_P4EST)
+    : forest(NULL), forestOptions(NULL)
+#endif
+  {
     _nPointsPerCircle = 55 ;
     _nPointsPerGap = 5;
     _hmin = 1.e-8; // update needed
@@ -35,19 +38,19 @@ class automaticMeshSizeField : public Field {
     _hbulk = 0.1; // update needed
     _gradientMax =1.4;
     _nRefine = 5;
-    
+
     options["nPointsPerCircle"] = new FieldOptionInt(_nPointsPerCircle,
 						     "Number of points per circle (adapt to curvature of surfaces)",&update_needed);
-    
+
     options["nPointsPerGap"] = new FieldOptionInt(_nPointsPerGap,
 						  "Number of points in thin layers",&update_needed);
-    
+
     options["hBulk"] = new FieldOptionDouble(_hbulk,
 					     "Size everywhere no size is prescribed", &update_needed);
-    
+
     options["gradientMax"] = new FieldOptionDouble(_gradientMax,
 						   "Maximun gradient of the size field",&update_needed);
-    
+
     options["NRefine"] = new FieldOptionInt(_nRefine,
 					    "Initial refinement level for the octree",&update_needed);
   }
