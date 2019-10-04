7b14bfd38833ffa4a4dd1dffd8ac2b4ce6bc5033
Christophe Geuzaine <cgeuzaine@ulg.ac.be>
2019-10-04 09:14:33 +0200

    fix SplitViewController in portrait mode

--- contrib/mobile/iOS/Onelab/SplitViewController.mm
+++ contrib/mobile/iOS/Onelab/SplitViewController.mm
@@ -9,7 +9,7 @@
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
   self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-  if (self) {
+  if(self) {
     // Custom initialization
   }
   return self;
@@ -18,16 +18,23 @@
 - (void)viewDidLoad
 {
   [super viewDidLoad];
+
+  // always show primary and secondary view controllers side-by-side (default
+  // since iOS9 is to hide the left pane in portrait mode)
+  self.preferredDisplayMode = UISplitViewControllerDisplayModeAllVisible;
+
   // Do any additional setup after loading the view.
-  UINavigationController *right = [self.viewControllers objectAtIndex:1]; // right UINavigationController (Detail)
-  for(UIViewController *v in right.viewControllers){
-    if ([v isKindOfClass:[ModelViewController class]]) {
+  UINavigationController *right = [self.viewControllers
+    objectAtIndex:1]; // right UINavigationController (Detail)
+  for(UIViewController *v in right.viewControllers) {
+    if([v isKindOfClass:[ModelViewController class]]) {
       modelViewController = (ModelViewController *)v;
     }
   }
-  UINavigationController *left = [self.viewControllers objectAtIndex:0]; // left UINavigationController (Master)
-  for(UIViewController *v in left.viewControllers){
-    if ([v isKindOfClass:[ParametersViewController class]]) {
+  UINavigationController *left = [self.viewControllers
+    objectAtIndex:0]; // left UINavigationController (Master)
+  for(UIViewController *v in left.viewControllers) {
+    if([v isKindOfClass:[ParametersViewController class]]) {
       parametersViewController = (ParametersViewController *)v;
     }
   }
@@ -35,7 +42,7 @@
   [self setPresentsWithGesture:NO];
 }
 
--(void)viewDidAppear:(BOOL)animated
+- (void)viewDidAppear:(BOOL)animated
 {
   modelViewController.initialModel = self.initialModel;
   [super viewDidAppear:animated];
