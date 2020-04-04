3bf7edcc91b51f8fab1963c809dbc280b7f2efed
Christophe Geuzaine <cgeuzaine@uliege.be>
2020-04-04 20:30:57 +0200

    Skip meshing of discrete entities only if all their bounding entities are discrete.
    This allows to remesh fully discrete models, from 1D to 3D.

--- Mesh/meshGFace.cpp
+++ Mesh/meshGFace.cpp
@@ -2854,23 +2854,24 @@ static bool meshGeneratorPeriodic(GFace *gf, int RECUR_ITER,
   return true;
 }
 
+static bool isFullyDiscrete(GFace *gf)
+{
+  if(gf->geomType() != GEntity::DiscreteSurface) return false;
+  if(static_cast<discreteFace *>(gf)->haveParametrization()) return false;
+  std::vector<GEdge *> e = gf->edges();
+  for(std::size_t i = 0; i < e.size(); i++) {
+    if(e[i]->geomType() != GEntity::DiscreteCurve ||
+       (e[i]->geomType() == GEntity::DiscreteCurve &&
+        static_cast<discreteEdge *>(e[i])->haveParametrization())) {
+      return false;
+    }
+  }
+  return true;
+}
+
 void deMeshGFace::operator()(GFace *gf)
 {
-  if(gf->geomType() == GEntity::DiscreteSurface) {
-    // if the surface and all the bounding curves are also discrete (without
-    // parametrization!), we keep the mesh
-    bool keep = !static_cast<discreteFace *>(gf)->haveParametrization();
-    std::vector<GEdge *> e = gf->edges();
-    for(std::size_t i = 0; i < e.size(); i++) {
-      if(e[i]->geomType() != GEntity::DiscreteCurve ||
-         (e[i]->geomType() == GEntity::DiscreteCurve &&
-          static_cast<discreteEdge *>(e[i])->haveParametrization())) {
-        keep = false;
-        break;
-      }
-    }
-    if(keep) return;
-  }
+  if(isFullyDiscrete(gf)) return;
   gf->deleteMesh();
   gf->meshStatistics.status = GFace::PENDING;
   gf->meshStatistics.nbTriangle = gf->meshStatistics.nbEdge = 0;
@@ -3107,7 +3108,7 @@ void orientMeshGFace::operator()(GFace *gf)
     // now. This has implications for high-order periodic meshes: see comment in
     // FixPeriodicMesh().
   }
-  else if(gf->geomType() == GEntity::DiscreteSurface ||
+  else if(isFullyDiscrete(gf) ||
           gf->geomType() == GEntity::BoundaryLayerSurface) {
     // Don't do anything
   }
