08525c8e68ec921cd68d48dee8471c15645af26e
Christophe Geuzaine <cgeuzaine@uliege.be>
2020-04-21 16:25:37 +0200

    remove old stuff

--- utils/icons/make_icon.sh
+++ utils/icons/make_icon.sh
@@ -4,7 +4,7 @@
 
 if [ $# -lt 2 ]; then
   echo "Usage: $0 pngfile iconname" 1>&2;
-  echo "Example: $0 gmsh_1024x1024.png MacIcons" 1>&2;
+  echo "Example: $0 gmsh-no-text.png MacIcons" 1>&2;
   exit 1;
 fi
 
