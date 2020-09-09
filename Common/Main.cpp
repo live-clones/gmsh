fa49adadc9c37faabdc4c740b79d901054da156e
Christophe Geuzaine <cgeuzaine@uliege.be>
2020-09-09 09:13:55 +0200

    tentative fix for link with mingw

--- Common/Main.cpp
+++ Common/Main.cpp
@@ -8,11 +8,14 @@
 
 #if defined(WIN32) && !defined(__CYGWIN__)
 // necessary to handle UTF command line arguments on Windows
-#include <windows.h>
+#define _UNICODE
+#define UNICODE
 #include <wchar.h>
+#include <stdio.h>
 // from OS.cpp
 extern unsigned int utf8FromUtf16(char *dst, unsigned int dstlen,
                                   const wchar_t *src, unsigned int srclen);
+extern "C"
 int wmain(int argc, wchar_t *wargv[], wchar_t *envp[])
 {
   char **argv = new char*[argc + 1];
