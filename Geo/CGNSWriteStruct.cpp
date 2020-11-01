3c3403907583fbc6400d4052fdc0fcd425ddc830
Christophe Geuzaine <cgeuzaine@uliege.be>
2020-11-01 19:31:33 +0100

    maximum simplification (here's nothing left ;-) before redoing interfaces, names and BC from scratch

--- Geo/CGNSWriteStruct.cpp
+++ Geo/CGNSWriteStruct.cpp
@@ -12,78 +12,61 @@
 
 #if defined(HAVE_LIBCGNS)
 
-// Structured CGNS file output is based on intial work financed by the Indian
-// Institute of Technology Hyderabad - BRNS sponsored project in 2018 Under the
-// guidance of Prof. Vinayak Eswaran <eswar@iith.ac.in>
-
 static int writeZonesStruct2D(int cgIndexFile, int cgIndexBase,
                               std::vector<GFace *> &faces,
                               double scalingFactor)
 {
-  for(std::size_t iFace = 0; iFace < faces.size(); iFace++) {
-    GFace *gf = faces[iFace];
+  for(auto gf : faces) {
+    cgsize_t imax = gf->transfinite_vertices.size();
+    cgsize_t jmax = gf->transfinite_vertices[0].size();
 
+    // write zone
     int cgIndexZone = 0;
-    cgsize_t cgZoneSize[9] = {0};
-
-    cgZoneSize[0] = gf->transfinite_vertices.size(); // imax, Number of vertices
-    cgZoneSize[1] = gf->transfinite_vertices[0].size(); // jmax
-    cgZoneSize[2] = cgZoneSize[0] - 1; // Number of elements
-    cgZoneSize[3] = cgZoneSize[1] - 1;
-    cgZoneSize[4] = 0;
-    cgZoneSize[5] = 0;
-
-    std::vector<double> dBuffer;
+    cgsize_t cgZoneSize[6] = {imax, jmax, imax - 1, jmax - 1, 0, 0};
     std::string zoneName = cgnsString("Surface_" + std::to_string(gf->tag()));
-
     if(cg_zone_write(cgIndexFile, cgIndexBase, zoneName.c_str(), cgZoneSize,
                      Structured, &cgIndexZone) != CG_OK)
       return cgnsError(__FILE__, __LINE__, cgIndexFile);
 
-    // write the grid node
+    // write grid
     int cgIndexGrid = 0;
     if(cg_grid_write(cgIndexFile, cgIndexBase, cgIndexZone, "GridCoordinates",
                      &cgIndexGrid) != CG_OK)
       return cgnsError(__FILE__, __LINE__, cgIndexFile);
 
-    // write the grid coordinates
+    // write x coordinates
     int cgIndexCoord = 0;
-    dBuffer.resize(cgZoneSize[0] * cgZoneSize[1]);
-
-    // x-coordinates for this zone
-    for(std::size_t k = 0; k < gf->transfinite_vertices[0].size(); k++) {
-      for(std::size_t j = 0; j < gf->transfinite_vertices.size(); j++) {
-        MVertex *v = gf->transfinite_vertices[j][k];
-        dBuffer[cgZoneSize[0] * k + j] = v->x() * scalingFactor;
+    std::vector<double> data(cgZoneSize[0] * cgZoneSize[1]);
+    for(std::size_t j = 0; j < jmax; j++) {
+      for(std::size_t i = 0; i < imax; i++) {
+        MVertex *v = gf->transfinite_vertices[i][j];
+        data[cgZoneSize[0] * j + i] = v->x() * scalingFactor;
       }
     }
-
     if(cg_coord_write(cgIndexFile, cgIndexBase, cgIndexZone, RealDouble,
-                      "CoordinateX", &dBuffer[0], &cgIndexCoord) != CG_OK)
+                      "CoordinateX", &data[0], &cgIndexCoord) != CG_OK)
       return cgnsError(__FILE__, __LINE__, cgIndexFile);
 
-    // y-coordinates for this zone
-    for(std::size_t k = 0; k < gf->transfinite_vertices[0].size(); k++) {
-      for(std::size_t j = 0; j < gf->transfinite_vertices.size(); j++) {
-        MVertex *v = gf->transfinite_vertices[j][k];
-        dBuffer[cgZoneSize[0] * k + j] = v->y() * scalingFactor;
+    // write y coordinates
+    for(std::size_t j = 0; j < jmax; j++) {
+      for(std::size_t i = 0; i < imax; i++) {
+        MVertex *v = gf->transfinite_vertices[i][j];
+        data[cgZoneSize[0] * j + i] = v->y() * scalingFactor;
       }
     }
-
     if(cg_coord_write(cgIndexFile, cgIndexBase, cgIndexZone, RealDouble,
-                      "CoordinateY", &dBuffer[0], &cgIndexCoord) != CG_OK)
+                      "CoordinateY", &data[0], &cgIndexCoord) != CG_OK)
       return cgnsError(__FILE__, __LINE__, cgIndexFile);
 
-    // z-coordinates for this zone
-    for(std::size_t k = 0; k < gf->transfinite_vertices[0].size(); k++) {
-      for(std::size_t j = 0; j < gf->transfinite_vertices.size(); j++) {
-        MVertex *v = gf->transfinite_vertices[j][k];
-        dBuffer[cgZoneSize[0] * k + j] = v->z() * scalingFactor;
+    // write z coordinates
+    for(std::size_t j = 0; j < jmax; j++) {
+      for(std::size_t i = 0; i < imax; i++) {
+        MVertex *v = gf->transfinite_vertices[i][j];
+        data[cgZoneSize[0] * j + i] = v->z() * scalingFactor;
       }
     }
-
     if(cg_coord_write(cgIndexFile, cgIndexBase, cgIndexZone, RealDouble,
-                      "CoordinateZ", &dBuffer[0], &cgIndexCoord) != CG_OK)
+                      "CoordinateZ", &data[0], &cgIndexCoord) != CG_OK)
       return cgnsError(__FILE__, __LINE__, cgIndexFile);
   }
   return 1;
@@ -93,80 +76,65 @@ static int writeZonesStruct3D(int cgIndexFile, int cgIndexBase,
                               std::vector<GRegion *> &regions,
                               double scalingFactor)
 {
-  for(std::size_t iRegion = 0; iRegion < regions.size(); iRegion++) {
-    GRegion *gr = regions[iRegion];
+  for(auto gr : regions) {
+    cgsize_t imax = gr->transfinite_vertices.size();
+    cgsize_t jmax = gr->transfinite_vertices[0].size();
+    cgsize_t kmax = gr->transfinite_vertices[0][0].size();
 
+    // write zone
     int cgIndexZone = 0;
-    cgsize_t cgZoneSize[9] = {0};
-
-    cgZoneSize[0] = gr->transfinite_vertices.size(); // imax Number of vertices
-    cgZoneSize[1] = gr->transfinite_vertices[0].size(); // jmax
-    cgZoneSize[2] = gr->transfinite_vertices[0][0].size(); // kmax
-    cgZoneSize[3] = cgZoneSize[0] - 1; // Number of elements
-    cgZoneSize[4] = cgZoneSize[1] - 1;
-    cgZoneSize[5] = cgZoneSize[2] - 1;
-    cgZoneSize[6] = 0;
-    cgZoneSize[7] = 0;
-    cgZoneSize[8] = 0;
-
-    int imax = cgZoneSize[0];
-    int jmax = cgZoneSize[1];
-    int kmax = cgZoneSize[2];
-
-    std::vector<double> dBuffer;
+    cgsize_t cgZoneSize[9] = {imax, jmax, kmax, imax - 1, jmax - 1, kmax - 1,
+                              0, 0, 0};
     std::string zoneName = cgnsString("Volume_" + std::to_string(gr->tag()));
-
     if(cg_zone_write(cgIndexFile, cgIndexBase, zoneName.c_str(), cgZoneSize,
                      Structured, &cgIndexZone) != CG_OK)
       return cgnsError(__FILE__, __LINE__, cgIndexFile);
 
-    // write the grid node
+    // write grid
     int cgIndexGrid = 0;
     if(cg_grid_write(cgIndexFile, cgIndexBase, cgIndexZone, "GridCoordinates",
                      &cgIndexGrid) != CG_OK)
       return cgnsError(__FILE__, __LINE__, cgIndexFile);
 
-    // write the grid coordinates
+    // write x coordinates
     int cgIndexCoord = 0;
-    dBuffer.resize(imax * jmax * kmax);
-
-    // x-coordinates for this zone
-    for(std::size_t kk = 0; kk < gr->transfinite_vertices[0][0].size(); kk++) {
-      for(std::size_t jj = 0; jj < gr->transfinite_vertices[0].size(); jj++) {
-        for(std::size_t ii = 0; ii < gr->transfinite_vertices.size(); ii++) {
-          MVertex *v = gr->transfinite_vertices[ii][jj][kk];
-          dBuffer[imax * jmax * kk + imax * jj + ii] = v->x() * scalingFactor;
+    std::vector<double> data(imax * jmax * kmax);
+    for(std::size_t k = 0; k < kmax; k++) {
+      for(std::size_t j = 0; j < jmax; j++) {
+        for(std::size_t i = 0; i < imax; i++) {
+          MVertex *v = gr->transfinite_vertices[i][j][k];
+          data[imax * jmax * k + imax * j + i] = v->x() * scalingFactor;
         }
       }
     }
     if(cg_coord_write(cgIndexFile, cgIndexBase, cgIndexZone, RealDouble,
-                      "CoordinateX", &dBuffer[0], &cgIndexCoord) != CG_OK)
+                      "CoordinateX", &data[0], &cgIndexCoord) != CG_OK)
       return cgnsError(__FILE__, __LINE__, cgIndexFile);
 
-    // y-coordinates for this zone
-    for(std::size_t kk = 0; kk < gr->transfinite_vertices[0][0].size(); kk++) {
-      for(std::size_t jj = 0; jj < gr->transfinite_vertices[0].size(); jj++) {
-        for(std::size_t ii = 0; ii < gr->transfinite_vertices.size(); ii++) {
-          MVertex *v = gr->transfinite_vertices[ii][jj][kk];
-          dBuffer[imax * jmax * kk + imax * jj + ii] = v->y() * scalingFactor;
+    // write y coordinates
+    for(std::size_t k = 0; k < kmax; k++) {
+      for(std::size_t j = 0; j < jmax; j++) {
+        for(std::size_t i = 0; i < imax; i++) {
+          MVertex *v = gr->transfinite_vertices[i][j][k];
+          data[imax * jmax * k + imax * j + i] = v->y() * scalingFactor;
         }
       }
     }
     if(cg_coord_write(cgIndexFile, cgIndexBase, cgIndexZone, RealDouble,
-                      "CoordinateY", &dBuffer[0], &cgIndexCoord) != CG_OK)
+                      "CoordinateY", &data[0], &cgIndexCoord) != CG_OK)
       return cgnsError(__FILE__, __LINE__, cgIndexFile);
 
-    // z-coordinates for this zone
-    for(std::size_t kk = 0; kk < gr->transfinite_vertices[0][0].size(); kk++) {
-      for(std::size_t jj = 0; jj < gr->transfinite_vertices[0].size(); jj++) {
-        for(std::size_t ii = 0; ii < gr->transfinite_vertices.size(); ii++) {
-          MVertex *v = gr->transfinite_vertices[ii][jj][kk];
-          dBuffer[imax * jmax * kk + imax * jj + ii] = v->z() * scalingFactor;
+    // write z coordinates
+    for(std::size_t k = 0; k < kmax; k++) {
+      for(std::size_t j = 0; j < jmax; j++) {
+        for(std::size_t i = 0; i < imax; i++) {
+          MVertex *v = gr->transfinite_vertices[i][j][k];
+          data[imax * jmax * k + imax * j + i] = v->z() * scalingFactor;
         }
       }
     }
     if(cg_coord_write(cgIndexFile, cgIndexBase, cgIndexZone, RealDouble,
-                      "CoordinateZ", &dBuffer[0], &cgIndexCoord) != CG_OK)
+                      "CoordinateZ", &data[0], &cgIndexCoord) != CG_OK)
       return cgnsError(__FILE__, __LINE__, cgIndexFile);
   }
   return 1;
