371deb4de9f6214f25831d2bbef3618de5951058
Christophe Geuzaine <cgeuzaine@uliege.be>
2020-12-16 21:00:46 +0100

    add and remove tabs

--- Fltk/physicalContextWindow.cpp
+++ Fltk/physicalContextWindow.cpp
@@ -4,6 +4,7 @@
 // issues on https://gitlab.onelab.info/gmsh/gmsh/issues.
 
 #include <stdlib.h>
+#include <FL/Fl_Box.H>
 #include <FL/Fl_Tabs.H>
 #include <FL/Fl_Return_Button.H>
 #include "GModel.h"
@@ -50,7 +51,7 @@ static void getExistingPhysicalTagName(const std::string &what,
   }
 }
 
-static void physical_cb(Fl_Widget *w, void *data)
+static void physical_add_cb(Fl_Widget *w, void *data)
 {
   std::string what;
   if(!data)
@@ -65,53 +66,52 @@ static void physical_cb(Fl_Widget *w, void *data)
   int existingTag;
   getExistingPhysicalTagName(what, name, tag, existingName, existingTag);
 
+  physicalContextWindow *pc = FlGui::instance()->physicalContext;
+
   if(existingName.size() || existingTag) {
-    FlGui::instance()->physicalContext->input[0]->textcolor(
-      FlGui::instance()->physicalContext->color);
-    FlGui::instance()->physicalContext->value[0]->textcolor(
-      FlGui::instance()->physicalContext->color);
-    if(what != "Name" &&
-       !strlen(FlGui::instance()->physicalContext->input[0]->value()))
-      FlGui::instance()->physicalContext->input[0]->value(existingName.c_str());
+    // change color to warn that the group exists
+    Fl_Color c = (!CTX::instance()->guiColorScheme ? FL_DARK_BLUE : FL_BLUE);
+    pc->input[0]->textcolor(c);
+    pc->value[0]->textcolor(c);
+    if(what != "Name" && !strlen(pc->input[0]->value()))
+      pc->input[0]->value(existingName.c_str());
     if(what != "Tag")
-      FlGui::instance()->physicalContext->value[0]->value(existingTag);
-    FlGui::instance()->physicalContext->append = true;
+      pc->value[0]->value(existingTag);
+    pc->append = true;
   }
   else {
-    FlGui::instance()->physicalContext->input[0]->textcolor(
-      FL_FOREGROUND_COLOR);
-    FlGui::instance()->physicalContext->value[0]->textcolor(
-      FL_FOREGROUND_COLOR);
-    if(what != "Tag" && FlGui::instance()->physicalContext->mode != "Remove")
-      FlGui::instance()->physicalContext->value[0]->value(NEWPHYSICAL());
-    FlGui::instance()->physicalContext->append = false;
+    pc->input[0]->textcolor(FL_FOREGROUND_COLOR);
+    pc->value[0]->textcolor(FL_FOREGROUND_COLOR);
+    if(what != "Tag")
+      pc->value[0]->value(NEWPHYSICAL());
+    pc->append = false;
   }
 
-  if(FlGui::instance()->physicalContext->butt[0]->active()) {
-    if(FlGui::instance()->physicalContext->butt[0]->value())
-      FlGui::instance()->physicalContext->value[0]->deactivate();
+  if(pc->butt[0]->active()) {
+    if(pc->butt[0]->value())
+      pc->value[0]->deactivate();
     else
-      FlGui::instance()->physicalContext->value[0]->activate();
+      pc->value[0]->activate();
   }
 
-  FlGui::instance()->physicalContext->input[0]->redraw();
-  FlGui::instance()->physicalContext->value[0]->redraw();
+  pc->input[0]->redraw();
+  pc->value[0]->redraw();
 }
 
 void physicalContextWindow::updateOnelabWidgets()
 {
-  printf("updating onelab widgets\n");
+  // TOOD
 
   //win->resize(win->x(), win->y(), win->w(), win->h() + 10);
 }
 
 physicalContextWindow::physicalContextWindow(int deltaFontSize)
-  : mode("Add"), append(false), color(FL_RED)
+  : mode("Add"), append(false)
 {
   FL_NORMAL_SIZE -= deltaFontSize;
 
-  int width = 34 * FL_NORMAL_SIZE;
-  int height = 5 * WB + 6 * BH;
+  int width = 30 * FL_NORMAL_SIZE;
+  int height = 5 * WB + 4 * BH;
 
   win = new paletteWindow(width, height,
                           CTX::instance()->nonModalWindows ? true : false,
@@ -124,25 +124,28 @@ physicalContextWindow::physicalContextWindow(int deltaFontSize)
       group[0] = new Fl_Group(WB, WB + BH, width - 2 * WB,
                               height - 2 * WB - 1 * BH, "Add");
 
-      input[0] = new Fl_Input_Choice(2 * WB, 2 * WB + 1 * BH,
+      box[0] = new Fl_Box(2 * WB, 2 * WB + 1 * BH, width, BH);
+      box[0]->align(FL_ALIGN_LEFT | FL_ALIGN_INSIDE);
+
+      input[0] = new Fl_Input_Choice(2 * WB, 2 * WB + 2 * BH,
                                      (int)(0.6 * width), BH, "Name");
       input[0]->value("");
       input[0]->align(FL_ALIGN_RIGHT);
-      input[0]->callback(physical_cb, (void *)"Name");
+      input[0]->callback(physical_add_cb, (void *)"Name");
       input[0]->when(FL_WHEN_CHANGED);
 
       value[0] =
-        new Fl_Value_Input(2 * WB, 2 * WB + 2 * BH, (int)(0.6 * width), BH, "Tag");
+        new Fl_Value_Input(2 * WB, 2 * WB + 3 * BH, (int)(0.6 * width), BH, "Tag");
       value[0]->value(0);
       value[0]->deactivate();
       value[0]->align(FL_ALIGN_RIGHT);
-      value[0]->callback(physical_cb, (void *)"Tag");
+      value[0]->callback(physical_add_cb, (void *)"Tag");
       value[0]->when(FL_WHEN_CHANGED);
 
-      butt[0] = new Fl_Check_Button(width - width / 4, 2 * WB + 2 * BH,
+      butt[0] = new Fl_Check_Button(width - width / 4, 2 * WB + 3 * BH,
                                     width / 4 - 2 * WB, BH, "Automatic");
       butt[0]->value(1);
-      butt[0]->callback(physical_cb);
+      butt[0]->callback(physical_add_cb);
 
       group[0]->end();
     }
@@ -151,7 +154,13 @@ physicalContextWindow::physicalContextWindow(int deltaFontSize)
       group[1] = new Fl_Group(WB, WB + BH, width - 2 * WB,
                               height - 2 * WB - 1 * BH, "Remove");
 
-      // TODO
+      box[1] = new Fl_Box(2 * WB, 2 * WB + 1 * BH, width, BH);
+      box[1]->align(FL_ALIGN_LEFT | FL_ALIGN_INSIDE);
+
+      choice[0] = new Fl_Choice(2 * WB, 2 * WB + 2 * BH,
+                                (int)(0.6 * width), BH, "Group");
+      choice[0]->align(FL_ALIGN_RIGHT);
+      //choice[0]->callback(physical_remove_cb, (void *)"Name");
 
       group[1]->end();
     }
@@ -169,7 +178,7 @@ void physicalContextWindow::show(const std::string &what, bool remove)
 {
   FlGui::instance()->lastContextWindow = 3;
 
-  // update window title
+  // update window title and labels
   int dim = (what == "Volume") ? 3 : (what == "Surface") ? 2 :
     (what == "Curve") ? 1 : (what == "Point") ? 0 : -1;
   if(dim < 0) {
@@ -177,43 +186,82 @@ void physicalContextWindow::show(const std::string &what, bool remove)
     return;
   }
   win->copy_label(std::string("Physical " + what + " Context").c_str());
+  std::string ent(what + "(s)");
+  std::transform(ent.begin(), ent.end(), ent.begin(), ::tolower);
+  box[0]->copy_label
+    (std::string("Create or choose group, and select " + ent + " to add").c_str());
+  box[1]->copy_label
+    (std::string("Choose group and select " + ent + " to remove").c_str());
 
-  // create menu with existing physical names for the given dimension
-  static std::vector<Fl_Menu_Item> menu;
-  static std::vector<char *> names;
-  for(std::size_t i = 0; i < menu.size(); i++) menu[i].text = "";
-  for(std::size_t i = 0; i < names.size(); i++) free(names[i]);
-  names.clear();
-  menu.clear();
-  for(GModel::piter it = GModel::current()->firstPhysicalName();
-      it != GModel::current()->lastPhysicalName(); it++) {
-    if(it->first.first == dim) {
-      char *str = strdup(it->second.c_str());
-      Fl_Menu_Item item = {str, 0, 0, 0, 0};
-      names.push_back(str);
-      menu.push_back(item);
+  // get all physical group tags and names (this is relatively expensive - so we
+  // shouldn't do it in the callback)
+  std::map<int, std::vector<GEntity *> > groups[4];
+  GModel::current()->getPhysicalGroups(groups);
+  physicalTags.clear();
+  physicalNames.clear();
+  for(int dim = 0; dim < 4; dim++) {
+    for(auto &p : groups[dim]) {
+      std::pair<int, int> dimTag(dim, p.first);
+      std::string name = GModel::current()->getPhysicalName(dim, p.first);
+      physicalTags[dimTag] = name;
+      if(name.size()) physicalNames[name] = dimTag;
     }
   }
-  Fl_Menu_Item item = {0};
-  menu.push_back(item);
-  input[0]->menubutton()->copy(&menu[0]);
+
+  // create menu with existing physical names for the given dimension
+  std::vector<Fl_Menu_Item> menuAdd, menuRemove;
+  static std::vector<char *> toFree;
+  for(std::size_t i = 0; i < toFree.size(); i++) free(toFree[i]);
+  toFree.clear();
+  {
+    for(auto &p : physicalNames) {
+      if(p.second.first == dim) {
+        char *str = strdup(p.first.c_str());
+        Fl_Menu_Item item = {str, 0, 0, 0, 0};
+        toFree.push_back(str);
+        menuAdd.push_back(item);
+      }
+    }
+    Fl_Menu_Item item = {0};
+    menuAdd.push_back(item);
+    input[0]->menubutton()->copy(&menuAdd[0]);
+  }
+  {
+    for(auto &p : physicalTags) {
+      if(p.first.first == dim) {
+        std::string label = std::to_string(p.first.second);
+        if(p.second.size()) label += ": " + p.second;
+        char *str = strdup(label.c_str());
+        Fl_Menu_Item item = {str, 0, 0, 0, 0};
+        toFree.push_back(str);
+        menuRemove.push_back(item);
+      }
+    }
+    Fl_Menu_Item item = {0};
+    menuRemove.push_back(item);
+    choice[0]->copy(&menuRemove[0]);
+  }
+
+  for(int i = 0; i < 2; i++) {
+    group[i]->hide();
+    group[i]->deactivate();
+  }
 
   if(remove) {
     mode = "Remove";
-    butt[0]->deactivate();
-    value[0]->activate();
-    color = (!CTX::instance()->guiColorScheme ? FL_DARK_GREEN : FL_GREEN);
+    group[1]->show();
+    group[1]->activate();
   }
   else {
     mode = "Add";
-    butt[0]->activate();
+    group[0]->show();
+    group[0]->activate();
     if(butt[0]->value())
       value[0]->deactivate();
     else
       value[0]->activate();
-    color = (!CTX::instance()->guiColorScheme ? FL_DARK_RED : FL_RED);
+    physical_add_cb(0, (void *)"Name");
   }
-  physical_cb(0, (void *)"Name");
 
   updateOnelabWidgets();
 
--- Fltk/physicalContextWindow.h
+++ Fltk/physicalContextWindow.h
@@ -7,23 +7,28 @@
 #define PHYSICAL_CONTEXT_WINDOW_H
 
 #include <string>
+#include <map>
 #include <FL/Fl.H>
 #include <FL/Fl_Window.H>
 #include <FL/Fl_Input_Choice.H>
 #include <FL/Fl_Check_Button.H>
 #include <FL/Fl_Value_Input.H>
+#include <FL/Fl_Choice.H>
 
 class physicalContextWindow {
 public:
   Fl_Window *win;
   Fl_Tabs *tab;
   Fl_Group *group[2];
+  Fl_Box *box[10];
   Fl_Input_Choice *input[10];
   Fl_Check_Button *butt[10];
   Fl_Value_Input *value[10];
+  Fl_Choice *choice[10];
   std::string mode;
   bool append;
-  Fl_Color color;
+  std::map<std::pair<int, int>, std::string> physicalTags;
+  std::map<std::string, std::pair<int, int> > physicalNames;
 
 public:
   physicalContextWindow(int deltaFontSize = 0);
