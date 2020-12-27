0fc12690ac14fd1c900ce01817ee5494208f3137
Christophe Geuzaine <cgeuzaine@uliege.be>
2020-12-27 21:29:02 +0100

    cleanup

--- demos/api/import_perf.jl
+++ demos/api/import_perf.jl
@@ -12,30 +12,33 @@ nodes = Array{UInt,1}(undef, (N + 1) * (N + 1))
 # connectivities (node tags) of triangle elements
 tris = Array{UInt,1}(undef, N * N * 2 * 3)
 
-function tag(i, j)
+function get_node_tag(i, j)
     return (N + 1) * i + j + 1
 end
 
-k = 0
-l = 0
-for i in 0:N
-    for j in 0:N
-        nodes[1 + k] = tag(i, j)
-        coords[1 + 3 * k] = float(i) / N
-        coords[1 + 3 * k + 1] = float(j) / N
-        coords[1 + 3 * k + 2] = 0.05 * sin(10 * float(i + j) / N)
-        global k += 1
-        if (i > 0 && j > 0)
-            tris[1 + 6 * l] = tag(i - 1, j - 1)
-            tris[1 + 6 * l + 1] = tag(i, j - 1)
-            tris[1 + 6 * l + 2] = tag(i - 1, j)
-            tris[1 + 6 * l + 3] = tag(i, j - 1)
-            tris[1 + 6 * l + 4] = tag(i, j)
-            tris[1 + 6 * l + 5] = tag(i - 1, j)
-            global l += 1
+function create_mesh(N, nodes, coords, tris)
+    k = 0
+    l = 0
+    for i in 0:N
+        for j in 0:N
+            nodes[1 + k] = get_node_tag(i, j)
+            coords[1 + 3 * k] = float(i) / N
+            coords[1 + 3 * k + 1] = float(j) / N
+            coords[1 + 3 * k + 2] = 0.05 * sin(10 * float(i + j) / N)
+            k += 1
+            if (i > 0 && j > 0)
+                tris[1 + 6 * l] = get_node_tag(i - 1, j - 1)
+                tris[1 + 6 * l + 1] = get_node_tag(i, j - 1)
+                tris[1 + 6 * l + 2] = get_node_tag(i - 1, j)
+                tris[1 + 6 * l + 3] = get_node_tag(i, j - 1)
+                tris[1 + 6 * l + 4] = get_node_tag(i, j)
+                tris[1 + 6 * l + 5] = get_node_tag(i - 1, j)
+                l += 1
+            end
         end
     end
 end
+create_mesh(N, nodes, coords, tris)
 
 toc = gmsh.logger.getWallTime()
 println("==> created nodes and connectivities in ", toc - tic, " seconds")
--- demos/api/import_perf.py
+++ demos/api/import_perf.py
@@ -19,27 +19,29 @@ except:
     nodes = [0] * (N + 1) * (N + 1)
     tris = [0] * N * N * 2 * 3
 
-def tag(i, j):
+def get_node_tag(i, j):
     return (N + 1) * i + j + 1
 
-k = 0
-l = 0
-for i in range(N + 1):
-    for j in range(N + 1):
-        nodes[k] = tag(i, j)
-        coords[3 * k] = float(i) / N
-        coords[3 * k + 1] = float(j) / N
-        coords[3 * k + 2] = 0.05 * math.sin(10 * float(i + j) / N)
-        k = k + 1
-        if i > 0 and j > 0:
-            tris[6 * l] = tag(i - 1, j - 1)
-            tris[6 * l + 1] = tag(i, j - 1)
-            tris[6 * l + 2] = tag(i - 1, j)
-            tris[6 * l + 3] = tag(i, j - 1)
-            tris[6 * l + 4] = tag(i, j)
-            tris[6 * l + 5] = tag(i - 1, j)
-            l = l + 1
+def create_mesh(N, coords, nodes, tris):
+    k = 0
+    l = 0
+    for i in range(N + 1):
+        for j in range(N + 1):
+            nodes[k] = get_node_tag(i, j)
+            coords[3 * k] = float(i) / N
+            coords[3 * k + 1] = float(j) / N
+            coords[3 * k + 2] = 0.05 * math.sin(10 * float(i + j) / N)
+            k = k + 1
+            if i > 0 and j > 0:
+                tris[6 * l] = get_node_tag(i - 1, j - 1)
+                tris[6 * l + 1] = get_node_tag(i, j - 1)
+                tris[6 * l + 2] = get_node_tag(i - 1, j)
+                tris[6 * l + 3] = get_node_tag(i, j - 1)
+                tris[6 * l + 4] = get_node_tag(i, j)
+                tris[6 * l + 5] = get_node_tag(i - 1, j)
+                l = l + 1
 
+create_mesh(N, coords, nodes, tris)
 toc = gmsh.logger.getWallTime()
 print("==> created nodes and connectivities in {} seconds".format(toc - tic))
 
