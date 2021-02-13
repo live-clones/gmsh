ecfbd3939820930be1a67115c3a0b0a824500146
Christophe Geuzaine <cgeuzaine@uliege.be>
2021-02-13 17:41:52 +0100

    make mesh type selection interactive using onelab

--- demos/api/terrain_bspline.py
+++ demos/api/terrain_bspline.py
@@ -25,23 +25,42 @@ v = gmsh.model.occ.addBox(0, 0, -0.5, 1, 1, 1)
 gmsh.model.occ.fragment([(2, s)], [(3, v)])
 gmsh.model.occ.synchronize()
 
-# set this to True to build a fully hex mesh
-#transfinite = True
-transfinite = False
+# define a parameter to select the mesh type interactively
+gmsh.onelab.set("""[
+  {
+    "type":"number",
+    "name":"Parameters/Full-hex mesh?",
+    "values":[0],
+    "choices":[0, 1]
+  }
+]""")
 
-if transfinite:
-    NN = 30
-    for c in gmsh.model.getEntities(1):
-        gmsh.model.mesh.setTransfiniteCurve(c[1], NN)
-    for s in gmsh.model.getEntities(2):
-        gmsh.model.mesh.setTransfiniteSurface(s[1])
-        gmsh.model.mesh.setRecombine(s[0], s[1])
-        gmsh.model.mesh.setSmoothing(s[0], s[1], 100)
-    gmsh.model.mesh.setTransfiniteVolume(1)
-    gmsh.model.mesh.setTransfiniteVolume(2)
-else:
-    gmsh.option.setNumber('Mesh.MeshSizeMin', 0.05)
-    gmsh.option.setNumber('Mesh.MeshSizeMax', 0.05)
+def setMeshConstraint():
+    if gmsh.onelab.getNumber('Parameters/Full-hex mesh?') == 1:
+        NN = 30
+        for c in gmsh.model.getEntities(1):
+            gmsh.model.mesh.setTransfiniteCurve(c[1], NN)
+            for s in gmsh.model.getEntities(2):
+                gmsh.model.mesh.setTransfiniteSurface(s[1])
+                gmsh.model.mesh.setRecombine(s[0], s[1])
+                gmsh.model.mesh.setSmoothing(s[0], s[1], 100)
+        gmsh.model.mesh.setTransfiniteVolume(1)
+        gmsh.model.mesh.setTransfiniteVolume(2)
+    else:
+        gmsh.model.mesh.removeConstraints()
+        gmsh.option.setNumber('Mesh.MeshSizeMin', 0.05)
+        gmsh.option.setNumber('Mesh.MeshSizeMax', 0.05)
 
-if '-nopopup' not in sys.argv:
-    gmsh.fltk.run()
+def checkForEvent():
+    action = gmsh.onelab.getString("ONELAB/Action")
+    if len(action) and action[0] == "check":
+        gmsh.onelab.setString("ONELAB/Action", [""])
+        setMeshConstraint()
+    return 1
+
+if "-nopopup" not in sys.argv:
+    gmsh.fltk.initialize()
+    while gmsh.fltk.isAvailable() and checkForEvent():
+        gmsh.fltk.wait()
+
+gmsh.finalize()
