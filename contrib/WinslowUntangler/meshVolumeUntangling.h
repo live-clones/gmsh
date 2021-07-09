32f0250bafa52b97dadf4b8b13ff7ff2fc0fcc95
Christophe Geuzaine <cgeuzaine@uliege.be>
2021-07-09 08:22:23 +0200

    clang format + fix compile without Eigen and Alglib

--- contrib/WinslowUntangler/meshVolumeUntangling.h
+++ contrib/WinslowUntangler/meshVolumeUntangling.h
@@ -16,17 +16,18 @@ class GRegion;
  *        Winslow functional, with regularization for tangled elements.
  *        See the following paper for details:
  *          Foldover-free maps in 50 lines of code.
- *          Garanzha, V., Kaporin, I., Kudryavtseva, L., Protais, F., Ray, N., & Sokolov, D. (2021). 
- *          arXiv preprint arXiv:2102.03069.
- *        Non-tetrahedron elements are subdivided into (overlapping) tetrahedra for the smoothing.
- *        Only for linear geometry, not high-order.
- *        Mesh vertices on boundary curves and surfaces are fixed.
+ *          Garanzha, V., Kaporin, I., Kudryavtseva, L., Protais, F., Ray, N., &
+ * Sokolov, D. (2021). arXiv preprint arXiv:2102.03069. Non-tetrahedron elements
+ * are subdivided into (overlapping) tetrahedra for the smoothing. Only for
+ * linear geometry, not high-order. Mesh vertices on boundary curves and
+ * surfaces are fixed.
  *
  * @param gr The region containing the volume elements
- * @param iterMax Number of outer iterations in the optimization (re-evaluation of coefficients in Winslow)
- *                Typical range is 10-100.
+ * @param iterMax Number of outer iterations in the optimization (re-evaluation
+ * of coefficients in Winslow) Typical range is 10-100.
  * @param timeMax Maximum time budget for the smoothing (in seconds)
  *
  * @return True if the smoothing have been applied (mesh may still be tangled)
  */
-bool untangleGRegionMeshConstrained(GRegion* gr, int iterMax = 100, double timeMax = 9999.);
+bool untangleGRegionMeshConstrained(GRegion *gr, int iterMax = 100,
+                                    double timeMax = 9999.);
