50c1e432def33a68e9ef303c075dcf4528c177f4
tleyssens6 <61109046+tleyssens6@users.noreply.github.com>
2021-11-10 14:21:09 +0100

    added mean edge length in alpha shapes

--- Mesh/alphaShapes.cpp
+++ Mesh/alphaShapes.cpp
@@ -5,7 +5,7 @@
 #include "gmsh.h"
 #include <iostream>
 
-double alphaShape (const size_t *t, const std::vector<double> &p ){
+double alphaShape (const size_t *t, const std::vector<double> &p, const double hMean){
   double tetcircumcenter(double a[3], double b[3], double c[3], double d[3],
 			 double circumcenter[3], double *xi, double *eta,
 			 double *zeta);
@@ -20,11 +20,32 @@ double alphaShape (const size_t *t, const std::vector<double> &p ){
   double d[3] = {w[0], w[1], w[2]};
   tetcircumcenter(a,b,c,d,C, &xi, &eta, &zeta);
   double R = sqrt ((x[0]-C[0])*(x[0]-C[0])+(x[1]-C[1])*(x[1]-C[1])+(x[2]-C[2])*(x[2]-C[2]));
-  std::cout << "circumRadius : " << R << "\n";
-  return R;
+  return R/hMean;
 }
 
-//double meanEdgeLength()
+double twoPointsDistance(const double *p0, const double *p1){
+  return sqrt( (p0[0]-p1[0])*(p0[0]-p1[0]) + (p0[1]-p1[1])*(p0[1]-p1[1]) + (p0[2]-p1[2])*(p0[2]-p1[2]));
+}
+
+
+double meanEdgeLength (const std::vector<double> &p, const std::vector<size_t> &tetrahedra){
+  double hMean = 0;
+  for (size_t i=0; i<tetrahedra.size(); i+=4){
+    const double *p0 = &p[3*tetrahedra[i+0]];
+    const double *p1 = &p[3*tetrahedra[i+1]];
+    const double *p2 = &p[3*tetrahedra[i+2]];
+    const double *p3 = &p[3*tetrahedra[i+3]];
+    double d0 = twoPointsDistance(p0, p1);
+    double d1 = twoPointsDistance(p0, p2);
+    double d2 = twoPointsDistance(p0, p3);
+    double d3 = twoPointsDistance(p1, p2);
+    double d4 = twoPointsDistance(p1, p3);
+    double d5 = twoPointsDistance(p2, p3);
+    hMean += std::min({d0,d1,d2,d3,d4,d5});
+  }
+  hMean = hMean / (tetrahedra.size()/4);
+  return hMean;
+}
 
 
 static int _faces [4][3] = {{0,1,2}, {0,1,3}, {0,2,3}, {1,2,3}};
@@ -100,16 +121,10 @@ int computeTetNeighbors_ (const std::vector<size_t> &tetrahedra, std::vector<siz
     size_t *ft1 = &temp[5*counter];
 
     if (compareFourInt(ft0,ft1) == 0){
-      std::cout << "are equal!! \n";
       neigh[4*ft0[3]+ft0[4]] = ft1[3];
       neigh[4*ft1[3]+ft1[4]] = ft0[3];
       counter++;
     }
-  }
-  
-  std::cout << "size of neigh : " << neigh.size() << "\n";
-  for (int i=0; i<neigh.size(); i++){
-    std::cout << neigh[i] << "\n";
   }
 
   delete [] temp;  
@@ -124,9 +139,9 @@ int alphaShapes_ (const double threshold,
 		 std::vector<size_t> &neigh) {
 
   gmsh::model::mesh::tetrahedralize(pts, tetrahedra);
-  std::cout << tetrahedra.size() << "\n";
   for (size_t i = 0; i < tetrahedra.size(); i++)tetrahedra[i]--;
-  std::cout << "hereree \n";
+  
+  double hMean = meanEdgeLength(pts,tetrahedra);
   computeTetNeighbors_ (tetrahedra, neigh);
 
   std::vector<bool> _touched;
@@ -135,8 +150,7 @@ int alphaShapes_ (const double threshold,
 
   for (size_t i = 0; i < tetrahedra.size(); i+=4){
       size_t *t = &tetrahedra[i];
-      std::cout << "here \n";
-      if (alphaShape(t, pts) < threshold && _touched[i/4] == false){
+      if (alphaShape(t, pts, hMean) < threshold && _touched[i/4] == false){
         std::stack<size_t> _s;
         std::vector<size_t> _domain;
         std::vector<size_t> _boundary;
@@ -148,13 +162,12 @@ int alphaShapes_ (const double threshold,
           _s.pop();
           for (int j=0;j<4;j++){
             size_t tj = neigh[4*t+j];
-            std::cout << "tj : " << tj << "\n";
             if (tj == tetrahedra.size()){
               _boundary.push_back(t);
               _boundary.push_back(j);
             }
             else if (!_touched[tj]){
-              if (alphaShape(&tetrahedra[4*tj], pts) < threshold){
+              if (alphaShape(&tetrahedra[4*tj], pts, hMean) < threshold){
                 _s.push(tj);
                 _touched[tj] = true;
                 _domain.push_back(tj);	    
