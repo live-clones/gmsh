2d39174ef2383430257d60a11e67439a97a18cfa
tleyssens6 <61109046+tleyssens6@users.noreply.github.com>
2021-11-10 12:04:26 +0100

    alpha-shapes algo update

--- Mesh/alphaShapes.cpp
+++ Mesh/alphaShapes.cpp
@@ -3,6 +3,7 @@
 #include <stack>
 #include "alphaShapes.h"
 #include "gmsh.h"
+#include <iostream>
 
 double alphaShape (const size_t *t, const std::vector<double> &p ){
   double tetcircumcenter(double a[3], double b[3], double c[3], double d[3],
@@ -19,8 +20,11 @@ double alphaShape (const size_t *t, const std::vector<double> &p ){
   double d[3] = {w[0], w[1], w[2]};
   tetcircumcenter(a,b,c,d,C, &xi, &eta, &zeta);
   double R = sqrt ((x[0]-C[0])*(x[0]-C[0])+(x[1]-C[1])*(x[1]-C[1])+(x[2]-C[2])*(x[2]-C[2]));
+  std::cout << "circumRadius : " << R << "\n";
   return R;
 }
+
+//double meanEdgeLength()
 
 
 static int _faces [4][3] = {{0,1,2}, {0,1,3}, {0,2,3}, {1,2,3}};
@@ -67,10 +71,10 @@ int compareFourInt (const void *a , const void *b){
 
 int computeTetNeighbors_ (const std::vector<size_t> &tetrahedra, std::vector<size_t> &neigh){
   
-  neigh.resize(4*tetrahedra.size());
+  neigh.resize(tetrahedra.size());
   for (size_t i=0;i<neigh.size();i++)neigh[i] = tetrahedra.size();
   
-  size_t *temp = new size_t [20*tetrahedra.size()];
+  size_t *temp = new size_t [5*tetrahedra.size()];
   size_t counter = 0;
   for (size_t i = 0; i < tetrahedra.size(); i+=4){
     const size_t *t = &tetrahedra[i];
@@ -80,28 +84,34 @@ int computeTetNeighbors_ (const std::vector<size_t> &tetrahedra, std::vector<siz
       temp[counter++] = f[0];
       temp[counter++] = f[1];
       temp[counter++] = f[2];
-      temp[counter++] = i;      
-      temp[counter++] = j;      
+      temp[counter++] = i/4;      
+      temp[counter++] = j;
     }
   }  
 
-  qsort(temp, 4*tetrahedra.size(), 5*sizeof(size_t),compareFourInt);
+  qsort(temp, tetrahedra.size(), 5*sizeof(size_t),compareFourInt);
 
   // loop over faces
   counter  = 0;
   while (1){
-    if (counter == 4*tetrahedra.size())break;
+    if (counter == tetrahedra.size())break;
     size_t *ft0 = &temp[5*(counter++)];
-    if (counter == 4*tetrahedra.size())break;
+    if (counter == tetrahedra.size())break;
     size_t *ft1 = &temp[5*counter];
 
     if (compareFourInt(ft0,ft1) == 0){
+      std::cout << "are equal!! \n";
       neigh[4*ft0[3]+ft0[4]] = ft1[3];
       neigh[4*ft1[3]+ft1[4]] = ft0[3];
       counter++;
     }
   }
   
+  std::cout << "size of neigh : " << neigh.size() << "\n";
+  for (int i=0; i<neigh.size(); i++){
+    std::cout << neigh[i] << "\n";
+  }
+
   delete [] temp;  
   return 0;
 }
@@ -114,50 +124,53 @@ int alphaShapes_ (const double threshold,
 		 std::vector<size_t> &neigh) {
 
   gmsh::model::mesh::tetrahedralize(pts, tetrahedra);
-  for (size_t i = 0; i < 4*tetrahedra.size(); i++)tetrahedra[i]--;
-
+  std::cout << tetrahedra.size() << "\n";
+  for (size_t i = 0; i < tetrahedra.size(); i++)tetrahedra[i]--;
+  std::cout << "hereree \n";
   computeTetNeighbors_ (tetrahedra, neigh);
 
   std::vector<bool> _touched;
-  _touched.resize(tetrahedra.size());
+  _touched.resize(tetrahedra.size()/4);
   for (size_t i=0;i<_touched.size();i++)_touched[i] = false;
 
   for (size_t i = 0; i < tetrahedra.size(); i+=4){
-    size_t *t = &tetrahedra[i];
-    if (alphaShape(t, pts) > threshold && _touched[i] == false){
-      std::stack<size_t> _s;
-      std::vector<size_t> _domain;
-      std::vector<size_t> _boundary;
-      _s.push(i);
-      _touched[i] = true;
-      _domain.push_back(i);
-      while(!_s.empty()){
-	size_t t = _s.top();
-	_s.pop();
-	for (int j=0;j<4;j++){
-	  size_t tj = neigh[4*t+j];
-	  if (tj == tetrahedra.size()){
-	    _boundary.push_back(t);
-	    _boundary.push_back(j);
-	  }
-	  else if (!_touched[tj]){
-	    if (alphaShape(&tetrahedra[tj], pts) > threshold){
-	      _s.push(tj);
-	      _touched[tj] = true;
-	      _domain.push_back(tj);	    
-	    }	    
-	    else {
-	      _boundary.push_back(t);
-	      _boundary.push_back(j);	      
-	    }
-	  }
-	}	  
+      size_t *t = &tetrahedra[i];
+      std::cout << "here \n";
+      if (alphaShape(t, pts) < threshold && _touched[i/4] == false){
+        std::stack<size_t> _s;
+        std::vector<size_t> _domain;
+        std::vector<size_t> _boundary;
+        _s.push(i/4);
+        _touched[i] = true;
+        _domain.push_back(i/4);
+        while(!_s.empty()){
+          size_t t = _s.top();
+          _s.pop();
+          for (int j=0;j<4;j++){
+            size_t tj = neigh[4*t+j];
+            std::cout << "tj : " << tj << "\n";
+            if (tj == tetrahedra.size()){
+              _boundary.push_back(t);
+              _boundary.push_back(j);
+            }
+            else if (!_touched[tj]){
+              if (alphaShape(&tetrahedra[4*tj], pts) < threshold){
+                _s.push(tj);
+                _touched[tj] = true;
+                _domain.push_back(tj);	    
+              }	    
+              else {
+                _boundary.push_back(t);
+                _boundary.push_back(j);	      
+              }
+            }
+          }	  
+        }
+        boundaries.push_back(_boundary);
+        domains.push_back(_domain);
       }
-      boundaries.push_back(_boundary);
-      domains.push_back(_domain);
-    }
   }
-  for (size_t i = 0; i < 4*tetrahedra.size(); i++)tetrahedra[i]++;
+  for (size_t i = 0; i < tetrahedra.size(); i++)tetrahedra[i]++;
   return 0;
 }
 
