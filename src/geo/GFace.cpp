e362433bcdaee7c4e3cd2c45c1551393230e2e52
Christophe Geuzaine <cgeuzaine@uliege.be>
2022-01-25 10:17:25 +0100

    error -> warning

--- src/geo/GFace.cpp
+++ src/geo/GFace.cpp
@@ -1084,9 +1084,9 @@ void GFace::XYZtoUV(double X, double Y, double Z, double &U, double &V,
   if(!onSurface) return;
 
   if(relax < 1.e-3)
-    Msg::Error("Inverse surface mapping could not converge");
+    Msg::Warning("Inverse surface mapping could not converge");
   else {
-    Msg::Info("point %g %g %g : Relaxation factor = %g", X, Y, Z, 0.75 * relax);
+    Msg::Info("Point %g %g %g: Relaxation factor = %g", X, Y, Z, 0.75 * relax);
     XYZtoUV(X, Y, Z, U, V, 0.75 * relax, onSurface, convTestXYZ);
   }
 }
