785bfd4e4de712dcb98ab724e8e37b98731f3109
Jonathan Lambrechts <jonathan.lambrechts@uclouvain.be>
2022-08-29 09:45:24 +0200

    sdktowheel : f-strings

--- utils/pypi/sdktowheel.py
+++ utils/pypi/sdktowheel.py
@@ -23,9 +23,9 @@ sdk_dir = sys.argv[1]
 plat_name = sys.argv[2]
 setup_dir = os.path.dirname(__file__)
 
-dist_info_dir = glob.glob(sdk_dir + '/lib/gmsh*.dist-info')[0]
-dist_info_dir = dist_info_dir.removeprefix(sdk_dir + '/lib/')
-match = re.match("(.*)-(.*)\.dist-info", dist_info_dir)
+dist_info_dir = glob.glob(os.path.join(sdk_dir, 'lib', 'gmsh*.dist-info'))[0]
+dist_info_dir = dist_info_dir.removeprefix(os.path.join(sdk_dir, 'lib', ''))
+match = re.match('(.*)-(.*)\.dist-info', dist_info_dir)
 name = match.group(1)
 version = match.group(2)
 
@@ -34,7 +34,7 @@ wheel_name = "-".join([name, version ,'py2.py3', 'none', plat_name])
 libversion = ".".join(version.split(".")[:2])
 
 with tempfile.TemporaryDirectory() as wheel_dir:
-    data_dir = os.path.join(wheel_dir, 'gmsh-' + version + '.data')
+    data_dir = os.path.join(wheel_dir, f'gmsh-{version}.data')
 
     os.makedirs(os.path.join(data_dir, 'data', 'lib'))
     os.makedirs(os.path.join(data_dir, 'scripts'))
@@ -49,18 +49,18 @@ with tempfile.TemporaryDirectory() as wheel_dir:
     shutil.copy(os.path.join(setup_dir, 'gmsh'),
                 os.path.join(wheel_dir, data_dir, 'scripts'))
 
-    if os.path.isfile(os.path.join(sdk_dir, 'lib', 'gmsh-' + libversion + '.dll')):
-        shutil.copy(os.path.join(sdk_dir, 'lib', 'gmsh-' + libversion + '.dll'),
+    if os.path.isfile(os.path.join(sdk_dir, 'lib', f'gmsh-{libversion}.dll')):
+        shutil.copy(os.path.join(sdk_dir, 'lib', f'gmsh-{libversion}.dll'),
                     os.path.join(data_dir, 'data', 'lib'))
         shutil.copy(os.path.join(sdk_dir, 'lib', 'gmsh.lib'),
                     os.path.join(data_dir, 'data', 'lib'))
         shutil.copy(os.path.join(setup_dir, "gmsh.bat"),
                     os.path.join(data_dir, 'scripts'))
-    elif os.path.isfile(os.path.join(sdk_dir, 'lib', 'libgmsh.so.' + libversion)):
-        shutil.copy(os.path.join(sdk_dir, 'lib', 'libgmsh.so.' + libversion),
+    elif os.path.isfile(os.path.join(sdk_dir, 'lib', f'libgmsh.so.{libversion}')):
+        shutil.copy(os.path.join(sdk_dir, 'lib', f'libgmsh.so.{libversion}'),
                     os.path.join(data_dir, 'data', 'lib'))
-    elif os.path.isfile(os.path.join(sdk_dir, 'lib', 'libgmsh.' + libversion + '.dylib')):
-        shutil.copy(os.path.join(sdk_dir, 'lib', 'libgmsh.' + libversion + '.dylib'),
+    elif os.path.isfile(os.path.join(sdk_dir, 'lib', f'libgmsh.{libversion}.dylib')):
+        shutil.copy(os.path.join(sdk_dir, 'lib', f'libgmsh.{libversion}.dylib'),
                     os.path.join(data_dir, 'data', 'lib'))
     else :
         print('library not found')
@@ -89,17 +89,16 @@ with tempfile.TemporaryDirectory() as wheel_dir:
                 content = f.read()
                 checksum = hashlib.sha256(content).hexdigest()
                 length = len(content)
-                records.append(','.join([
-                    os.path.relpath(fpath, wheel_dir),
-                    "sha256=" + checksum,
-                    str(len(content))]))
+                relpath = os.path.relpath(fpath, wheel_dir)
+                records.append(f'{relpath},sha256={checksum},{length}')
 
-    with open(os.path.join(wheel_dir, dist_info_dir, 'RECORD'), 'a') as f:
+    with open(os.path.join(wheel_dir, dist_info_dir, 'RECORD'), 'w') as f:
         for r in records:
             f.write(r+"\n")
-        f.write(os.path.join(wheel_dir, dist_info_dir, 'RECORD') + ',,\n')
+        relpath = os.path.join(wheel_dir, dist_info_dir, 'RECORD')
+        f.write(f'{relpath},,\n')
 
     # zip
     shutil.make_archive(wheel_name, 'zip', wheel_dir)
-    shutil.move(wheel_name + '.zip', wheel_name + '.whl')
-    print(wheel_name + '.whl successfully built.')
+    shutil.move(f'{wheel_name}.zip', f'{wheel_name}.whl')
+    print(f'{wheel_name}.whl successfully built.')
