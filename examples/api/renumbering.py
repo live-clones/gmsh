d7a18a72d6587ba2214827e6a5095fa990006516
Christophe Geuzaine <cgeuzaine@uliege.be>
2023-07-04 15:21:09 +0200

    respect -nopopup

--- examples/api/renumbering.py
+++ examples/api/renumbering.py
@@ -1,4 +1,6 @@
 import gmsh
+import sys
+
 gmsh.initialize()
 gmsh.option.setNumber("Mesh.MeshSizeMax", 0.1)
 gmsh.option.setNumber("Mesh.MeshSizeMin", 0.1)
@@ -8,14 +10,17 @@ gmsh.model.occ.addRectangle(0,0,0, 1,1)
 gmsh.model.occ.synchronize()
 
 gmsh.model.mesh.generate()
-gmsh.fltk.run()
+if '-nopopup' not in sys.argv:
+    gmsh.fltk.run()
 
 old, new = gmsh.model.mesh.computeRenumbering('RCMK')
 gmsh.model.mesh.renumberNodes(old, new)
-gmsh.fltk.run()
+if '-nopopup' not in sys.argv:
+    gmsh.fltk.run()
 
 old, new = gmsh.model.mesh.computeRenumbering('Hilbert')
 gmsh.model.mesh.renumberNodes(old, new)
-gmsh.fltk.run()
+if '-nopopup' not in sys.argv:
+    gmsh.fltk.run()
 
 gmsh.finalize()
