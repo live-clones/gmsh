1d29154e2d0ea4de0dfaa770679995fb41529396
Christophe Geuzaine <cgeuzaine@uliege.be>
2023-07-04 09:43:44 +0200

    size_t -> std::size_t

--- src/mesh/meshRenumber.cpp
+++ src/mesh/meshRenumber.cpp
@@ -13,20 +13,20 @@
 // E. Béchet, C. Geuzaine, Université de Liège
 
 struct SortClass {
-  const size_t *_degree;
-  bool operator()(const size_t i, const size_t j)
+  const std::size_t *_degree;
+  bool operator()(const std::size_t i, const std::size_t j)
   {
     return _degree[i] < _degree[j];
   }
 };
 
-static void RCM(std::vector<size_t> &sorted, const size_t *const row,
-                const size_t *const indices)
+static void RCM(std::vector<std::size_t> &sorted, const std::size_t *const row,
+                const std::size_t *const indices)
 {
-  std::vector<size_t> min; // long long for padding (64bits)
-  std::vector<size_t> Nmin;
-  std::vector<size_t> degree;
-  std::vector<size_t> newIndices;
+  std::vector<std::size_t> min; // long long for padding (64bits)
+  std::vector<std::size_t> Nmin;
+  std::vector<std::size_t> degree;
+  std::vector<std::size_t> newIndices;
 #pragma omp parallel num_threads(Msg::GetNumThreads())
   {
     unsigned int numThreads = Msg::GetNumThreads();
@@ -69,9 +69,9 @@ static void RCM(std::vector<size_t> &sorted, const size_t *const row,
   }
 
   sorted[Nmin[0]] = sorted.size();
-  size_t nbrNodesSorted = sorted.size() - 1;
+  std::size_t nbrNodesSorted = sorted.size() - 1;
 
-  std::queue<size_t> queue;
+  std::queue<std::size_t> queue;
   for(auto i = row[Nmin[0]]; i < row[Nmin[0] + 1]; ++i) {
     queue.push(newIndices[i]);
   }
@@ -87,8 +87,8 @@ static void RCM(std::vector<size_t> &sorted, const size_t *const row,
       queue.pop();
     }
     else {
-      size_t min = row[sorted.size()] - row[0];
-      size_t Nmin = 0;
+      std::size_t min = row[sorted.size()] - row[0];
+      std::size_t Nmin = 0;
       for(auto i = 0ULL; i < sorted.size(); ++i) {
         if(min > degree[i] && sorted[i] == 0) {
           min = degree[i];
@@ -100,13 +100,13 @@ static void RCM(std::vector<size_t> &sorted, const size_t *const row,
   }
 }
 
-int meshRenumber_Vertices_RCMK(const std::vector<size_t> &elementTags,
-                               std::map<size_t, size_t> &permutations)
+int meshRenumber_Vertices_RCMK(const std::vector<std::size_t> &elementTags,
+                               std::map<std::size_t, std::size_t> &permutations)
 {
   GModel *gm = GModel ::current();
   permutations.clear();
 
-  std::vector<MElement*> elements;
+  std::vector<MElement *> elements;
   if(elementTags.empty()) {
     std::vector<GEntity *> entities;
     gm->getEntities(entities);
@@ -120,13 +120,13 @@ int meshRenumber_Vertices_RCMK(const std::vector<size_t> &elementTags,
     for(auto n : elementTags) elements.push_back(gm->getMeshElementByTag(n));
   }
 
-  std::map<MVertex *, size_t> initial_numbering;
-  size_t count = 0;
-  std::vector<std::pair<size_t, size_t>> coords;
-  size_t numbers[1000];
+  std::map<MVertex *, std::size_t> initial_numbering;
+  std::size_t count = 0;
+  std::vector<std::pair<std::size_t, std::size_t>> coords;
+  std::size_t numbers[1000];
   std::map<std::size_t, MVertex *> inverse_numbering;
   for(auto e : elements) {
-    for(size_t i = 0; i < e->getNumVertices(); i++) {
+    for(std::size_t i = 0; i < e->getNumVertices(); i++) {
       MVertex *v = e->getVertex(i);
       auto it = initial_numbering.find(v);
       if(it == initial_numbering.end()) {
@@ -137,8 +137,8 @@ int meshRenumber_Vertices_RCMK(const std::vector<size_t> &elementTags,
       else
         numbers[i] = it->second;
     }
-    for(size_t i = 0; i < e->getNumVertices(); i++) {
-      for(size_t j = 0; j < e->getNumVertices(); j++) {
+    for(std::size_t i = 0; i < e->getNumVertices(); i++) {
+      for(std::size_t j = 0; j < e->getNumVertices(); j++) {
         coords.push_back(std::make_pair(numbers[i], numbers[j]));
       }
     }
@@ -148,7 +148,7 @@ int meshRenumber_Vertices_RCMK(const std::vector<size_t> &elementTags,
   auto last = std::unique(coords.begin(), coords.end());
   coords.erase(last, coords.end());
 
-  std::vector<size_t> ai, aj;
+  std::vector<std::size_t> ai, aj;
   ai.push_back(0);
   std::size_t line = coords[0].first;
   for(auto c : coords) {
@@ -160,7 +160,7 @@ int meshRenumber_Vertices_RCMK(const std::vector<size_t> &elementTags,
   }
   ai.push_back(aj.size());
 
-  std::vector<size_t> sorted(count);
+  std::vector<std::size_t> sorted(count);
 
   RCM(sorted, &ai[0], &aj[0]);
 
@@ -171,13 +171,14 @@ int meshRenumber_Vertices_RCMK(const std::vector<size_t> &elementTags,
   return 0;
 }
 
-int meshRenumber_Vertices_Hilbert(const std::vector<size_t> &elementTags,
-                                  std::map<size_t, size_t> &permutations)
+int meshRenumber_Vertices_Hilbert(
+  const std::vector<std::size_t> &elementTags,
+  std::map<std::size_t, std::size_t> &permutations)
 {
   GModel *gm = GModel ::current();
   permutations.clear();
 
-  std::set<MVertex*> allv;
+  std::set<MVertex *> allv;
   if(elementTags.empty()) {
     std::vector<GEntity *> entities;
     gm->getEntities(entities);
@@ -196,7 +197,7 @@ int meshRenumber_Vertices_Hilbert(const std::vector<size_t> &elementTags,
     }
   }
 
-  std::vector<MVertex*> v(allv.begin(), allv.end());
+  std::vector<MVertex *> v(allv.begin(), allv.end());
   SortHilbert(v);
   for(std::size_t i = 0; i < v.size(); i++) {
     permutations[v[i]->getNum()] = i + 1;
