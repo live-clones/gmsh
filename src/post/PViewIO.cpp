live-clones/gmsh

commit 9ba11ce16322321edb53ce9ca50c7a9dded5cb1d

enable unknown sections (and thus model attributes) after post-processing data
TODO: we should break PView::readMSH() into functions and call them directly in GModel::readMSH() - this would remove code duplication and remove the extra node/element parsing when reading post-processing data

src/post/PViewIO.cpp
@@ -84,6 +84,9 @@ bool PView::readPOS(const std::string &fileName, int fileIndex)
 bool PView::readMSH(const std::string &fileName, int fileIndex,
                     int partitionToRead)
 {
+  // TODO: this should be replaced by functions reading each section, called
+  // directly from the GModel::readMSH()
+
   FILE *fp = Fopen(fileName.c_str(), "rb");
   if(!fp) {
     Msg::Error("Unable to open file '%s'", fileName.c_str());
@@ -107,6 +110,9 @@ bool PView::readMSH(const std::string &fileName, int fileIndex,
     }
 
     if(feof(fp)) break;
+
+    std::string sectionName(&str[1]);
+    std::string endSectionName = "End" + sectionName;
 
     if(!strncmp(&str[1], "MeshFormat", 10)) {
       double version;
@@ -132,6 +138,16 @@ bool PView::readMSH(const std::string &fileName, int fileIndex,
           Msg::Debug("Swapping bytes from binary file");
         }
       }
+    }
+    else if(!strncmp(&str[1], "PhysicalNames", 13) ||
+            !strncmp(&str[1], "Entities", 8) ||
+            !strncmp(&str[1], "PartitionedEntities", 19) ||
+            !strncmp(&str[1], "Nodes", 5) ||
+            !strncmp(&str[1], "Elements", 8) ||
+            !strncmp(&str[1], "Periodic", 8) ||
+            !strncmp(&str[1], "GhostElements", 13) ||
+            !strncmp(&str[1], "Parametrizations", 16)) {
+      // skip
     }
     else if(!strncmp(&str[1], "InterpolationScheme", 19)) {
       std::string name;
@@ -311,10 +327,29 @@ bool PView::readMSH(const std::string &fileName, int fileIndex,
         }
       }
     }
+    else if(strlen(&str[1]) > 0){
+      if(!CTX::instance()->mesh.ignoreUnknownSections) {
+        sectionName.pop_back();
+        Msg::Info("Storing section $%s as model attribute", sectionName.c_str());
+        std::vector<std::string> section;
+        while(1) {
+          if(!fgets(str, sizeof(str), fp) || feof(fp) ||
+             !strncmp(&str[1], endSectionName.c_str(), endSectionName.size())) {
+            break;
+          }
+          std::string s(str);
+          if(s.back() == '\n') s.pop_back();
+          if(s.back() == '\r') s.pop_back();
+          section.push_back(s);
+        }
+        model->getAttributes()[sectionName] = section;
+      }
+    }
 
-    do {
-      if(!fgets(str, sizeof(str), fp) || feof(fp)) break;
-    } while(str[0] != '$');
+    while(strncmp(&str[1], endSectionName.c_str(), endSectionName.size())) {
+      if(!fgets(str, sizeof(str), fp) || feof(fp)) { break; }
+    }
+    str[0] = 'a';
   }
 
   fclose(fp);

src/geo/GModelIO_MSH2.cpp
@@ -611,19 +611,26 @@ int GModel::_readMSH2(const std::string &name)
       }
     }
     else if(!strncmp(&str[1], "NodeData", 8)) {
-      // there's some nodal post-processing data to read later on, so
-      // cache the vertex indexing data
+      // there's some nodal post-processing data to read later on, so cache the
+      // vertex indexing data
       if(vertexVector.size())
         _vertexVectorCache = vertexVector;
       else
         _vertexMapCache = vertexMap;
       postpro = true;
+      // TODO: the break prevents other sections to be read and makes the
+      // post-pro reader slower - we should do the reading here instead of using
+      // PView::readMSH()
       break;
     }
     else if(!strncmp(&str[1], "ElementData", 11) ||
-            !strncmp(&str[1], "ElementNodeData", 15)) {
+            !strncmp(&str[1], "ElementNodeData", 15) ||
+            !strncmp(&str[1], "InterpolationScheme", 19)) {
       // there's some element post-processing data to read later on
       postpro = true;
+      // TODO: the break prevents other sections to be read and makes the
+      // post-pro reader slower - we should do the reading here instead of using
+      // PView::readMSH()
       break;
     }
     else if(strlen(&str[1]) > 0){

src/geo/GModelIO_MSH3.cpp
@@ -526,8 +526,13 @@ int GModel::_readMSH3(const std::string &name)
     // Post-processing sections
     else if(!strncmp(&str[1], "NodeData", 8) ||
             !strncmp(&str[1], "ElementData", 11) ||
-            !strncmp(&str[1], "ElementNodeData", 15)) {
+            !strncmp(&str[1], "ElementNodeData", 15) ||
+            !strncmp(&str[1], "InterpolationScheme", 19)) {
+      // there's some element post-processing data to read later on
       postpro = true;
+      // TODO: the break prevents other sections to be read and makes the
+      // post-pro reader slower - we should do the reading here instead of using
+      // PView::readMSH()
       break;
     }
 

src/geo/GModelIO_MSH4.cpp
@@ -1414,8 +1414,13 @@ int GModel::_readMSH4(const std::string &name)
     }
     else if(!strncmp(&str[1], "NodeData", 8) ||
             !strncmp(&str[1], "ElementData", 11) ||
-            !strncmp(&str[1], "ElementNodeData", 15)) {
+            !strncmp(&str[1], "ElementNodeData", 15) ||
+            !strncmp(&str[1], "InterpolationScheme", 19)) {
+      // there's some element post-processing data to read later on
       postpro = true;
+      // TODO: the break prevents other sections to be read and makes the
+      // post-pro reader slower - we should do the reading here instead of using
+      // PView::readMSH()
       break;
     }
     else if(strlen(&str[1]) > 0){

commit 0932f41fb54517f3c3114e483dbffc238054ea45

tentative fix for regression introduced in 9ba11ce163 w.r.t. handling unknown sections after post-processing data (#2934)

src/post/PViewIO.cpp
@@ -116,7 +116,7 @@ bool PView::readMSH(const std::string &fileName, int fileIndex,
 
     if(!strncmp(&str[1], "MeshFormat", 10)) {
       double version;
-      if(!fgets(str, sizeof(str), fp)) {
+      if(!fgets(str, sizeof(str), fp) || feof(fp)) {
         fclose(fp);
         return false;
       }
@@ -327,8 +327,18 @@ bool PView::readMSH(const std::string &fileName, int fileIndex,
         }
       }
     }
-    else if(strlen(&str[1]) > 0){
-      if(!CTX::instance()->mesh.ignoreUnknownSections) {
+    else if(strncmp(&str[1], "End", 3) && strlen(&str[1]) > 0){
+      // str could contain garbage as we might skip binary data - one more
+      // reason to replace PView::readMSH by functions reading each section,
+      // called directly from the GModel::readMSH()
+      bool skip = false;
+      for(int j = 0; j < strlen(&str[1]); j++) {
+        if(!isascii((&str[1])[j])) {
+          skip = true;
+          break;
+        }
+      }
+      if(!skip && !CTX::instance()->mesh.ignoreUnknownSections) {
         sectionName.pop_back();
         Msg::Info("Storing section $%s as model attribute", sectionName.c_str());
         std::vector<std::string> section;
@@ -346,10 +356,9 @@ bool PView::readMSH(const std::string &fileName, int fileIndex,
       }
     }
 
-    while(strncmp(&str[1], endSectionName.c_str(), endSectionName.size())) {
-      if(!fgets(str, sizeof(str), fp) || feof(fp)) { break; }
-    }
-    str[0] = 'a';
+    do {
+      if(!fgets(str, sizeof(str), fp) || feof(fp)) break;
+    } while(str[0] != '$');
   }
 
   fclose(fp);