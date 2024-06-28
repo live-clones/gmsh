89ebe53766d89489d3c76373875ddf049baf7c80
Christophe Geuzaine <cgeuzaine@uliege.be>
2024-06-28 09:21:07 +0200

    latest changes to important for a patch release - bumping minor version

--- src/common/GmshMessage.cpp
+++ src/common/GmshMessage.cpp
@@ -51,6 +51,10 @@
 #include "extraDialogs.h"
 #endif
 
+#if defined(HAVE_OCC)
+#include <Standard_Version.hxx>
+#endif
+
 #if defined(_OPENMP)
 #include <omp.h>
 #endif
@@ -393,15 +397,18 @@ void Msg::Exit(int level, bool forceLevel)
 #if defined(HAVE_MPI)
   if(level) MPI_Abort(MPI_COMM_WORLD, level);
 #endif
+
+#if defined(HAVE_OCC) && (OCC_VERSION_HEX > 0x070800)
 #if defined(__APPLE__) || defined(__linux__)
-  // temporary workaround for recent versions of OpenCASCADE (>= 7.8) which on
-  // macOS and linux invoke a global destructor in the STEP module that
-  // sometimes lead to a segfault - using _exit() bypasses the global
-  // destructors
+#warning "Using _exit() instead of exit() as workaround for OCC >= 7.8 STEP bug"
+  // workaround for recent versions of OpenCASCADE (>= 7.8) which on macOS and
+  // linux invoke a global destructor in the STEP module that sometimes leads to
+  // a segfault - using _exit() bypasses the global destructor
   _exit((forceLevel || level) ? level : _atLeastOneErrorInRun);
-#else
-  exit((forceLevel || level) ? level : _atLeastOneErrorInRun);
 #endif
+#endif
+
+  exit((forceLevel || level) ? level : _atLeastOneErrorInRun);
 }
 
 static int streamIsFile(FILE *stream)
