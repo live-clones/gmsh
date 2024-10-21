01d2e72fece0a8847e3f036fd0aec0437b6e6125
Boris Martin <boris.martin@uliege.be>
2024-10-21 23:13:45 +0200

    Added overlap entities (1D, 2D)

---
+++ src/geo/overlapEdge.h
@@ -0,0 +1,65 @@
+// Gmsh - Copyright (C) 1997-2024 C. Geuzaine, J.-F. Remacle
+//
+// See the LICENSE.txt file in the Gmsh root directory for license information.
+// Please report all issues on https://gitlab.onelab.info/gmsh/gmsh/issues.
+
+#ifndef OVERLAP_EDGE_H
+#define OVERLAP_EDGE_H
+
+#include "GModel.h"
+#include "partitionEdge.h"
+#include "MLine.h"
+
+class overlapEdgeManager;
+
+class overlapEdge : public discreteEdge {
+private:
+  std::pair<int, int> _partition; // Overlap of first as a subset of second
+  partitionEdge *_overlapOf,
+    *_overlapOn; // must be the two partitioned entities, with the same parent.
+  overlapEdgeManager *_manager;
+
+public:
+  overlapEdge(GModel *model, int num, partitionEdge *of, partitionEdge *on)
+    : discreteEdge(model, num), _overlapOf(of), _overlapOn(on), _manager(nullptr)
+  {
+    if(of->getParentEntity() != on->getParentEntity()) {
+      Msg::Error("Overlap edges must have the same parent entity");
+    }
+    if(of->getPartitions().size() != 1 || on->getPartitions().size() != 1) {
+      Msg::Error("Overlap edges must have exactly one partition");
+    }
+    _partition = std::make_pair(of->getPartitions()[0], on->getPartitions()[0]);
+  }
+
+  partitionEdge *getOverlapOf() const { return _overlapOf; }
+  partitionEdge *getOverlapOn() const { return _overlapOn; }
+  virtual GeomType geomType() const override { return OverlapCurve; }
+  int of() const { return _partition.first; }
+  int on() const { return _partition.second; }
+  overlapEdgeManager *getManager() const { return _manager; }
+  void setManager(overlapEdgeManager *manager) { _manager = manager; }
+
+  // All entites which must be exported for this to have all needed nodes
+  std::set<GEntity *> getCoveredEntities() const
+  {
+    std::set<GEntity *> entities;
+    entities.insert(_overlapOn);
+    for(MLine *line : this->lines) {
+      for(unsigned n = 0; n < 2; n++) {
+        MVertex *v = line->getVertex(n);
+        entities.insert(v->onWhat());
+      }
+    }
+    return entities;
+  }
+
+  virtual ~overlapEdge() { deleteMesh(); }
+  virtual void deleteMesh() override
+  {
+    // Do not free elements, they are owned by the partitioned entities
+    this->lines.clear();
+  }
+};
+
+#endif
---
+++ src/geo/overlapFace.h
@@ -0,0 +1,66 @@
+// Gmsh - Copyright (C) 1997-2024 C. Geuzaine, J.-F. Remacle
+//
+// See the LICENSE.txt file in the Gmsh root directory for license information.
+// Please report all issues on https://gitlab.onelab.info/gmsh/gmsh/issues.
+
+#ifndef OVERLAP_FACE_H
+#define OVERLAP_FACE_H
+
+#include "GModel.h"
+#include "partitionFace.h"
+
+class overlapFaceManager;
+
+class overlapFace : public discreteFace {
+private:
+  std::pair<int, int> _partition; // Overlap of first as a subset of second
+  partitionFace *_overlapOf,
+    *_overlapOn; // must be the two partitioned entities, with the same parent.
+  overlapFaceManager *_manager;
+
+public:
+  overlapFace(GModel *model, int num, partitionFace *of, partitionFace *on)
+    : discreteFace(model, num), _overlapOf(of), _overlapOn(on), _manager(nullptr)
+  {
+    if(of->getParentEntity() != on->getParentEntity()) {
+      Msg::Error("Overlap faces must have the same parent entity");
+    }
+    if(of->getPartitions().size() != 1 || on->getPartitions().size() != 1) {
+      Msg::Error("Overlap faces must have exactly one partition");
+    }
+    _partition = std::make_pair(of->getPartitions()[0], on->getPartitions()[0]);
+  }
+
+  partitionFace *getOverlapOf() const { return _overlapOf; }
+  partitionFace *getOverlapOn() const { return _overlapOn; }
+  virtual GeomType geomType() const override { return OverlapSurface; }
+  int of() const { return _partition.first; }
+  int on() const { return _partition.second; }
+  overlapFaceManager *getManager() const { return _manager; }
+  void setManager(overlapFaceManager *manager) { _manager = manager; }
+
+  // All entites which must be exported for this to have all needed nodes
+  std::set<GEntity *> getCoveredEntities() const
+  {
+    std::set<GEntity *> entities;
+    entities.insert(_overlapOn);
+    for(MTriangle *t : this->triangles) {
+      for(unsigned n = 0; n < 3; n++) {
+        MVertex *v = t->getVertex(n);
+        entities.insert(v->onWhat());
+      }
+    }
+    return entities;
+  }
+
+  virtual ~overlapFace() { deleteMesh(); }
+  virtual void deleteMesh() override
+  {
+    // Do not free elements, they are owned by the partitioned entities
+    this->triangles.clear();
+    this->quadrangles.clear();
+    this->polygons.clear();
+  }
+};
+
+#endif
