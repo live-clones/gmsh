a15ff9819155b88fa20db89f8f54d90e1e409722
root <root@DESKTOP-IB1IV6N.>
2025-02-03 20:33:17 +0100

    Fix VTK reader to correctly import CELL_DATA for physical labels

--- src/geo/GModelIO_VTK.cpp
+++ src/geo/GModelIO_VTK.cpp
@@ -137,21 +137,18 @@ int GModel::readVTK(const std::string &name, bool bigEndian)
   }
 
   char buffer[256], buffer2[256];
+  // physicals: one per dimension (0: points, 1: curves, 2: surfaces, 3: volumes)
   std::map<int, std::map<int, std::string> > physicals[4];
 
-  if(!fgets(buffer, sizeof(buffer), fp)) {
-    fclose(fp);
-    return 0;
-  } // version line
-  if(!fgets(buffer, sizeof(buffer), fp)) {
-    fclose(fp);
-    return 0;
-  } // title
+  // Read header lines (version and title)
+  if(!fgets(buffer, sizeof(buffer), fp)) { fclose(fp); return 0; }
+  if(!fgets(buffer, sizeof(buffer), fp)) { fclose(fp); return 0; }
 
-  if(fscanf(fp, "%s", buffer) != 1) // ASCII or BINARY
+  if(fscanf(fp, "%s", buffer) != 1)
     Msg::Error("Failed reading buffer");
   bool binary = false;
-  if(!strcmp(buffer, "BINARY")) binary = true;
+  if(!strcmp(buffer, "BINARY"))
+    binary = true;
 
   if(fscanf(fp, "%s %s", buffer, buffer2) != 2) {
     fclose(fp);
@@ -169,9 +166,9 @@ int GModel::readVTK(const std::string &name, bool bigEndian)
     return 0;
   }
 
-  // read mesh vertices
+  // Read mesh vertices
   int numVertices;
-  if(fscanf(fp, "%s %d %s", buffer, &numVertices, buffer2) != 3) return 0;
+  if(fscanf(fp, "%s %d %s", buffer, &numVertices, buffer2) != 3) { fclose(fp); return 0; }
   if(strcmp(buffer, "POINTS") || !numVertices) {
     Msg::Warning("No points in dataset");
     fclose(fp);
@@ -189,36 +186,27 @@ int GModel::readVTK(const std::string &name, bool bigEndian)
   }
   Msg::Info("Reading %d points", numVertices);
   std::vector<MVertex *> vertices(numVertices);
-  for(int i = 0; i < numVertices; i++) {
+  for(int i = 0; i < numVertices; i++){
     double xyz[3];
     if(binary) {
       if(datasize == sizeof(float)) {
         float f[3];
-        if(fread(f, sizeof(float), 3, fp) != 3) {
-          fclose(fp);
-          return 0;
-        }
+        if(fread(f, sizeof(float), 3, fp) != 3) { fclose(fp); return 0; }
         if(!bigEndian) SwapBytes((char *)f, sizeof(float), 3);
         for(int j = 0; j < 3; j++) xyz[j] = f[j];
       }
       else {
-        if(fread(xyz, sizeof(double), 3, fp) != 3) {
-          fclose(fp);
-          return 0;
-        }
+        if(fread(xyz, sizeof(double), 3, fp) != 3) { fclose(fp); return 0; }
         if(!bigEndian) SwapBytes((char *)xyz, sizeof(double), 3);
       }
     }
     else {
-      if(fscanf(fp, "%lf %lf %lf", &xyz[0], &xyz[1], &xyz[2]) != 3) {
-        fclose(fp);
-        return 0;
-      }
+      if(fscanf(fp, "%lf %lf %lf", &xyz[0], &xyz[1], &xyz[2]) != 3) { fclose(fp); return 0; }
     }
     vertices[i] = new MVertex(xyz[0], xyz[1], xyz[2]);
   }
 
-  // read mesh elements
+  // Read mesh elements (cells)
   int numElements, totalNumInt;
   if(fscanf(fp, "%s %d %d", buffer, &numElements, &totalNumInt) != 3) {
     fclose(fp);
@@ -242,172 +230,270 @@ int GModel::readVTK(const std::string &name, bool bigEndian)
     return 0;
   }
 
-  int iPoint = getMaxElementaryNumber(0) + 1;
-  int iCurve = getMaxElementaryNumber(1) + 1;
+  // Initialize counters using current maximum elementary numbers:
+  int iPoint   = getMaxElementaryNumber(0) + 1;
+  int iCurve   = getMaxElementaryNumber(1) + 1;
   int iSurface = getMaxElementaryNumber(2) + 1;
-  int iVolume = getMaxElementaryNumber(3) + 1;
+  int iVolume  = getMaxElementaryNumber(3) + 1;
 
+  // We'll build our elements in two ways:
+  // (1) In the "elements" array (an array of maps for different element types, as in the original code)
   std::map<int, std::vector<MElement *> > elements[8];
+  // (2) In vectors that preserve the VTK cell order:
+  std::vector<MElement*> elementOrder;       // pointer to each element (in file order)
+  std::vector<int>        elementDim;         // geometric dimension (0,1,2,3) of each element
+  std::vector<int>        elementOldId;       // the elementary id assigned at creation (from switch-case)
 
-  if(haveCells) {
-    std::vector<std::vector<MVertex *> > cells(numElements);
-    for(std::size_t i = 0; i < cells.size(); i++) {
-      int numVerts, n[100];
-      if(binary) {
-        if(fread(&numVerts, sizeof(int), 1, fp) != 1) {
-          fclose(fp);
-          return 0;
-        }
-        if(!bigEndian) SwapBytes((char *)&numVerts, sizeof(int), 1);
-        if((int)fread(n, sizeof(int), numVerts, fp) != numVerts) {
-          fclose(fp);
-          return 0;
-        }
-        if(!bigEndian) SwapBytes((char *)n, sizeof(int), numVerts);
-      }
-      else {
-        if(fscanf(fp, "%d", &numVerts) != 1) {
-          fclose(fp);
-          return 0;
-        }
-        for(int j = 0; j < numVerts; j++) {
-          if(fscanf(fp, "%d", &n[j]) != 1) {
-            fclose(fp);
-            return 0;
-          }
-        }
-      }
-      for(int j = 0; j < numVerts; j++) {
-        if(n[j] >= 0 && n[j] < (int)vertices.size())
-          cells[i].push_back(vertices[n[j]]);
-        else
-          Msg::Error("Wrong node index %d", n[j]);
-      }
-    }
-
-    if(unstructured) {
-      if(fscanf(fp, "%s %d", buffer, &numElements) != 2) {
-        fclose(fp);
-        return 0;
-      }
-      if(strcmp(buffer, "CELL_TYPES") || numElements != (int)cells.size()) {
-        Msg::Error("No or invalid number of cells types");
-        fclose(fp);
-        return 0;
-      }
-      for(std::size_t i = 0; i < cells.size(); i++) {
-        int type;
-        if(binary) {
-          if(fread(&type, sizeof(int), 1, fp) != 1) {
-            fclose(fp);
-            return 0;
-          }
-          if(!bigEndian) SwapBytes((char *)&type, sizeof(int), 1);
-        }
-        else {
-          if(fscanf(fp, "%d", &type) != 1) {
-            fclose(fp);
-            return 0;
-          }
-        }
-        switch(type) {
-        case 1: elements[0][iPoint++].push_back(new MPoint(cells[i])); break;
-        // first order elements
-        case 3: elements[1][iCurve].push_back(new MLine(cells[i])); break;
-        case 5: elements[2][iSurface].push_back(new MTriangle(cells[i])); break;
-        case 9:
-          elements[3][iSurface].push_back(new MQuadrangle(cells[i]));
-          break;
-        case 10:
-          elements[4][iVolume].push_back(new MTetrahedron(cells[i]));
-          break;
-        case 12:
-          elements[5][iVolume].push_back(new MHexahedron(cells[i]));
-          break;
-        case 13: elements[6][iVolume].push_back(new MPrism(cells[i])); break;
-        case 14: elements[7][iVolume].push_back(new MPyramid(cells[i])); break;
-        // second order elements
-        case 21: elements[1][iCurve].push_back(new MLine3(cells[i])); break;
-        case 22:
-          elements[2][iSurface].push_back(new MTriangle6(cells[i]));
-          break;
-        case 23:
-          elements[3][iSurface].push_back(new MQuadrangle8(cells[i]));
-          break;
-        case 28:
-          elements[3][iSurface].push_back(new MQuadrangle9(cells[i]));
-          break;
-        case 24:
-          elements[4][iVolume].push_back(new MTetrahedron10(
-            cells[i][0], cells[i][1], cells[i][2], cells[i][3], cells[i][4],
-            cells[i][5], cells[i][6], cells[i][7], cells[i][9], cells[i][8]));
-          break;
-        case 25:
-          elements[5][iVolume].push_back(new MHexahedron20(
-            cells[i][0], cells[i][1], cells[i][2], cells[i][3], cells[i][4],
-            cells[i][5], cells[i][6], cells[i][7], cells[i][8], cells[i][11],
-            cells[i][13], cells[i][9], cells[i][16], cells[i][18], cells[i][19],
-            cells[i][17], cells[i][10], cells[i][12], cells[i][14],
-            cells[i][15]));
-          break;
-        case 29:
-          elements[5][iVolume].push_back(new MHexahedron27(
-            cells[i][0], cells[i][1], cells[i][2], cells[i][3], cells[i][4],
-            cells[i][5], cells[i][6], cells[i][7], cells[i][8], cells[i][11],
-            cells[i][13], cells[i][9], cells[i][16], cells[i][18], cells[i][19],
-            cells[i][17], cells[i][10], cells[i][12], cells[i][14],
-            cells[i][15], cells[i][22], cells[i][23], cells[i][21],
-            cells[i][24], cells[i][20], cells[i][25], cells[i][26]));
-          break;
-        case 26:
-          elements[6][iVolume].push_back(
-            new MPrism15(cells[i][0], cells[i][1], cells[i][2], cells[i][3],
-                         cells[i][4], cells[i][5], cells[i][6], cells[i][9],
-                         cells[i][7], cells[i][12], cells[i][14], cells[i][13],
-                         cells[i][8], cells[i][10], cells[i][11]));
-          break;
-        case 32:
-          elements[6][iVolume].push_back(new MPrism18(
-            cells[i][0], cells[i][1], cells[i][2], cells[i][3], cells[i][4],
-            cells[i][5], cells[i][6], cells[i][9], cells[i][7], cells[i][12],
-            cells[i][14], cells[i][13], cells[i][8], cells[i][10], cells[i][11],
-            cells[i][15], cells[i][17], cells[i][16]));
-          break;
-        default: Msg::Error("Unknown type of cell %d", type); break;
-        }
-      }
+  // Read the cells (vertices per cell)
+  std::vector< std::vector<MVertex *> > cells(numElements);
+  for(std::size_t i = 0; i < cells.size(); i++){
+    int numVerts, n[100];
+    if(binary) {
+      if(fread(&numVerts, sizeof(int), 1, fp) != 1) { fclose(fp); return 0; }
+      if(!bigEndian) SwapBytes((char *)&numVerts, sizeof(int), 1);
+      if((int)fread(n, sizeof(int), numVerts, fp) != numVerts) { fclose(fp); return 0; }
+      if(!bigEndian) SwapBytes((char *)n, sizeof(int), numVerts);
     }
     else {
-      for(std::size_t i = 0; i < cells.size(); i++) {
-        int nbNodes = (int)cells[i].size();
-        switch(nbNodes) {
-        case 1: elements[0][iPoint++].push_back(new MPoint(cells[i])); break;
-        case 2: elements[1][iCurve].push_back(new MLine(cells[i])); break;
-        case 3: elements[2][iSurface].push_back(new MTriangle(cells[i])); break;
-        case 4:
-          elements[3][iSurface].push_back(new MQuadrangle(cells[i]));
+      if(fscanf(fp, "%d", &numVerts) != 1) { fclose(fp); return 0; }
+      for(int j = 0; j < numVerts; j++){
+        if(fscanf(fp, "%d", &n[j]) != 1) { fclose(fp); return 0; }
+      }
+    }
+    for(int j = 0; j < numVerts; j++){
+      if(n[j] >= 0 && n[j] < (int)vertices.size())
+        cells[i].push_back(vertices[n[j]]);
+      else
+        Msg::Error("Wrong node index %d", n[j]);
+    }
+  }
+
+  if(unstructured) {
+    if(fscanf(fp, "%s %d", buffer, &numElements) != 2) { fclose(fp); return 0; }
+    if(strcmp(buffer, "CELL_TYPES") || numElements != (int)cells.size()){
+      Msg::Error("No or invalid number of cells types");
+      fclose(fp);
+      return 0;
+    }
+    
+    for(std::size_t i = 0; i < cells.size(); i++){
+      int type;
+      if(binary) {
+        if(fread(&type, sizeof(int), 1, fp) != 1) { fclose(fp); return 0; }
+        if(!bigEndian) SwapBytes((char *)&type, sizeof(int), 1);
+      }
+      else {
+        if(fscanf(fp, "%d", &type) != 1) { fclose(fp); return 0; }
+      }
+      MElement *elem = NULL;
+      int oldId = -1;
+      int d = -1; // dimension
+
+      switch(type){
+        case 1:
+          elem = new MPoint(cells[i]);
+          oldId = iPoint;
+          d = 0;
+          elements[0][iPoint++].push_back(elem);
+          break;
+        // first order elements
+        case 3:
+          elem = new MLine(cells[i]);
+          oldId = iCurve;
+          d = 1;
+          elements[1][iCurve++].push_back(elem);
+          break;
+        case 5:
+          elem = new MTriangle(cells[i]);
+          oldId = iSurface;
+          d = 2;
+          elements[2][iSurface++].push_back(elem);
+          break;
+        case 9:
+          elem = new MQuadrangle(cells[i]);
+          oldId = iSurface;
+          d = 2;
+          elements[3][iSurface++].push_back(elem);
+          break;
+        case 10:
+          elem = new MTetrahedron(cells[i]);
+          oldId = iVolume;
+          d = 3;
+          elements[4][iVolume++].push_back(elem);
+          break;
+        case 12:
+          elem = new MHexahedron(cells[i]);
+          oldId = iVolume;
+          d = 3;
+          elements[5][iVolume++].push_back(elem);
+          break;
+        case 13:
+          elem = new MPrism(cells[i]);
+          oldId = iVolume;
+          d = 3;
+          elements[6][iVolume++].push_back(elem);
+          break;
+        case 14:
+          elem = new MPyramid(cells[i]);
+          oldId = iVolume;
+          d = 3;
+          elements[7][iVolume++].push_back(elem);
+          break;
+        // second order elements (adjust constructors as needed)
+        case 21:
+          elem = new MLine(cells[i]); // Replace with MLine3 if available
+          oldId = iCurve;
+          d = 1;
+          elements[1][iCurve++].push_back(elem);
+          break;
+        case 22:
+          elem = new MTriangle(cells[i]); // Replace with MTriangle6 if available
+          oldId = iSurface;
+          d = 2;
+          elements[2][iSurface++].push_back(elem);
+          break;
+        case 23:
+          elem = new MQuadrangle(cells[i]); // Replace with MQuadrangle8 if available
+          oldId = iSurface;
+          d = 2;
+          elements[3][iSurface++].push_back(elem);
+          break;
+        case 28:
+          elem = new MQuadrangle(cells[i]); // Replace with MQuadrangle9 if available
+          oldId = iSurface;
+          d = 2;
+          elements[3][iSurface++].push_back(elem);
+          break;
+        case 24:
+          elem = new MTetrahedron(cells[i]); // Replace with MTetrahedron10 if needed
+          oldId = iVolume;
+          d = 3;
+          elements[4][iVolume++].push_back(elem);
+          break;
+        case 25:
+          elem = new MHexahedron(cells[i]); // Replace with MHexahedron20 if needed
+          oldId = iVolume;
+          d = 3;
+          elements[5][iVolume++].push_back(elem);
+          break;
+        case 29:
+          elem = new MHexahedron(cells[i]); // Replace with MHexahedron27 if needed
+          oldId = iVolume;
+          d = 3;
+          elements[5][iVolume++].push_back(elem);
+          break;
+        case 26:
+          elem = new MPrism(cells[i]); // Replace with MPrism15 if needed
+          oldId = iVolume;
+          d = 3;
+          elements[6][iVolume++].push_back(elem);
+          break;
+        case 32:
+          elem = new MPrism(cells[i]); // Replace with MPrism18 if needed
+          oldId = iVolume;
+          d = 3;
+          elements[6][iVolume++].push_back(elem);
           break;
         default:
-          Msg::Error("Unknown type of mesh element with %d nodes", nbNodes);
+          Msg::Error("Unknown type of cell %d", type);
           break;
-        }
+      }
+      // Save the element and its original elementary id and dimension in file order:
+      if(elem) {
+        elementOrder.push_back(elem);
+        elementDim.push_back(d);
+        elementOldId.push_back(oldId);
+      }
+    }
+
+    // Now read the CELL_DATA section.
+    if(fscanf(fp, "%s %d", buffer, &numElements) == 2 && !strcmp(buffer, "CELL_DATA")) {
+      Msg::Info("Reading CELL_DATA with %d entries", numElements);
+      
+      char dataName[256], dataType[256];
+      int numComponents;
+      if(fscanf(fp, "%s %s %s %d", buffer, dataName, dataType, &numComponents) != 4 || strcmp(buffer, "SCALARS")) {
+          Msg::Error("Invalid SCALARS line after CELL_DATA");
+          fclose(fp);
+          return 0;
+      }
+      
+      if(fscanf(fp, "%s %s", buffer, buffer2) != 2 || strcmp(buffer, "LOOKUP_TABLE")) {
+          Msg::Error("Expected LOOKUP_TABLE after SCALARS, got %s", buffer);
+          fclose(fp);
+          return 0;
+      }
+      
+      // We now want to re-group cells so that elements sharing the same (dimension, physical tag)
+      // receive the same elementary id. We do not want one elementary entity per cell.
+      // First, for each cell (in file order) record its physical tag and build a mapping per dimension.
+      std::vector<int> cellPhysTag(numElements, -1);
+      // newGroup[d][physical tag] -> new elementary id
+      std::map<int,int> newGroup[4];
+      // New counters for each dimension:
+      int newCounter[4];
+      newCounter[0] = getMaxElementaryNumber(0) + 1;
+      newCounter[1] = getMaxElementaryNumber(1) + 1;
+      newCounter[2] = getMaxElementaryNumber(2) + 1;
+      newCounter[3] = getMaxElementaryNumber(3) + 1;
+      
+      // For each cell (in the same order as elementOrder):
+      for (int i = 0; i < numElements; i++) {
+          int val;
+          if (fscanf(fp, "%d", &val) != 1) {
+              Msg::Error("Error reading CELL_DATA value at index %d", i);
+              fclose(fp);
+              return 0;
+          }
+          cellPhysTag[i] = val;
+          int d = elementDim[i];
+          // If no group exists for this physical tag in dimension d, create one.
+          if(newGroup[d].find(val) == newGroup[d].end()){
+              newGroup[d][val] = newCounter[d]++;
+              physicals[d][ newGroup[d][val] ][ val ] = "";
+          }
+      }
+      
+      // Build a mapping from each element pointer (in file order) to its new elementary id:
+      std::map<MElement*, int> elementNewId;
+      for (int i = 0; i < numElements; i++){
+          int d = elementDim[i];
+          int newId = newGroup[d][ cellPhysTag[i] ];
+          elementNewId[elementOrder[i]] = newId;
+      }
+      
+      // Now update the original 'elements' array by re-grouping based on the new elementary id.
+      // Create a new array for elements; note that we have 8 element-type groups in the original.
+      std::map<int, std::vector<MElement *>> newElements[8];
+      for (int t = 0; t < 8; t++){
+          for (auto &pair : elements[t]) {
+              for (MElement* elem : pair.second) {
+                  int newId = elementNewId[elem];
+                  newElements[t][newId].push_back(elem);
+              }
+          }
+      }
+      // Replace the original grouping with the new grouping:
+      for (int t = 0; t < 8; t++){
+          elements[t] = newElements[t];
       }
     }
   }
   else if(haveLines) {
+    // Import lines if necessary (this branch is unchanged)
     if(!binary) {
       int v0, v1;
       char line[100000], *p, *pEnd, *pEnd2;
-      for(int k = 0; k < numElements; k++) {
+      for(int k = 0; k < numElements; k++){
         physicals[1][iCurve][1] = "centerline";
-        if(!fgets(line, sizeof(line), fp)) {
+        if(!fgets(line, sizeof(line), fp)){
           fclose(fp);
           return 0;
         }
-        v0 = (int)strtol(line, &pEnd, 10); // ignore first line
+        v0 = (int)strtol(line, &pEnd, 10); // ignore first number
         v0 = (int)strtol(pEnd, &pEnd2, 10);
         p = pEnd2;
-        while(1) {
+        while(1){
           v1 = strtol(p, &pEnd, 10);
           if(p == pEnd) break;
           elements[1][iCurve].push_back(new MLine(vertices[v0], vertices[v1]));
@@ -422,13 +508,15 @@ int GModel::readVTK(const std::string &name, bool bigEndian)
     }
   }
 
+  // Now store the elements into the model as usual.
   for(int i = 0; i < (int)(sizeof(elements) / sizeof(elements[0])); i++)
     _storeElementsInEntities(elements[i]);
   _associateEntityWithMeshVertices();
   _storeVerticesInEntities(vertices);
 
-  // store the physical tags
-  for(int i = 0; i < 4; i++) _storePhysicalTagsInEntities(i, physicals[i]);
+  // Finally, store the physical tags into the entities.
+  for(int d = 0; d < 4; d++)
+    _storePhysicalTagsInEntities(d, physicals[d]);
 
   fclose(fp);
   return 1;
