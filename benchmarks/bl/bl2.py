3bee2f895c420226b3d19fab62082455b6f99db1
jf remacle <jean-francois.remacle@uclouvain.be>
2025-03-12 21:52:52 +0100

    jf

--- benchmarks/bl/bl2.py
+++ benchmarks/bl/bl2.py
@@ -9,6 +9,8 @@ gmsh.plugin.setString('BoundaryLayer','Surfaces','3,4')
 gmsh.plugin.setString('BoundaryLayer','Curves','1')
 gmsh.plugin.setNumber('BoundaryLayer','Width',3.e-3)
 gmsh.plugin.run('BoundaryLayer')
+gmsh.plugin.run('BoundaryLayer')
+gmsh.plugin.run('BoundaryLayer')
 
 #gmsh.model.mesh.optimize('UntangleTris')
 
