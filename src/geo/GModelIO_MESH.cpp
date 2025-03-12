334c635bbd45a3981867759043a3a5c42ba00343
Florian Blachère <florian.blachere@utt.fr>
2025-03-12 14:37:09 +0100

    reorder vertices in P2 triangles

--- src/geo/GModelIO_MESH.cpp
+++ src/geo/GModelIO_MESH.cpp
@@ -205,7 +205,7 @@ int GModel::readMESH(const std::string &name)
           if(!fgets(buffer, sizeof(buffer), fp)) break;
           int n[10], cl;
           sscanf(buffer, "%d %d %d %d %d %d %d %d %d %d %d", &n[0], &n[1],
-                 &n[2], &n[3], &n[4], &n[5], &n[6], &n[7], &n[9], &n[8], &cl);
+                 &n[2], &n[3], &n[4], &n[5], &n[6], &n[7], &n[8], &n[9], &cl);
           for(int j = 0; j < 10; j++) n[j]--;
           std::vector<MVertex *> vertices;
           if(!getMeshVertices(10, n, vertexVector, vertices)) {
