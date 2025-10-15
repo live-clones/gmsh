ef4ded00748bb9dd958fb1dc9c8ffc2b518e5790
Boris Martin <boris.martin@uliege.be>
2025-10-15 18:05:15 +0200

    Improved error msg

--- src/geo/GModelIO_MSH4.cpp
+++ src/geo/GModelIO_MSH4.cpp
@@ -547,28 +547,28 @@ readMSH4Nodes(GModel *const model, FILE *fp, bool binary, bool &dense,
     if(!entity) {
       switch(entityDim) {
       case 0: {
-        Msg::Info("Creating discrete point %d", entityTag);
+        Msg::Info("Creating discrete point %d with %lu nodes.", entityTag, numNodes);
         GVertex *gv = new discreteVertex(model, entityTag);
         GModel::current()->add(gv);
         entity = gv;
         break;
       }
       case 1: {
-        Msg::Info("Creating discrete curve %d", entityTag);
+        Msg::Info("Creating discrete curve %d with %lu nodes.", entityTag, numNodes);
         GEdge *ge = new discreteEdge(model, entityTag, nullptr, nullptr);
         GModel::current()->add(ge);
         entity = ge;
         break;
       }
       case 2: {
-        Msg::Info("Creating discrete surface %d", entityTag);
+        Msg::Info("Creating discrete surface %d with %lu nodes.", entityTag, numNodes);
         GFace *gf = new discreteFace(model, entityTag);
         GModel::current()->add(gf);
         entity = gf;
         break;
       }
       case 3: {
-        Msg::Info("Creating discrete volume %d", entityTag);
+        Msg::Info("Creating discrete volume %d with %lu nodes.", entityTag, numNodes);
         GRegion *gr = new discreteRegion(model, entityTag);
         GModel::current()->add(gr);
         entity = gr;
