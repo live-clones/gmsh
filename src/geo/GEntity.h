4c97c654da820d7205d9a5116b3d8ab4d2b4c147
Boris Martin <boris.martin@uliege.be>
2026-01-07 12:34:39 +0100

    Added entity type strings for overlaps

--- src/geo/GEntity.h
+++ src/geo/GEntity.h
@@ -126,7 +126,9 @@ public:
     PartitionVolume,
     GhostCurve,
     GhostSurface,
-    GhostVolume
+    GhostVolume,
+    OverlapSurface,
+    OverlapVolume,
   };
 
   // the mesh generation status
@@ -174,7 +176,9 @@ public:
       "Partition volume",
       "Ghost curve",
       "Ghost surface",
-      "Ghost volume"};
+      "Ghost volume",
+      "Overlap surface",
+      "Overlap volume"};
     unsigned int type = (unsigned int)geomType();
     if(type >= sizeof(name) / sizeof(name[0]))
       return "Undefined";
