9a6ed8fe3a32418230cfae8863c0e51bd7fb3163
jf remacle <jean-francois.remacle@uclouvain.be>
2026-02-04 15:36:16 +0100

    bl3d examples

---
+++ benchmarks/bl/bl3d_3.py
@@ -0,0 +1,23 @@
+import gmsh
+l = 8.e-2
+gmsh.initialize()
+gmsh.merge('bl3d_3.brep')
+gmsh.option.setNumber('Mesh.MeshSizeFactor',.2);
+gmsh.model.mesh.generate(3)
+
+
+gmsh.plugin.setString('BoundaryLayer','Surfaces','7,8,9,10')
+gmsh.plugin.setString('BoundaryLayer','Volumes','1')
+gmsh.plugin.setNumber('BoundaryLayer','Thickness',1/40.)
+gmsh.plugin.setNumber('BoundaryLayer','Size',1/400.)
+
+gmsh.plugin.run('BoundaryLayer')
+
+gmsh.option.setNumber('Geometry.SurfaceLabels',1);
+gmsh.option.setNumber('Geometry.PointLabels',1);
+gmsh.option.setNumber('Geometry.CurveLabels',1);
+
+
+#gmsh.model.mesh.optimize('UntangleTris')
+
+gmsh.fltk.run()
