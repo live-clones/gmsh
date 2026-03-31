c3c66c9d6f11d65347cc34b4d0c8c03d03adcf27
Jiří Szotkowski <szotkowski.jiri@email.cz>
2026-03-31 11:10:39 +0200

    Inserting dummy elements in place of boundary edges

--- src/plugin/DuplicateNodes.cpp
+++ src/plugin/DuplicateNodes.cpp
@@ -20,6 +20,8 @@
 
 #include "FlGui.h"
 
+#include <algorithm>
+
 // StringXNumber DuplicateNodesOption_Number[] = {
 //   {GMSH_FULLRC, "Dimension", nullptr, 1.},
 //   {GMSH_FULLRC, "PhysicalGroup", nullptr, 1.},
@@ -39,14 +41,12 @@ StringXNumber DuplicateNodesOption_Number[] = {
 
 extern "C" {
 GMSH_Plugin *GMSH_RegisterDuplicateNodesPlugin()
-{
-  return new GMSH_DuplicateNodesPlugin();
-}
+{ return new GMSH_DuplicateNodesPlugin(); }
 }
 
 std::string GMSH_DuplicateNodesPlugin::getHelp() const
 {
-  return "Plugin(Crack) creates a crack around the orientable, "
+  return "TODO: EDIT THIS TEXT Plugin(Crack) create "
          "manifold physical group `PhysicalGroup' of dimension "
          "`Dimension' (1 or 2), embedded in a mesh of dimension "
          "`Dimension' + 1."
@@ -70,14 +70,10 @@ std::string GMSH_DuplicateNodesPlugin::getHelp() const
 }
 
 int GMSH_DuplicateNodesPlugin::getNbOptions() const
-{
-  return sizeof(DuplicateNodesOption_Number) / sizeof(StringXNumber);
-}
+{ return sizeof(DuplicateNodesOption_Number) / sizeof(StringXNumber); }
 
 StringXNumber *GMSH_DuplicateNodesPlugin::getOption(int iopt)
-{
-  return &DuplicateNodesOption_Number[iopt];
-}
+{ return &DuplicateNodesOption_Number[iopt]; }
 
 class EdgeData {
 public:
@@ -95,6 +91,83 @@ struct MEdgeDataLessThan {
     return false;
   }
 };
+
+void generateNewVerticesDependingOnShrinkFactorAndCentroid(
+  double lam, const std::vector<MVertex *> &vs, std::vector<MVertex *> &nvs,
+  GEntity *e)
+{
+  double cenx = 0.0, ceny = 0.0, cenz = 0.0;
+
+  for(size_t i = 0; i < vs.size(); i++) {
+    MVertex *v = vs[i];
+    cenx += v->x() / ((double)vs.size());
+    ceny += v->y() / ((double)vs.size());
+    cenz += v->z() / ((double)vs.size());
+  }
+
+  for(size_t i = 0; i < vs.size(); i++) {
+    MVertex *v = vs[i];
+    nvs.push_back(new MVertex((1.0 - lam) * v->x() + lam * cenx,
+                              (1.0 - lam) * v->y() + lam * ceny,
+                              (1.0 - lam) * v->z() + lam * cenz, e));
+  }
+}
+
+uint64_t getEdgeHash(const size_t a, const size_t b)
+{ return (a << SHIFT) | b; }
+
+void decryptEdgeHash(const uint64_t edgeHash, size_t &a, size_t &b)
+{
+  a = (edgeHash >> SHIFT);
+  b = edgeHash & ((1LL << SHIFT) - 1);
+}
+
+void removeVerticesAndElementsFromEntities(
+  GModel *m, const std::vector<GEntity *> &entities)
+{
+  for(auto e : entities) {
+    for(size_t i = 0; i < e->getNumMeshElements(); i++) {
+      MElement *el = e->getMeshElement(i);
+
+      std::vector<MVertex *> vs;
+      el->getVertices(vs);
+
+      for(size_t j = 0; j < vs.size(); j++) {
+        e->removeMeshVertex(vs[j], true);
+      }
+    }
+
+    e->removeElements(true);
+  }
+
+  m->destroyMeshCaches();
+}
+
+void insertDummyElement(const int elType, const size_t iel, const size_t ioel,
+                        MVertex *a, MVertex *b, MVertex *oa, MVertex *ob,
+                        std::vector<MElement *> &newElements,
+                        std::vector<size_t> &newElementsEntity)
+{
+  if(elType == 0) // QUADS
+  {
+    MQuadrangle *q = new MQuadrangle(b, a, oa, ob);
+    newElements.push_back(q);
+    if(newElementsEntity[iel] == 12) {}
+    newElementsEntity.push_back(
+      std::max(newElementsEntity[iel], newElementsEntity[ioel]));
+  }
+  else if(elType == 1) // TRIANGLES
+  {
+    MTriangle *t1 = new MTriangle(b, a, oa);
+    newElements.push_back(t1);
+    newElementsEntity.push_back(
+      std::max(newElementsEntity[iel], newElementsEntity[ioel]));
+    MTriangle *t2 = new MTriangle(oa, ob, b);
+    newElements.push_back(t2);
+    newElementsEntity.push_back(
+      std::max(newElementsEntity[iel], newElementsEntity[ioel]));
+  }
+}
 
 /*
 ELEMENT TYPE:
@@ -116,13 +189,67 @@ PView *GMSH_DuplicateNodesPlugin::execute(PView *view)
   std::vector<GEntity *> entities;
   m->getEntities(entities);
 
-  // m->getBoundaryTags()
+  // std::vector<std::pair<int, int>> lineDimTags;
+  // for(size_t i = 0; i < entities.size(); i++) {
+  //   GEntity *e = entities[i];
+  //   if(e->dim() != 1) continue;
 
-  std::map<size_t, std::vector<MVertex *>> newNodes;
-  std::map<size_t, std::vector<MElement *>> newElements;
-  std::map<size_t, std::map<uint64_t, size_t>> edgeToElement;
-  std::map<size_t, size_t> parentVertex;
+  //   lineDimTags.emplace_back(1, e->tag());
+  // }
 
+  // std::vector<std::pair<int, int>> boundaryDimTags;
+  // m->getBoundaryTags(lineDimTags, boundaryDimTags, false);
+
+  // for(size_t i = 0; i < lineDimTags.size(); i++) {
+  //   Msg::Info("line: %d %d", lineDimTags[i].first, lineDimTags[i].second);
+  // }
+
+  // for(size_t i = 0; i < boundaryDimTags.size(); i++) {
+  //   Msg::Info("boundary: %d %d", boundaryDimTags[i].first,
+  //             boundaryDimTags[i].second);
+  // }
+
+  // return view;
+
+  std::vector<MVertex *> newNodes;
+  std::vector<size_t> newNodesEntity;
+  std::vector<MElement *> newElements;
+  std::vector<size_t> newElementsEntity;
+  std::map<uint64_t, size_t> edgeHashToElementIndex;
+  std::map<size_t, size_t> parentVertexNum;
+  std::map<uint64_t, size_t> edgeHashTo1DElementNum;
+  std::map<size_t, std::vector<size_t>>
+    neighboringSurfacesOf1DElements; // 1D elem by num, entity by index
+  std::map<size_t, size_t> oneDElementNumToLineIndex;
+
+  // Fill edgeHashTo1DElementNum datastructure
+  //  and oneDElementNumToLineIndex
+  for(size_t ie = 0; ie < entities.size(); ie++) {
+    GEntity *e = entities[ie];
+    if(e->dim() != 1) continue;
+
+    for(size_t iel = 0; iel < e->getNumMeshElements(); iel++) {
+      MElement *el = e->getMeshElement(iel);
+
+      oneDElementNumToLineIndex[el->getNum()] = ie;
+
+      std::vector<MVertex *> vs;
+      el->getVertices(vs);
+
+      std::vector<size_t> ivs;
+      for(size_t i = 0; i < vs.size(); i++) { ivs.push_back(vs[i]->getNum()); }
+
+      for(size_t i = 0; i < ivs.size(); i++) {
+        const size_t a = ivs[i], b = ivs[(i + 1) % ivs.size()];
+        uint64_t hash = getEdgeHash(a, b);
+        uint64_t dualHash = getEdgeHash(b, a);
+        edgeHashTo1DElementNum[hash] = el->getNum();
+        edgeHashTo1DElementNum[dualHash] = el->getNum();
+      }
+    }
+  }
+
+  // Fill neighboringSurfacesTo1DElements datastructure
   for(size_t ie = 0; ie < entities.size(); ie++) {
     GEntity *e = entities[ie];
     if(e->dim() != 2) continue;
@@ -133,134 +260,17 @@ PView *GMSH_DuplicateNodesPlugin::execute(PView *view)
       std::vector<MVertex *> vs;
       el->getVertices(vs);
 
-      double cenx = 0.0, ceny = 0.0, cenz = 0.0;
-      for(size_t i = 0; i < vs.size(); i++) {
-        MVertex *v = vs[i];
-        cenx += v->x() / ((double)vs.size());
-        ceny += v->y() / ((double)vs.size());
-        cenz += v->z() / ((double)vs.size());
-      }
-
-      std::vector<MVertex *> nvs;
-      double lam = DNPP_SHRINK;
-
-      for(size_t i = 0; i < vs.size(); i++) {
-        MVertex *v = vs[i];
-        nvs.push_back(new MVertex((1.0 - lam) * v->x() + lam * cenx,
-                                  (1.0 - lam) * v->y() + lam * ceny,
-                                  (1.0 - lam) * v->z() + lam * cenz, e));
-      }
-
-      for(size_t i = 0; i < nvs.size(); i++) { newNodes[ie].push_back(nvs[i]); }
-
-      MElement *nel;
-      if(nvs.size() == 3) { nel = new MTriangle(nvs[0], nvs[1], nvs[2]); }
-      else if(nvs.size() == 4) {
-        nel = new MQuadrangle(nvs[0], nvs[1], nvs[2], nvs[3]);
-      }
-      else {
-        Msg::Error(
-          "The mesh contains 2D element which is not triangle nor quadrangle.");
-        return view;
-      }
-      newElements[ie].push_back(nel);
-
       std::vector<size_t> ivs;
       for(size_t i = 0; i < vs.size(); i++) { ivs.push_back(vs[i]->getNum()); }
 
       for(size_t i = 0; i < ivs.size(); i++) {
-        edgeToElement[ie][(ivs[i] << SHIFT) | ivs[(i + 1) % ivs.size()]] =
-          newElements[ie].size() - 1;
-      }
+        const size_t a = ivs[i], b = ivs[(i + 1) % ivs.size()];
+        uint64_t hash = getEdgeHash(a, b);
 
-      for(size_t i = 0; i < nvs.size(); i++) {
-        parentVertex[nvs[i]->getNum()] = ivs[i];
-      }
-    }
-  }
-
-  for(auto e : entities) {
-    if(e->dim() != 2) continue;
-
-    for(size_t i = 0; i < e->getNumMeshElements(); i++) {
-      MElement *el = e->getMeshElement(i);
-
-      std::vector<MVertex *> vs;
-      el->getVertices(vs);
-
-      for(size_t i = 0; i < vs.size(); i++) {
-        e->removeMeshVertex(vs[i], true);
-      }
-    }
-
-    e->removeElements(true);
-  }
-
-  m->destroyMeshCaches();
-
-  std::map<uint64_t, bool> edgeVisited;
-  for(size_t ie = 0; ie < entities.size(); ie++) {
-    if(entities[ie]->dim() != 2) continue;
-    for(auto &[edgeHash, iel] : edgeToElement[ie]) {
-      if(edgeVisited.count(edgeHash)) continue;
-      edgeVisited[edgeHash] = true;
-
-      size_t ia, ib;
-      ib = edgeHash & ((1LL << SHIFT) - 1);
-      ia = (edgeHash >> SHIFT);
-
-      uint64_t dualHash = (ib << SHIFT) | ia;
-      if(!edgeToElement[ie].count(dualHash)) continue;
-      edgeVisited[dualHash] = true;
-      const size_t ioel = edgeToElement[ie][dualHash];
-
-      size_t iael = -1, ibel = -1, iaoel = -1, iboel = -1;
-      std::vector<MVertex *> elv;
-      newElements[ie][iel]->getVertices(elv);
-      for(size_t i = 0; i < elv.size(); i++) {
-        auto v = elv[i];
-        auto vNum = v->getNum();
-        if(parentVertex[vNum] == ia) {
-          iael = i;
-          continue;
+        if(edgeHashTo1DElementNum.count(hash)) {
+          neighboringSurfacesOf1DElements[edgeHashTo1DElementNum[hash]]
+            .push_back(ie);
         }
-        if(parentVertex[vNum] == ib) {
-          ibel = i;
-          continue;
-        }
-      }
-      std::vector<MVertex *> oelv;
-      newElements[ie][ioel]->getVertices(oelv);
-      for(size_t i = 0; i < oelv.size(); i++) {
-        auto v = oelv[i];
-        auto vNum = v->getNum();
-        if(parentVertex[vNum] == ia) {
-          iaoel = i;
-          continue;
-        }
-        if(parentVertex[vNum] == ib) {
-          iboel = i;
-          continue;
-        }
-      }
-
-      MVertex *a, *b, *oa, *ob;
-      a = newElements[ie][iel]->getVertex(iael);
-      b = newElements[ie][iel]->getVertex(ibel);
-      oa = newElements[ie][ioel]->getVertex(iaoel);
-      ob = newElements[ie][ioel]->getVertex(iboel);
-
-      if(DNPP_ELTYPETOINSERT == 0) // QUADS
-      {
-        MQuadrangle *q = new MQuadrangle(b, a, oa, ob);
-        newElements[ie].push_back(q);
-      }
-      else if(DNPP_ELTYPETOINSERT == 1) // TRIANGLES
-      {
-        MTriangle *nel1 = new MTriangle(b, a, oa);
-        newElements[ie].push_back(nel1);
-        MTriangle *nel2 = new MTriangle(oa, ob, b);
-        newElements[ie].push_back(nel2);
       }
     }
   }
@@ -269,17 +279,174 @@ PView *GMSH_DuplicateNodesPlugin::execute(PView *view)
     GEntity *e = entities[ie];
     if(e->dim() != 2) continue;
 
-    if(!newNodes.count(ie)) continue;
-    for(size_t iv = 0; iv < newNodes[ie].size(); iv++) {
-      MVertex *v = newNodes[ie][iv];
-      e->addMeshVertex(v);
+    for(size_t iel = 0; iel < e->getNumMeshElements(); iel++) {
+      MElement *el = e->getMeshElement(iel);
+
+      std::vector<MVertex *> vs;
+      el->getVertices(vs);
+      std::vector<MVertex *> nvs;
+      generateNewVerticesDependingOnShrinkFactorAndCentroid(DNPP_SHRINK, vs,
+                                                            nvs, e);
+
+      for(size_t i = 0; i < nvs.size(); i++) { newNodes.push_back(nvs[i]); }
+
+      MElement *nel;
+      if(nvs.size() == 3) { nel = new MTriangle(nvs[0], nvs[1], nvs[2]); }
+      else if(nvs.size() == 4) {
+        nel = new MQuadrangle(nvs[0], nvs[1], nvs[2], nvs[3]);
+      }
+      else {
+        Msg::Error("The mesh contains 2D element which is not triangle nor "
+                   "quadrangle.");
+        return view;
+      }
+      newElements.push_back(nel);
+      newElementsEntity.push_back(ie);
+      const size_t newFullMeasureElementIndex = newElements.size() - 1;
+
+      std::vector<size_t> nums_vs;
+      for(size_t i = 0; i < vs.size(); i++) {
+        nums_vs.push_back(vs[i]->getNum());
+      }
+
+      std::vector<size_t> targetEntityOfCopiedNode(nvs.size(), ie);
+      for(size_t i = 0; i < nums_vs.size(); i++) {
+        const size_t ia = i, ib = (i + 1) % nums_vs.size();
+        const size_t a = nums_vs[ia], b = nums_vs[ib];
+        uint64_t hash = getEdgeHash(a, b);
+        edgeHashToElementIndex[hash] = newFullMeasureElementIndex;
+
+        if(edgeHashTo1DElementNum.count(hash)) {
+          size_t num1DElement = edgeHashTo1DElementNum[hash];
+          const auto &surfaces = neighboringSurfacesOf1DElements[num1DElement];
+
+          if(surfaces.size() == 1) {
+            // Boundary --> add 1D element and dummy element
+            const size_t oneDEntityIndex =
+              oneDElementNumToLineIndex[num1DElement];
+            MVertex *oa = new MVertex(vs[ia]->x(), vs[ia]->y(), vs[ia]->z(),
+                                      entities[oneDEntityIndex]);
+            MVertex *ob = new MVertex(vs[ib]->x(), vs[ib]->y(), vs[ib]->z(),
+                                      entities[oneDEntityIndex]);
+
+            newNodes.push_back(oa);
+            newNodes.push_back(ob);
+
+            targetEntityOfCopiedNode.push_back(
+              oneDElementNumToLineIndex[num1DElement]);
+            targetEntityOfCopiedNode.push_back(
+              oneDElementNumToLineIndex[num1DElement]);
+
+            newElements.push_back(new MLine(oa, ob));
+            newElementsEntity.push_back(oneDEntityIndex);
+
+            insertDummyElement(DNPP_ELTYPETOINSERT, newFullMeasureElementIndex,
+                               newElements.size() - 1, nvs[ia], nvs[ib], oa, ob,
+                               newElements, newElementsEntity);
+
+            continue;
+          }
+
+          if(surfaces.size() == 2) {
+            // Inner edge (line, 1D entity) --> one side of the 2D element will
+            // be also 1D element; which one of the two elements will be chosen
+            // is decided by the surface tag <-- the 2D element in the surface
+            // with lesser tag will contain the 1D element
+            if(*std::min_element(surfaces.begin(), surfaces.end()) != ie)
+              continue;
+
+            size_t targetEntity = oneDElementNumToLineIndex[num1DElement];
+            targetEntityOfCopiedNode[ia] = targetEntity;
+            targetEntityOfCopiedNode[ib] = targetEntity;
+            newElements.push_back(new MLine(nvs[ia], nvs[ib]));
+            newElementsEntity.push_back(targetEntity);
+
+            continue;
+          }
+
+          Msg::Error(
+            "There is a 1D element that has more than 2 neighboring surfaces.");
+          return view;
+        }
+      }
+
+      for(size_t i = 0; i < nvs.size(); i++) {
+        parentVertexNum[nvs[i]->getNum()] = nums_vs[i];
+      }
+
+      for(size_t i = 0; i < targetEntityOfCopiedNode.size(); i++) {
+        newNodesEntity.push_back(targetEntityOfCopiedNode[i]);
+      }
+    }
+  }
+
+  removeVerticesAndElementsFromEntities(m, entities);
+
+  std::map<uint64_t, bool> edgeVisited;
+  // Iterate through edges within the entity
+  for(auto &[edgeHash, iel] : edgeHashToElementIndex) {
+    // Check whether this edge was already processed
+    if(edgeVisited.count(edgeHash)) continue;
+    edgeVisited[edgeHash] = true;
+    size_t ia, ib;
+    decryptEdgeHash(edgeHash, ia, ib);
+    uint64_t dualHash = getEdgeHash(ib, ia);
+    if(!edgeHashToElementIndex.count(dualHash)) continue;
+    edgeVisited[dualHash] = true;
+    const size_t ioel = edgeHashToElementIndex[dualHash];
+
+    // Find, which vertices (from the set of newly added ones) need to be
+    // connected
+    size_t iael = -1, ibel = -1, iaoel = -1, iboel = -1;
+
+    std::vector<MVertex *> elv;
+    newElements[iel]->getVertices(elv);
+    for(size_t i = 0; i < elv.size(); i++) {
+      auto v = elv[i];
+      auto vNum = v->getNum();
+      if(parentVertexNum[vNum] == ia) {
+        iael = i;
+        continue;
+      }
+      if(parentVertexNum[vNum] == ib) {
+        ibel = i;
+        continue;
+      }
+    }
+    std::vector<MVertex *> oelv;
+    newElements[ioel]->getVertices(oelv);
+    for(size_t i = 0; i < oelv.size(); i++) {
+      auto v = oelv[i];
+      auto vNum = v->getNum();
+      if(parentVertexNum[vNum] == ia) {
+        iaoel = i;
+        continue;
+      }
+      if(parentVertexNum[vNum] == ib) {
+        iboel = i;
+        continue;
+      }
     }
 
-    if(!newElements.count(ie)) continue;
-    for(size_t iel = 0; iel < newElements[ie].size(); iel++) {
-      MElement *el = newElements[ie][iel];
-      e->addElement(el);
-    }
+    // Add the quad or triangle
+    MVertex *a, *b, *oa, *ob;
+    a = newElements[iel]->getVertex(iael);
+    b = newElements[iel]->getVertex(ibel);
+    oa = newElements[ioel]->getVertex(iaoel);
+    ob = newElements[ioel]->getVertex(iboel);
+
+    insertDummyElement(DNPP_ELTYPETOINSERT, iel, ioel, a, b, oa, ob,
+                       newElements, newElementsEntity);
+  }
+
+  for(size_t iv = 0; iv < newNodes.size(); iv++) {
+    MVertex *v = newNodes[iv];
+    entities[newNodesEntity[iv]]->addMeshVertex(v);
+  }
+
+  for(size_t iel = 0; iel < newElements.size(); iel++) {
+    MElement *el = newElements[iel];
+    entities[newElementsEntity[iel]]->addElement(el);
   }
 
   m->destroyMeshCaches();
