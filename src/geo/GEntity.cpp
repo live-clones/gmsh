5970323acca5d9c3329e3177649ef7ca96f46811
Christophe Geuzaine <cgeuzaine@uliege.be>
2026-09-08 11:32:43 +0200

    geo: take the clipping planes out of the element visibility test, and give whole element mode the elements a plane cuts in an array of its own, drawn whole while OpenGL clips the rest

--- src/geo/GEntity.cpp
+++ src/geo/GEntity.cpp
@@ -21,7 +21,8 @@ int GEntity::numSelected = 0;
 GEntity::GEntity(GModel *m, int t)
   : _model(m), _tag(t), _meshMaster(this), _visible(1), _selection(0),
     _onlySomeElementsVisible(1), _obb(nullptr), va_lines(nullptr),
-    va_triangles(nullptr), va_caps(nullptr)
+    va_triangles(nullptr), va_clip_lines(nullptr),
+    va_clip_triangles(nullptr)
 {
   // default color when none is explicitly specified - don't change this, as it
   // is documented and used in scripts to detect if explicit colors are set
@@ -35,13 +36,15 @@ void GEntity::deleteVertexArrays()
   va_lines = nullptr;
   if(va_triangles) delete va_triangles;
   va_triangles = nullptr;
-  deleteCapVertexArrays();
+  deleteClipVertexArrays();
 }
 
-void GEntity::deleteCapVertexArrays()
+void GEntity::deleteClipVertexArrays()
 {
-  if(va_caps) delete va_caps;
-  va_caps = nullptr;
+  if(va_clip_lines) delete va_clip_lines;
+  va_clip_lines = nullptr;
+  if(va_clip_triangles) delete va_clip_triangles;
+  va_clip_triangles = nullptr;
 }
 
 char GEntity::getVisibility()
